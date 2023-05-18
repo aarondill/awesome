ecce34c23fb84197a8b73ae396ead478b374e6f6
aarondill <aaronsacks2006@gmail.com>
2023-05-17 21:44:33 -0500

    change defaults, add launcher, and export layout-box
    Former-commit-id: ec65bb82419828ed16ad267f892589b2ed83ea48

--- layout/top-panel.lua
+++ layout/top-panel.lua
@@ -3,12 +3,10 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 local TaskList = require("widget.task-list")
 local TagList = require("widget.tag-list")
-local gears = require("gears")
-local clickable_container = require("widget.material.clickable-container")
-local mat_icon_button = require("widget.material.icon-button")
-local mat_icon = require("widget.material.icon")
+local LayoutBox = require("widget.layout-box")
 local dpi = require("beautiful").xresources.apply_dpi
-local icons = require("theme.icons")
+
+local menu = require("widget.launcher")
 
 -- Titus - Horizontal Tray
 local systray = wibox.widget.systray()
@@ -24,42 +22,12 @@ local textclock = wibox.widget.textclock('<span font="Roboto Mono 12">%I:%M %p</
 
 -- Add a calendar (credits to kylekewley for the original code)
 local month_calendar = awful.widget.calendar_popup.month({
-	screen = s,
-	start_sunday = false,
+	start_sunday = true,
 	week_numbers = true,
 })
 month_calendar:attach(textclock)
 
 local clock_widget = wibox.container.margin(textclock, dpi(13), dpi(13), dpi(9), dpi(8))
-
-local add_button = mat_icon_button(mat_icon(icons.plus, dpi(24)))
-add_button:buttons(gears.table.join(awful.button({}, 1, nil, function()
-	awful.spawn(awful.screen.focused().selected_tag.defaultApp, {
-		tag = _G.mouse.screen.selected_tag,
-		placement = awful.placement.bottom_right,
-	})
-end)))
-
--- Create an imagebox widget which will contains an icon indicating which layout we're using.
--- We need one layoutbox per screen.
-local LayoutBox = function(s)
-	local layoutBox = clickable_container(awful.widget.layoutbox(s))
-	layoutBox:buttons(awful.util.table.join(
-		awful.button({}, 1, function()
-			awful.layout.inc(1)
-		end),
-		awful.button({}, 3, function()
-			awful.layout.inc(-1)
-		end),
-		awful.button({}, 4, function()
-			awful.layout.inc(1)
-		end),
-		awful.button({}, 5, function()
-			awful.layout.inc(-1)
-		end)
-	))
-	return layoutBox
-end
 
 local TopPanel = function(s)
 	local panel = wibox({
@@ -85,12 +53,10 @@ local TopPanel = function(s)
 		layout = wibox.layout.align.horizontal,
 		{
 			layout = wibox.layout.fixed.horizontal,
-			-- Create a taglist widget
+			menu(s),
 			TagList(s),
-			TaskList(s),
-			add_button,
 		},
-		nil,
+		TaskList(s),
 		{
 			layout = wibox.layout.fixed.horizontal,
 			wibox.container.margin(systray, dpi(3), dpi(3), dpi(6), dpi(3)),
---
+++ widget/launcher.lua
@@ -0,0 +1,67 @@
+local awful = require("awful")
+local dpi = require("beautiful").xresources.apply_dpi
+local wibox = require("wibox")
+local apps = require("configuration.apps")
+local hotkeys_popup = require("awful.hotkeys_popup")
+local menubar = require("menubar")
+local icons = require("theme.icons")
+
+-- Load Debian menu entries
+local has_debian, debian = pcall(require, "debian.menu")
+local has_fdo, freedesktop = pcall(require, "freedesktop")
+
+function Menu(_)
+	-- Create a launcher widget and a main menu
+	local awesome_ctrl_menu = {
+		{
+			"hotkeys",
+			function()
+				hotkeys_popup.show_help(nil, awful.screen.focused())
+			end,
+		},
+		{ "manual", apps.default.terminal .. " -e man awesome" },
+		{ "edit config", apps.default.editor .. " " .. awesome.conffile },
+		{ "restart", awesome.restart },
+		{
+			"quit",
+			function()
+				awesome.quit()
+			end,
+		},
+	}
+
+	local menu_awesome = { "Awesome", awesome_ctrl_menu }
+	local menu_terminal = { "Open Terminal", apps.default.terminal }
+
+	local mainmenu
+	if has_fdo then
+		mainmenu = freedesktop.menu.build({
+			before = { menu_awesome },
+			after = { menu_terminal },
+		})
+	elseif has_debian then
+		mainmenu = awful.menu({
+			items = {
+				menu_awesome,
+				{ "Debian", debian.menu.Debian_menu.Debian },
+				menu_terminal,
+			},
+		})
+	else
+		mainmenu = awesome.menu({ items = { menu_awesome, menu_terminal } })
+	end
+
+	local launcher = awful.widget.launcher({
+		menu = mainmenu,
+		image = icons.launcher,
+		-- resize = true,
+	})
+
+	-- Menubar configuration
+	menubar.utils.terminal = apps.default.terminal -- Set the terminal for applications that require it
+
+	local m = dpi(6)
+	return wibox.container.margin(launcher, m, m, m, m)
+end
+
+return Menu
