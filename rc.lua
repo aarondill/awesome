448efd98d1d85825b1f2c0c4fb19e84cca93e915
aarondill <aaronsacks2006@gmail.com>
2023-05-17 21:18:29 -0500

    clone from from https://github.com/ChrisTitusTech/titus-awesome

--- rc.lua
+++ rc.lua
@@ -1,35 +1,71 @@
--- TODO:
--- Create global tablist for use in all cases
+local gears = require('gears')
+local awful = require('awful')
+local wibox = require("wibox")
+require('awful.autofocus')
+local beautiful = require('beautiful')
 
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
+-- Theme
+beautiful.init(require('theme'))
 
--- Focus on mouse over
-require("awful.autofocus")
-
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
-
--- Show a notification if something goes wrong
-require("modules.error_handling")
-
--- Auto-start
-require("modules.auto-start")
-
--- theme(themeName)
-require("theme")(nil)
 
 -- Layout
-require("layout")
+require('layout')
 
-local bindings = require("modules.bindings")
--- Set keys
-root.keys(bindings.globalkeys)
+-- Init all modules
+require('module.notifications')
+require('module.auto-start')
+require('module.decorate-client')
+-- Backdrop causes bugs on some gtk3 applications
+--require('module.backdrop')
+require('module.exit-screen')
+require('module.quake-terminal')
 
-require("modules.rules")
-require("modules.signals")
+-- Setup all configurations
+require('configuration.client')
+require('configuration.tags')
+_G.root.keys(require('configuration.keys.global'))
 
--- Different tags for each wallpaper
-require("modules.wallpaper")
+-- {{{ Screen
+-- Reset wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal( "property::geometry", function(s) beautiful.wallpaper.maximized( beautiful.wallpaper, s, beautiful.wallpapers) end )
+
+-- Signal function to execute when a new client appears.
+_G.client.connect_signal(
+  'manage',
+  function(c)
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    if not _G.awesome.startup then
+      awful.client.setslave(c)
+    end
+
+    if _G.awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+      -- Prevent clients from being unreachable after screen count changes.
+      awful.placement.no_offscreen(c)
+    end
+  end
+)
+
+-- Enable sloppy focus, so that focus follows mouse.
+--[[
+_G.client.connect_signal(
+  'mouse::enter',
+  function(c)
+    c:emit_signal('request::activate', 'mouse_enter', {raise = true})
+  end
+)
+--]]
+
+-- Make the focused window have a glowing border
+_G.client.connect_signal(
+  'focus',
+  function(c)
+    c.border_color = beautiful.border_focus
+  end
+)
+_G.client.connect_signal(
+  'unfocus',
+  function(c)
+    c.border_color = beautiful.border_normal
+  end
+)
---
+++ widget/layout-box.lua
@@ -0,0 +1,24 @@
+local awful = require("awful")
+local clickable_container = require("widget.material.clickable-container")
+
+-- Create an imagebox widget which will contains an icon indicating which layout we're using.
+-- We need one layoutbox per screen.
+local LayoutBox = function(s)
+	local layoutBox = clickable_container(awful.widget.layoutbox(s))
+	layoutBox:buttons(awful.util.table.join(
+		awful.button({}, 1, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 3, function()
+			awful.layout.inc(-1)
+		end),
+		awful.button({}, 4, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 5, function()
+			awful.layout.inc(-1)
+		end)
+	))
+	return layoutBox
+end
+return LayoutBox
