b1f3e41c2f3f73ea5b8610ad6fc4b3378ff8affe
aarondill <aaronsacks2006@gmail.com>
2023-05-18 20:28:08 -0500

    keep the selected tag on restart
    Former-commit-id: 555e9dc9533bb3b2977c6f28b4e7eb8452fee43d

--- rc.lua
+++ rc.lua
@@ -44,6 +44,8 @@ root.keys(require("configuration.keys.global"))
 
 -- Different tags for each wallpaper
 require("module.wallpaper")
+-- Keep selected tag on restart
+require("module.persistent-tag")
 
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
