577739aed5fefd397135cc209b3a23fe6c273daa
aarondill <aaronsacks2006@gmail.com>
2023-05-18 20:40:47 -0500

    better tag handling
    Former-commit-id: 6be164d4ef39e51734c01d653668f4b4207df7c3

--- module/persistent-tag.lua
+++ module/persistent-tag.lua
@@ -1,18 +1,24 @@
+local awful = require("awful")
 awesome.connect_signal("exit", function(reason_restart)
 	if not reason_restart then
 		return
 	end
 
-	local file = io.open("/tmp/awesomewm-last-selected-tags", "w+")
-	if not file then
-		return
-	end
-
+	local str = ""
 	for s in screen do
-		file:write(s.selected_tag.index, "\n")
+		local tags = s.selected_tags
+		for _, tag in ipairs(tags) do
+			str = str .. tag.index .. "\n"
+		end
+		str = str .. "\n"
 	end
 
-	file:close()
+	-- empty line means next screen
+	local file = io.open("/tmp/awesomewm-last-selected-tags", "w+")
+	if file then
+		file:write(str)
+		file:close()
+	end
 end)
 
 awesome.connect_signal("startup", function()
@@ -21,16 +27,28 @@ awesome.connect_signal("startup", function()
 		return
 	end
 
+	local screen_tags = {}
+	local scrI = 1
 	local selected_tags = {}
-
 	for line in file:lines() do
+		if line == "\n" or line == "" then
+			screen_tags[scrI] = selected_tags
+			selected_tags = {}
+			scrI = scrI + 1
+		end
 		table.insert(selected_tags, tonumber(line))
 	end
-
+	-- awful.screen[s].tags
 	for s in screen do
-		local i = selected_tags[s.index]
-		local t = s.tags[i]
-		t:view_only()
+		local sel_tags = screen_tags[s.index]
+		if sel_tags then
+			for _, tag in ipairs(sel_tags) do
+				local t = s.tags[tag]
+				if t then
+					t.selected = true
+				end
+			end
+		end
 	end
 
 	file:close()
