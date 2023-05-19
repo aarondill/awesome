80272f91310e38e28955fbb81f0fa081c8629cb7
aarondill <aaronsacks2006@gmail.com>
2023-05-18 20:24:43 -0500

    use pasystray for audio, finish
    Former-commit-id: 059c314ac3f8bf687ec88f0630385ec330077f0c

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -33,7 +33,7 @@ return {
 		"picom --config " .. filesystem.get_configuration_dir() .. "/configuration/picom.conf",
 		"nm-applet --indicator", -- wifi
 		"blueman-applet", --bluetooth
-		"pnmixer", -- shows an audiocontrol applet in systray when installed.
+		"pasystray", -- shows an audiocontrol applet in systray when installed.
 		--'blueberry-tray', -- Bluetooth tray icon
 		"numlockx on", -- enable numlock
 		"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)", -- credential manager
