70beedb7551bafeb941286c79ad358bfab7a58f4
aarondill <aaronsacks2006@gmail.com>
2023-05-18 22:52:00 -0500

    add super+ctrl+drag to resize

--- configuration/client/buttons.lua
+++ configuration/client/buttons.lua
@@ -5,11 +5,23 @@ local modkey = require("configuration.keys.mod").modKey
 
 return gears.table.join(
 	awful.button({}, 1, function(c)
-		_G.client.focus = c
-		c:raise()
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
 	end),
-	awful.button({ modkey }, 1, awful.mouse.client.move),
-	awful.button({ modkey }, 3, awful.mouse.client.resize),
+	awful.button({ modkey }, 1, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+		awful.mouse.client.move(c)
+	end),
+	awful.button({ modkey }, 3, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+		awful.mouse.client.resize(c)
+	end),
+
+	-- ctrl+super+drag = resize client
+	awful.button({ modkey, "Control" }, 1, function(c)
+		c:emit_signal("request::activate", "mouse_click", { raise = true })
+		awful.mouse.client.resize(c)
+	end),
+
 	awful.button({ modkey }, 4, function()
 		awful.layout.inc(1)
 	end),
