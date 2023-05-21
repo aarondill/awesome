c7607dfe6b185c35ff95610aa90f927e11f6bb47
aarondill <aaronsacks2006@gmail.com>
2023-05-21 01:45:12 -0500

    notify on errors

--- util/installed.lua
+++ util/installed.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local naughty = require("naughty")
+local handle_error = require("handle_error")
 
 -- State to ensure only one notification is sent
 local has_notified = false
@@ -11,24 +12,27 @@ local has_notified = false
 ---WARNING: Use sparingly, as this function must call 'which' and wait for it's response.
 ---If possible, cache the results and avoid calling it again.
 local function installed(program, cb)
-	awful.spawn.easy_async({ "which", program }, function(stdout, _, exitreason, exitcode)
-		-- If command not found
-		if exitreason == "exit" and exitcode == 127 then
-			if not has_notified then
-				naughty.notify({
-					preset = naughty.config.presets.warn,
-					title = "Could not find 'which'",
-					text = "Please ensure 'which' is installed have a better experience.",
-				})
+	awful.spawn.easy_async(
+		{ "which", program },
+		handle_error(function(stdout, _, exitreason, exitcode)
+			-- If command not found
+			if exitreason == "exit" and exitcode == 127 then
+				if not has_notified then
+					naughty.notify({
+						preset = naughty.config.presets.warn,
+						title = "Could not find 'which'",
+						text = "Please ensure 'which' is installed have a better experience.",
+					})
+				end
 			end
-		end
 
-		if exitreason == "exit" and exitcode == 0 then
-			cb(stdout)
-		else
-			cb(nil)
-		end
-	end)
+			if exitreason == "exit" and exitcode == 0 then
+				cb(stdout)
+			else
+				cb(nil)
+			end
+		end)
+	)
 end
 
 return installed
