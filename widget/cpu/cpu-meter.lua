251e7eeaf6144970307bf834014fc535e8e04d6f
aarondill <aaronsacks2006@gmail.com>
2023-05-21 00:21:36 -0500

    rewrite cpu-meter to work in more cases
    Former-commit-id: 55e93b3ad8d5ea5c0b2d0fb4f55e293de43bc98a

--- widget/cpu/cpu-meter.lua
+++ widget/cpu/cpu-meter.lua
@@ -1,44 +1,74 @@
 local wibox = require("wibox")
-local mat_list_item = require("widget.material.list-item")
-local mat_slider = require("widget.material.slider")
 local mat_icon = require("widget.material.icon")
 local icons = require("theme.icons")
 local watch = require("awful.widget.watch")
 local dpi = require("beautiful").xresources.apply_dpi
 
-local total_prev = 0
-local idle_prev = 0
+---@class CPUWidgetConfig
+---The number of decimal places to display in the percentage (default: 0).
+---@field precision integer
+---How often to update the widget in seconds (default: 15).
+---@field timeout integer?
+---The program to spawn on click of the widget. If nil, the widget will not be clickable.
+---@field spawn_on_click string?
 
-local slider = wibox.widget({
-	read_only = true,
-	widget = mat_slider,
-})
+---Create a new CPU usage widget
+---@param args CPUWidgetConfig?
+---@return table
+function CPU(args)
+	args = args or {}
+	local cpu_total_prev = 0
+	local idle_prev = 0
 
-watch([[bash -c "cat /proc/stat | grep '^cpu '"]], 1, function(_, stdout)
-	local user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice =
-		stdout:match("(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s")
+	local text_box = wibox.widget({
+		widget = wibox.widget.textbox,
+		text = "default",
+	})
 
-	local total = user + nice + system + idle + iowait + irq + softirq + steal
+	watch([[bash -c "cat /proc/stat | grep '^cpu '"]], args.timeout or 15, function(_, stdout)
+		local rem = stdout:match("cpu%s+(.*)")
+		local user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice
+		-- find values, even if not exist
+		user, rem = rem:match("(%d+)(.*)")
+		nice, rem = rem:match("(%d+)(.*)")
+		system, rem = rem:match("(%d+)(.*)")
+		idle, rem = rem:match("(%d+)(.*)")
+		iowait, rem = rem:match("(%d+)(.*)")
+		irq, rem = rem:match("(%d+)(.*)")
+		softirq, rem = rem:match("(%d+)(.*)")
+		steal, rem = rem:match("(%d+)(.*)")
+		guest, rem = rem:match("(%d+)(.*)")
+		guest_nice, rem = rem:match("(%d+)(.*)")
 
-	local diff_idle = idle - idle_prev
-	local diff_total = total - total_prev
-	local diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10
+		local cpu_total = user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice
 
-	slider:set_value(diff_usage)
+		-- Get the delta between two reads
+		local diff_cpu = cpu_total - cpu_total_prev
+		-- Get the idle time delta
+		local diff_idle = idle - idle_prev
+		-- Calc time spent working
+		local cpu_used = diff_cpu - diff_idle
+		-- Calc percentage
+		local cpu_usage = 100 * cpu_used / diff_cpu
 
-	total_prev = total
-	idle_prev = idle
-	collectgarbage("collect")
-end)
+		-- Round to percentage
+		text_box:set_text(string.format("%." .. (args.precision or 0) .. "f%%", cpu_usage))
 
-local cpu_meter = wibox.widget({
-	wibox.widget({
-		icon = icons.chart,
-		size = dpi(24),
-		widget = mat_icon,
-	}),
-	slider,
-	widget = mat_list_item,
-})
+		cpu_total_prev = cpu_total
+		idle_prev = idle
+		collectgarbage("collect")
+	end)
 
-return cpu_meter
+	local cpu_meter = wibox.widget({
+		wibox.widget({
+			icon = icons.chart,
+			size = dpi(24),
+			widget = mat_icon,
+		}),
+		text_box,
+		widget = wibox.layout.fixed.horizontal,
+	})
+
+	return cpu_meter
+end
+return CPU
