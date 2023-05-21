89eea624aef6b41f17c84a17b03a98e21fa6cf55
aarondill <aaronsacks2006@gmail.com>
2023-05-21 01:10:08 -0500

    add warning to installed util docs about cost
    Former-commit-id: a1c28920b899b7ffc023f0133ad732c89270545f

--- util/installed.lua
+++ util/installed.lua
@@ -5,9 +5,11 @@ local naughty = require("naughty")
 local has_notified = false
 
 ---Check if a program is available and pass it to the callback.
----Uses which to find the executable. Will only when which itself is installed.
+---Uses 'which' to find the executable. Will only when 'which' itself is installed.
 ---@param program string? the program to check. If nil, will not be checked.
 ---@param cb fun(path: string?) run with the path if found, or nil if not found.
+---WARNING: Use sparingly, as this function must call 'which' and wait for it's response.
+---If possible, cache the results and avoid calling it again.
 local function installed(program, cb)
 	awful.spawn.easy_async({ "which", program }, function(stdout, _, exitreason, exitcode)
 		-- If command not found
