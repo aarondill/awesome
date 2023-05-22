6d766278efff56ffd12985ab3073aaa6dca4818a
aarondill <aaronsacks2006@gmail.com>
2023-05-21 20:58:18 -0500

    fix the handle_error function
    Former-commit-id: d03fb08731f6eca3603882262c48389ab2d2f4ce

--- util/handle_error.lua
+++ util/handle_error.lua
@@ -1,14 +1,16 @@
 local naughty = require("naughty")
 ---Run func and notify on error
 ---Not typed because generics can't handle it properly
-local function handle_error(func, ...)
-	local ok, err = pcall(func, ...)
-	if not ok then
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			title = "Something went terribly wrong",
-			text = tostring(err),
-		})
+local function handle_error(func)
+	return function(...)
+		local ok, err = pcall(func, ...)
+		if not ok then
+			naughty.notify({
+				preset = naughty.config.presets.critical,
+				title = "Something went terribly wrong",
+				text = tostring(err),
+			})
+		end
 	end
 end
 
