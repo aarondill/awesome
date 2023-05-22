5ff84be5fa5926fd8419a2987ef795339126d2e7
aarondill <aaronsacks2006@gmail.com>
2023-05-22 02:14:02 -0500

    actually handle the shell paremeter in auto-start

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -4,20 +4,26 @@
 local awful = require("awful")
 local apps = require("configuration.apps")
 
+--- Runs the command "once". If cmd is a table where tbl[2] == true, it will be run in a shell, else it will be exec'ed in the shell
 local function run_once(cmd)
 	local findme = cmd
+	local in_shell = false
 	if type(cmd) == "table" then
 		findme = cmd[1]
+		if #cmd >= 2 then
+			in_shell = (cmd[2] and true) or false
+		end
 	elseif type(cmd) ~= "string" then
 		error("cmd must be a table or a string")
 	end
 	local firstspace = findme:find(" ")
-	if firstspace then
-		findme = findme:sub(0, firstspace - 1)
-		awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || { %s; }", findme, cmd))
+  --stylua: ignore
+	if firstspace then findme = findme:sub(0, firstspace - 1) end
+
+	if in_shell then
+		awful.spawn.with_shell(string.format("pgrep -u $USER -x %s >/dev/null || { %s; }", findme, cmd))
 	else
-		-- best case senario, just one command. Exec directly.
-		awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || exec %s", findme, cmd))
+		awful.spawn.with_shell(string.format("pgrep -u $USER -x %s >/dev/null || exec %s", findme, cmd))
 	end
 end
 
