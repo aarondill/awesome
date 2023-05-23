bacf5653bcb7d59f4ed6bcb5d33a695fe5c36ee1
aarondill <aaronsacks2006@gmail.com>
2023-05-23 09:56:51 -0500

    revert the tasklist object to fix the tasklist (WIP)
    Former-commit-id: c82feb546e5c26e5e61b267d6fbc10ed9b9a63f0

--- widget/task-list.lua
+++ widget/task-list.lua
@@ -177,13 +177,14 @@ local tasklist_buttons = gears.table.join(
 )
 
 local TaskList = function(s)
-	return awful.widget.tasklist({
-		screen = s,
-		filter = awful.widget.tasklist.filter.currenttags,
-		buttons = tasklist_buttons,
-		update_function = list_update,
-		base_widget = wibox.layout.fixed.horizontal(),
-	})
+	return awful.widget.tasklist(
+		s,
+		awful.widget.tasklist.filter.currenttags,
+		tasklist_buttons,
+		{},
+		list_update,
+		wibox.layout.fixed.horizontal()
+	)
 end
 
 return TaskList
