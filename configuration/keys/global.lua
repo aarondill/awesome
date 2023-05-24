42e676e84d577e90bb68101e25d935d0bbcbaa50
aarondill <aaronsacks2006@gmail.com>
2023-05-24 08:37:57 -0500

    fix capitalization of show-->Show

--- configuration/keys/global.lua
+++ configuration/keys/global.lua
@@ -10,7 +10,7 @@ local apps = require("configuration.apps")
 local globalKeys = gears.table.join(
 	-- Hotkeys
 	awful.key({ modkey }, "F1", hotkeys_popup.show_help, { description = "Show help", group = "awesome" }),
-	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
+	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "Show help", group = "awesome" }),
 
 	-- Client management
 	awful.key({ modkey }, "j", function()
