66f4c30e4dc2d13d02bb5e072ac60ce49d693162
aarondill <aaronsacks2006@gmail.com>
2023-05-25 19:10:34 -0500

    refactor the update_function to allow text instead of icons

--- widget/tag-list.lua
+++ widget/tag-list.lua
@@ -31,71 +31,80 @@ local function create_buttons(buttons, object)
 	end
 end
 
-local function list_update(w, buttons, label, data, objects)
+--- Update the taglist.
+---@param taglist_widget table the widget.
+---@param buttons table
+---@param label fun(object: table):  text:string, bg: string, bg_image:string, icon:string Function to generate label parameters from an object.
+---@param data table Current data/cache, indexed by objects.
+---@param objects table Objects to be displayed / updated.
+---@param args table? The table arguments
+local function list_update(taglist_widget, buttons, label, data, objects, args)
 	-- update the widgets, creating them if needed
-	w:reset()
-	for i, o in ipairs(objects) do
-		local cache = data[o]
-		local ib, tb, bgb, tbm, ibm, l, bg_clickable
+	taglist_widget:reset()
+	for _, object in ipairs(objects) do
+		local cache = data[object]
+		local imagebox, textbox, background_box, textbox_margin, imagebox_margin, layout, bg_clickable
 		if cache then
-			ib = cache.ib
-			tb = cache.tb
-			bgb = cache.bgb
-			tbm = cache.tbm
-			ibm = cache.ibm
+			imagebox = cache.ib
+			textbox = cache.tb
+			background_box = cache.bgb
+			textbox_margin = cache.tbm
+			imagebox_margin = cache.ibm
 		else
-			ib = wibox.widget.imagebox()
-			tb = wibox.widget.textbox()
-			bgb = wibox.container.background()
-			tbm = wibox.container.margin(tb, dpi(6), dpi(6))
-			ibm = wibox.container.margin(ib, dpi(6), dpi(6), dpi(6), dpi(6))
-			l = wibox.layout.fixed.horizontal()
+			imagebox = wibox.widget.imagebox()
+			textbox = wibox.widget.textbox()
+			background_box = wibox.container.background()
+			textbox_margin = wibox.container.margin(textbox, dpi(6), dpi(6))
+			imagebox_margin = wibox.container.margin(imagebox, dpi(6), dpi(6), dpi(6), dpi(6))
+			layout = wibox.layout.fixed.horizontal()
 			bg_clickable = clickable_container()
 
 			-- All of this is added in a fixed widget
-			l:fill_space(true)
-			l:add(ibm)
+			layout:fill_space(true)
+			layout:add(imagebox_margin)
+			layout:add(textbox_margin)
+
 			-- l:add(tbm)
-			bg_clickable:set_widget(l)
+			bg_clickable:set_widget(layout)
 
 			-- And all of this gets a background
-			bgb:set_widget(bg_clickable)
+			background_box:set_widget(bg_clickable)
 
-			bgb:buttons(create_buttons(buttons, o))
+			background_box:buttons(create_buttons(buttons, object))
 
-			data[o] = {
-				ib = ib,
-				tb = tb,
-				bgb = bgb,
-				tbm = tbm,
-				ibm = ibm,
+			data[object] = {
+				ib = imagebox,
+				tb = textbox,
+				bgb = background_box,
+				tbm = textbox_margin,
+				ibm = imagebox_margin,
 			}
 		end
 
-		local text, bg, bg_image, icon, args = label(o, tb)
-		args = args or {}
+		local text, bg, bg_image, icon, item_args = label(object)
+		item_args = item_args or {}
 
-		-- The text might be invalid, so use pcall.
-		if text == nil or text == "" then
-			tbm:set_margins(0)
+		-- If no text, or icon_only mode don't show the textbox margin
+		if text == nil or text == "" or taglist_widget.icon_only then
+			textbox_margin:set_margins(0)
 		else
-			if not tb:set_markup_silently(text) then
-				tb:set_markup("<i>&lt;Invalid text&gt;</i>")
+			if not textbox:set_markup_silently(text) then
+				textbox:set_markup("<i>&lt;Invalid text&gt;</i>")
 			end
 		end
-		bgb:set_bg(bg)
-		bgb:set_bgimage(bg_image)
+		background_box:set_bg(bg)
+		background_box:set_bgimage(bg_image)
 		if icon then
-			ib.image = icon
+			imagebox.image = icon
 		else
-			ibm:set_margins(0)
+			imagebox_margin:set_margins(0)
 		end
 
-		bgb.shape = args.shape
-		bgb.shape_border_width = args.shape_border_width
-		bgb.shape_border_color = args.shape_border_color
+		background_box.shape = item_args.shape
+		background_box.shape_border_width = item_args.shape_border_width
+		background_box.shape_border_color = item_args.shape_border_color
 
-		w:add(bgb)
+		taglist_widget:add(background_box)
 	end
 end
 
@@ -127,7 +136,6 @@ local TagList = function(s)
 			end)
 		),
 		style = {},
-		-- TODO: Make this show the name instead of (and?) icons
 		update_function = list_update,
 	})
 end
