384e432cffa3e82fedd343dca95e8d055cb145af
aarondill <aaronsacks2006@gmail.com>
2023-05-26 14:10:04 -0500

    testing setup in layouts.lua
    Former-commit-id: cd8880e401f40b5cec89fe84881ed1f2ccbaa2dc [formerly af589b4a584bddf6a32339dcf1e80d71a462829b]
    Former-commit-id: f00c681f4775c06c5bfe991a6e46cb4353d1187c

--- configuration/layouts.lua
+++ configuration/layouts.lua
@@ -6,4 +6,25 @@ local layouts = {
 	awful.layout.suit.magnifier,
 	awful.layout.suit.floating,
 }
+
+--HACK: Show them all!
+-- layouts = {
+-- 	-- awful.layout.suit.corner.nw,
+-- 	-- awful.layout.suit.corner.sw,
+-- 	-- awful.layout.suit.corner.se,
+-- 	-- awful.layout.suit.max,
+-- 	-- awful.layout.suit.max.fullscreen,
+--
+-- 	awful.layout.suit.tile,
+-- 	awful.layout.suit.tile.left,
+-- 	awful.layout.suit.tile.bottom,
+-- 	awful.layout.suit.tile.top,
+-- 	awful.layout.suit.spiral.dwindle,
+-- 	awful.layout.suit.spiral,
+-- 	awful.layout.suit.fair.horizontal,
+-- 	awful.layout.suit.fair,
+-- 	awful.layout.suit.floating,
+-- 	awful.layout.suit.magnifier,
+-- 	awful.layout.suit.corner.ne,
+-- }
 return layouts
