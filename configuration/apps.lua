17952520ef3198a4fd7f37d8f462b38ec2484410
aarondill <aaronsacks2006@gmail.com>
2023-05-26 00:40:48 -0500

    remove --daemon so it will save the PID to be killed
    Former-commit-id: 6c13b166df24adb706f954cfa789d5935d48802a [formerly 8f2fda8069784e933305c181618e974f0c194bba]
    Former-commit-id: 79d5e4c87d0763c6625d5001b7ca96fe4acb0943

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -51,7 +51,7 @@ local run_on_start_up = {
 	"blueman-applet", --bluetooth
 	"pasystray", -- shows an audiocontrol applet in systray when installed.
 	"exec numlockx on", -- enable numlock
-	"exec xfce4-power-manager --daemon", -- Power manager
+	"exec xfce4-power-manager", -- Power manager
 	string.format("sleep 1 && exec udiskie -c '%s/configuration/udiskie.yml'", filesystem.get_configuration_dir()), -- Automount disks.
 	-- Sleep to ensure it's last. My own preference. Feel free to remove it
 	"sleep 1.5 && exec ibus-daemon --xim -rd", -- Run ibus-daemon for language and emoji keyboard support
