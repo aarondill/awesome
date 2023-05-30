60aa1d0e1fcc7b3ae55dec2726ef02f6c1c0d4e2
aarondill <aaronsacks2006@gmail.com>
2023-05-29 22:42:33 -0500

    add startup notifications if apps fail

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -2,26 +2,59 @@
 -- Run all the apps listed in configuration/apps.lua as run_on_start_up only once when awesome start
 
 local awful = require("awful")
+local naughty = require("naughty")
+local beautiful = require("beautiful")
 local apps = require("configuration.apps")
 
 ---@param cmd_str string the thing to run
+---@return integer? pid of the process or nil if error
 local function run_once(cmd_str)
 	-- best case senario, just one command. Run.
-	---@type string|table
-	local cmd = cmd_str
+	---@type table
+	local cmd = { cmd_str }
 	-- Contains space, run in shell just in case
 	if cmd_str:find(" ") then
 		-- run in sh for memory performance
 		cmd = { "sh", "-c", cmd_str }
 	end
 
-	return awful.spawn(cmd, false)
+	local pid = awful.spawn.easy_async(
+		cmd,
+		---@param _ string
+		---@param stderr string
+		---@param exitreason "exit"|"signal"
+		---@param exitcode integer
+		function(_, stderr, exitreason, exitcode)
+			if exitreason == "exit" and exitcode == 0 then
+				return
+			end
+			naughty.notify({
+				presets = naughty.config.presets.warn,
+				icon = beautiful.icon_noti_error,
+				title = string.format('Error while starting "%s".', table.concat(cmd, " ")),
+				text = string.format(
+					'%s: %d.\nStderr: "%s"',
+					exitreason == "exit" and "exit code" or "killed with signal",
+					exitcode,
+					stderr:gsub("\n$", "")
+				),
+				timeout = 0,
+			})
+		end
+	)
+	if type(pid) ~= "number" then
+		return nil
+	end
+	return pid
 end
 
 local processes = {}
 
 for _, app in ipairs(apps.run_on_start_up) do
-	processes[#processes + 1] = run_once(app)
+	local pid = run_once(app)
+	if pid then
+		processes[#processes + 1] = pid
+	end
 end
 -- Kill them all on exit
 awesome.connect_signal("exit", function(_)
