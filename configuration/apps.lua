274abcd2464e96856c0759e178be89c50fb387fb
aarondill <aaronsacks2006@gmail.com>
2023-06-02 00:19:11 -0500

    add dpms on startup

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -51,7 +51,8 @@ local run_on_start_up = {
 	"blueman-applet", --bluetooth
 	"pasystray", -- shows an audiocontrol applet in systray when installed.
 	-- "exec xfce4-power-manager", -- Power manager
-	"exec xset s off off", -- Disable screen saver because it's actually broken
+	"exec xset s 600 0", -- Set screen saver
+	"exec xset -dpms", -- Disable dpms because doesn't work with keys?
 	"exec xss-lock -- lock", -- Lock on suspend or dpms
 	string.format("sleep 1 && exec udiskie -c '%s/configuration/udiskie.yml'", filesystem.get_configuration_dir()), -- Automount disks.
 	-- Sleep to ensure it's last. My own preference. Feel free to remove it
