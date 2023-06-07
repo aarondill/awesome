2b15b6bf2eb3a0f1f04175ef965b0f12f6490ae3
aarondill <aaronsacks2006@gmail.com>
2023-06-07 16:33:05 -0500

    add a timeout for the run prompt
    Former-commit-id: 11e025e35bee45d0edeed17ab2cfe599948b9de3 [formerly c5995279291bea9dc024f9e3088d63872bd11cef]
    Former-commit-id: 5887a041ef06d64be861426aea7535e1ff6e855f

--- widget/run-prompt.lua
+++ widget/run-prompt.lua
@@ -1,18 +1,40 @@
 local awful = require("awful")
 local gears = require("gears")
--- local wibox = require("wibox")
--- local dpi = require("beautiful").xresources.apply_dpi
--- local clickable_container = require("widget.material.clickable-container")
+
+local timer = gears.timer or timer
 
 local function Run_prompt(s)
 	local promptbox = awful.widget.prompt({
 		history_path = gears.filesystem.get_cache_dir() .. "run_prompt_history",
 		prompt = "Run: ",
 	})
+
+	promptbox.timer = timer({
+		timeout = 5,
+		callback = function()
+			promptbox.widget:set_text("")
+			return false
+		end,
+	})
+
+	local old_run = promptbox.run
+	promptbox.run = function(...)
+		if promptbox.timer and promptbox.timer.started then
+			promptbox.timer:stop()
+			require("naughty").notify({
+				text = "stop timer",
+			})
+		end
+		old_run(...)
+	end
 	promptbox.exe_callback = function(cmd)
 		local result = awesome.spawn(cmd, false)
 		if type(result) == "string" then
 			promptbox.widget:set_text(result)
+
+			if promptbox.timer and promptbox.timer.again then
+				promptbox.timer:again()
+			end
 		end
 	end
 	s.run_promptbox = promptbox -- HACK: Attaches to screen object. I don't know how else to do this.
