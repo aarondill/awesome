00df4ea56d58283321caae9b61bfd4151c1ffd37
aarondill <aaronsacks2006@gmail.com>
2023-06-08 11:05:53 -0500

    add util.make_clickable_if_prog

---
+++ util/make_clickable_if_prog.lua
@@ -0,0 +1,26 @@
+-- replace_if_present(cmd, replace_widget, replace_in, function(path, replace_widget, replace_in)
+-- Do something with path (guarenteed to be non-nil)
+-- end)
+---@param cmd string
+---@param replace_widget table
+---@param replace_in table
+---@param cb fun(path:string, replace_widget:table, replace_in:table)
+local function make_clickable_if_prog(cmd, replace_widget, replace_in, cb)
+	local gears = require("gears")
+	local awful = require("awful")
+	local installed = require("util.installed")
+	local replace_in_widget = require("util.replace_in_widget")
+	if not cmd or not replace_widget or not replace_in or not cb then
+		error("Replace_if_present requires 4 arguments")
+	end
+	installed(cmd, function(path_or_nil)
+		if path_or_nil then
+			local clickable = require("widget.material.clickable-container")(replace_widget)
+			clickable:buttons(gears.table.join(awful.button({}, 1, nil, function()
+				return cb(path_or_nil, replace_widget, replace_in)
+			end)))
+			replace_in_widget(replace_in, replace_widget, clickable)
+		end
+	end)
+end
+return make_clickable_if_prog
