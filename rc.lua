0061010236fda558a087609652df7775bb923aa1
aarondill <aaronsacks2006@gmail.com>
2023-06-11 19:31:08 -0500

    reenable setting package.path
    Former-commit-id: 5c74fce540eef1831f2f567f14cb307b6ab2a418 [formerly 94af2a45f8f3eaef727fdec89dde269eecfec8ed]
    Former-commit-id: b8df9eda608ebf047dc41737169e4b97d7ae2720

--- rc.lua
+++ rc.lua
@@ -15,14 +15,13 @@ pcall(require, "luarocks.loader")
 pcall(require, "awful.autofocus") -- Depreciated in V5
 
 -- -- Add configuration directory to package.?path so awesome --config FILE works right
--- local conf_dir = gears.filesystem.get_configuration_dir():sub(1, -2) -- Remove slash
--- package.path = string.format("%s/?.lua;%s/?/init.lua;%s", conf_dir, conf_dir, package.path)
--- package.cpath = string.format("%s/?.so;%s", conf_dir, package.cpath)
+local conf_dir = gears.filesystem.get_configuration_dir():sub(1, -2) -- Remove slash
+package.path = string.format("%s/?.lua;%s/?/init.lua;%s", conf_dir, conf_dir, package.path)
+package.cpath = string.format("%s/?.so;%s", conf_dir, package.cpath)
 
 -- Theme
 beautiful.init(require("theme"))
 require("module.notifications")
-require("naughty").notify({ text = package.path, timeout = 0 })
 
 require("module.git-submodule")
 
