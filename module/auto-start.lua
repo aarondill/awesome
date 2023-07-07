614b26e2c0406ce30d2860129fb95c2792a6db96
aarondill <aaronsacks2006@gmail.com>
2023-07-07 00:03:10 -0500

    add logs to auto-start module
    commands that fail *very* quickly might not be able to be logged. Gotta love race conditions.
    
    
    Former-commit-id: 81ca02da473b5c91046e4fd8f7fd25f0659576a8 [formerly 33471e57ec158c99f566ba8232a10166b3c8c4da]
    Former-commit-id: cdb31dd3a7deda3c99a6b6eabf4ef74c1cb29a3f

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -3,9 +3,15 @@
 local DEBUG = require("configuration").DEBUG
 
 local awful = require("awful")
+local gears = require("gears")
 local naughty = require("naughty")
 local beautiful = require("beautiful")
 local apps = require("configuration.apps")
+
+--- Directory for logging failed(?) application's output
+local log_dir = gears.filesystem.get_cache_dir() .. "auto-start/"
+--- A map of cmds to pids
+local processes = {}
 
 local function err(cmd, e)
 	naughty.notify({
@@ -17,28 +23,35 @@ local function err(cmd, e)
 	})
 end
 
----@param cmd_str string|string[] the thing to run
+--- Replace a file content or create a new one - Async :)
+---@param path string file path to write to
+---@param content string content to write to the file
+---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
+local function file_write(path, content)
+	local gio = require("lgi").Gio
+	gio.File.new_for_path(path):replace_contents_async(content, nil, function(file, task)
+		file:replace_contents_finish(task)
+	end, 0)
+end
+
+---@param cmd string|string[] the thing to run
 ---@return integer? pid of the process or nil if error
-local function run_once(cmd_str)
-	if not cmd_str then
+local function run_once(cmd)
+	if not cmd then
 		return nil
 	end
 	---@type string|string[]
-	local cmd
-	-- Safely exec because no space
-	if type(cmd_str) == "string" or type(cmd_str) == "table" then
-		cmd = cmd_str
-	else
+	if not type(cmd) == "string" and not type(cmd) == "table" then
 		error("Startup apps must be string or table")
 	end
 
 	local pid = awful.spawn.easy_async(
 		cmd,
-		---@param _ string
+		---@param stdout string
 		---@param stderr string
 		---@param exitreason "exit"|"signal"
 		---@param exitcode integer
-		function(_, stderr, exitreason, exitcode)
+		function(stdout, stderr, exitreason, exitcode)
 			if exitreason == "exit" and exitcode == 0 then
 				return
 			end
@@ -48,17 +61,37 @@ local function run_once(cmd_str)
 				-- Remove this to send notifications for missing commands.
 				return nil
 			end
+
+			--- The pid of the process. Don't assume this exists. Race Conditions.
+			local pid = processes[cmd]
+			--- Files to write to. These may be nil.
+			local log_file_stdout, log_file_stderr
+			if pid then
+				log_file_stdout = log_dir .. "/" .. pid .. "-stdout.log"
+				log_file_stderr = log_dir .. "/" .. pid .. "-stderr.log"
+			end
+			if log_file_stdout and log_file_stderr then
+				-- Ensure it exists!
+				gears.filesystem.make_parent_directories(log_file_stdout)
+				gears.filesystem.make_parent_directories(log_file_stderr)
+				-- *Async* write.
+				file_write(log_file_stdout, stdout)
+				file_write(log_file_stderr, stderr)
+			end
+
 			local text = ""
 			if exitreason == "signal" then
 				if exitcode == awesome.unix_signal.SIGSEGV then -- Segfault
-					text = string.format("Segfaulted!" .. "\n" .. "Stderr: %s", stderr)
+					text = "Segfaulted!"
 				else
 					local signame = tostring(awesome.unix_signal[exitcode])
 					text = string.format("killed with signal: %d (%s)", tostring(exitcode), signame)
 				end
 			else
-				local no_end_nl = stderr:gsub("\n$", "")
-				text = string.format("exit code: %d" .. "\n" .. "Stderr: %s", exitcode, no_end_nl)
+				text = string.format("exit code: %d", exitcode)
+			end
+			if log_file_stdout and log_file_stderr then
+				text = text .. "\n" .. ("Logs are available at: %s and %s"):format(log_file_stdout, log_file_stderr)
 			end
 			err(cmd, text)
 		end
@@ -72,17 +105,15 @@ local function run_once(cmd_str)
 	return nil
 end
 
-local processes = {}
-
 for _, app in ipairs(apps.run_on_start_up) do
 	local pid = run_once(app)
 	if pid then
-		processes[#processes + 1] = pid
+		processes[app] = pid
 	end
 end
 -- Kill them all on exit
 awesome.connect_signal("exit", function(_)
-	for _, pid in ipairs(processes) do
+	for _, pid in pairs(processes) do
 		-- killing -p means sending a signal to every process in the process group p. Awesome makes sure to spawn processes in a new session, so this works.
 		local suc = awesome.kill(-pid, 15) -- SIGTERM
 		if not suc then
