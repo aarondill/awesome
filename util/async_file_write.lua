55d5adc8a53feb7bc65064cd66560e6614bf9db2
aarondill <aaronsacks2006@gmail.com>
2023-07-11 02:54:12 -0500

    I hate memory management in garbage collected languages.
    The `replace_contents_async` function doesn't copy the `content` variable.
    This variable then gets garbage disposed before the function accesses it, resulting in undefined behavior.
    In practice, this wrote random data to the file, which typically contained shell command errors (idk why).
    
    The fix is to create a module scoped table that I can store the values into, which prevents them from being garbage collected.
    The content is used as the key, as it should (hopefully) be unique. Perhaps this should be changed to reduce collisions.
    As the content is therefore *not* garbage collected, it can then be used to index into the array and unset the value.
    
    
    Former-commit-id: ab83ce18a90ab9d668b11ba412dd9f4889e2d9a5 [formerly 9e0586fabca51118d7b95a6a5a8c23c122753e54]
    Former-commit-id: f64938baac06bc1b29ca1d5795ee376a1e00a24a

--- util/async_file_write.lua
+++ util/async_file_write.lua
@@ -1,12 +1,29 @@
+local gio = require("lgi").Gio
+
+---This array is in module scope, allowing values to be placed inside of it, without worrying they will be garbage collected
+---Be *CERTAIN* to clear the values when you are done with them. This can and will being a memory leak otherwise.
+---@type any[]
+local SAVE_FROM_GARBAGE_COLLECTION = {}
+--- Used to disable lua-language-server warning about unused variables
+SAVE_FROM_GARBAGE_COLLECTION = SAVE_FROM_GARBAGE_COLLECTION
+
 --- Replace a file content or create a new one - Async :)
 ---@param path string file path to write to
 ---@param content string content to write to the file
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_write(path, content)
-	local gio = require("lgi").Gio
-	gio.File.new_for_path(path):replace_contents_async(content, nil, function(file, task)
+	--- Store the content in the global array
+	SAVE_FROM_GARBAGE_COLLECTION[content] = true
+
+	---params(replace_contents_async): string contents, string etag, boolean make_backup, GFileCreateFlags flags, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
+	--- NOTE: This function does not copy `content`, so we must protect it from being garbage-collected (resulting in garbage being written to disk)
+	gio.File.new_for_path(path):replace_contents_async(content, nil, false, 0, nil, function(file, task)
+		--- Finish the write operation and close the file(?)
 		file:replace_contents_finish(task)
-	end, 0)
+
+		--- Clear the content to allow garbage collection - Avoid a memory leak
+		SAVE_FROM_GARBAGE_COLLECTION[content] = nil
+	end)
 end
 
 return file_write
