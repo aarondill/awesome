e877527c97b383293213e2bd7e53d3d3f785f07f
aarondill <aaronsacks2006@gmail.com>
2023-08-10 22:58:14 -0500

    fix spawnoptions fields docs

--- util/spawn.lua
+++ util/spawn.lua
@@ -2,24 +2,31 @@ local capi = { awesome = awesome }
 local awful_spawn = require("awful.spawn")
 
 ---@class SpawnOptions
----@field start_callback? fun(client: table) The callback to call when the application starts.
+---The callback to call when the application starts.
 ---This is passed the client object (https://awesomewm.org/doc/api/classes/client.html)
--- Note: this only works if the application implements startup notifications and sn_rules are enabled
----@field exit_callback? fun()
+---Note: this only works if the application implements startup notifications and sn_rules are enabled
+---@field start_callback? fun(client: table)
 ---The callback to call when the application exits.
----@field sn_rules? table|boolean The rules to apply to the window when the application starts.
+---code is the exit status or the signal that killed the application depending on reason
+---@field exit_callback? fun(reason: "exit"|"signal", code: integer)
+---The rules to apply to the window when the application starts.
 ---These are enabled by default. Pass false to disable startup notification detection.
 ---Note: this only works if the application implements startup notifications
----@field inherit_stdin? boolean Whether the command should inherit the stdin of awesome.
+---@field sn_rules? table|boolean
+---Whether the command should inherit the stdin of awesome.
 ---If true, stdin_fd will be returned, else stdin_fd will be nil
 ---This defaults to false to prevent it from taking control of the underlying terminal
----@field inherit_stdout? boolean Whether the command should inherit the stdout of awesome.
+---@field inherit_stdin? boolean
+---Whether the command should inherit the stdout of awesome.
 ---If true, stdout_fd will be returned, else stdout_fd will be nil
----@field inherit_stderr? boolean Whether the command should inherit the stderr of awesome.
+---@field inherit_stdout? boolean
+---Whether the command should inherit the stderr of awesome.
 ---If true, stderr_fd will be returned, else stderr_fd will be nil
----@field env? table|false A table of environment variables to apply to the command.
+---@field inherit_stderr? boolean
+---A table of environment variables to apply to the command.
 ---If false, the command will be started with an empty environment
 ---If nil, the command will inherit awesome's environment
+---@field env? table|false
 
 ---A wrapper around awesome.spawn
 ---This can be used as a drop in replacement for awful.spawn, when only the command is passed, otherwise:
@@ -27,21 +34,24 @@ local awful_spawn = require("awful.spawn")
 ---awful.spawn(command, sn_rules, callback) == spawn(command, { sn_rules = sn_rules, start_callback = callback })
 ---````
 ---If there was an error spawning the command, the first returned value will be the error message (a string) and all other values will be nil
+---
 ---@param cmd string|string[]
 ---@param opts SpawnOptions?
 ---@return integer|string pid_or_error the pid of the process if the process was started successfully, else a string error message
 ---@return string? snid
 ---@return integer? stdin_fd the file descriptor of the stdin of the command. This is only returned if the inherit_stdin is false (note: inherit_stdin defaults to false)
----@return integer? stdout_fd the file descriptor of the stdout of the command. This is only returned if inherit_stdout is false
+---the file descriptor of the stdout of the command. This is only returned if inherit_stdout is false
 ---This can be read using awful.spawn.read_lines, example from spawn.lua:
 ---```lua
 ---awful.spawn.read_lines(Gio.UnixInputStream.new(stdout_fd, true), stdout_callback, stdout_done_callback, true)
 ---```
----@return string? stderr_fd the file descriptor of the stderr of the command. This is only returned if inherit_stderr is false
+---@return integer? stdout_fd
+---the file descriptor of the stderr of the command. This is only returned if inherit_stderr is false
 ---This can be read using awful.spawn.read_lines, example from spawn.lua:
 ---```lua
 ---awful.spawn.read_lines(Gio.UnixInputStream.new(stderr_fd, true), stderr_callback, stderr_done_callback, true)
 ---```
+---@return string? stderr_fd
 ---
 ---Note: start_callback only works when opts.sn_rules is given
 ---@source Modified from /usr/share/awesome/lib/awful/spawn.lua
