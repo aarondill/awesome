efe86ae6f228f99ae97608a5a7d2c8f1c7093727
aarondill <aaronsacks2006@gmail.com>
2023-08-11 01:01:23 -0500

    move rofi_command to it's own file

--- configuration/apps/default.lua
+++ configuration/apps/default.lua
@@ -1,26 +1,4 @@
-local filesystem = require("gears.filesystem")
-
-local function rofi_command(...)
-  -- Thanks to jo148 on github for making rofi dpi aware!
-  local xres = require("beautiful").xresources
-  local args = { ... }
-  -- Ends in -show to pick default, but can be overridden by appending a mode
-  local cmd = {
-    "rofi",
-    "-dpi",
-    tostring(xres.get_dpi()),
-    "-width",
-    tostring(xres.apply_dpi(400)),
-    "-theme",
-    filesystem.get_configuration_dir() .. "configuration/rofi.rasi",
-    "-show",
-  }
-  for _, v in ipairs(args) do
-    table.insert(cmd, v)
-  end
-  return cmd
-end
-
+local rofi_command = require("configuration.apps.rofi_command")
 local terminal = "wezterm"
 
 -- List of apps to start by default on some actions - Don't use shell features.
---
+++ configuration/apps/rofi_command.lua
@@ -0,0 +1,23 @@
+local filesystem = require("gears.filesystem")
+
+local function rofi_command(...)
+  -- Thanks to jo148 on github for making rofi dpi aware!
+  local xres = require("beautiful").xresources
+  local args = { ... }
+  -- Ends in -show to pick default, but can be overridden by appending a mode
+  local cmd = {
+    "rofi",
+    "-dpi",
+    tostring(xres.get_dpi()),
+    "-width",
+    tostring(xres.apply_dpi(400)),
+    "-theme",
+    filesystem.get_configuration_dir() .. "configuration/rofi.rasi",
+    "-show",
+  }
+  for _, v in ipairs(args) do
+    table.insert(cmd, v)
+  end
+  return cmd
+end
+return rofi_command
