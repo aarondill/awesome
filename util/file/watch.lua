55df3dd2f5b6cf8a915f4c098f1fa617c17e037b
aarondill <aaronsacks2006@gmail.com>
2023-08-13 05:08:01 -0500

    fix some things in file/watch.lua

--- util/file/watch.lua
+++ util/file/watch.lua
@@ -1,14 +1,17 @@
 local gio = require("lgi").Gio
+local garbage_collection = require("util.garbage_collection")
+local handle_error = require("util.handle_error")
 ---@alias GioFileMonitorMethod "monitor" | "monitor_directory" | "monitor_file"
 ---@alias GioFileMonitorFlags "NONE" | "WATCH_HARD_LINKS" | "SEND_MOVED" | "WATCH_MOVES" | "WATCH_MOUNTS"
 ---@alias GioFileMonitorEvent "CHANGED" | "CHANGES_DONE_HINT" | "DELETED" | "CREATED" | "ATTRIBUTE_CHANGED" | "PRE_UNMOUNT" | "UNMOUNTED" | "MOVED" | "RENAMED" | "MOVED_IN" | "MOVED_OUT"
+---@alias GioFileWatcherHandler fun(type: GioFileMonitorEvent, path1: string, path2: string?)
 
 ---A common interface for implementing file watch functions
 ---@param path string the file path to watch
 ---@param method GioFileMonitorMethod the file watch method to call (monitor_*)
 ---See https://docs.gtk.org/gio/flags.FileMonitorFlags.html
 ---@param flags GioFileMonitorFlags[]? the flags to pass to the file watch method. nil corresponds to G_FILE_MONITOR_NONE
----@param cb fun(type: GioFileMonitorEvent, path1: string, path2: string?)
+---@param cb GioFileWatcherHandler
 local function watch_common(path, method, flags, cb)
   if type(cb) ~= "function" then error("callback must be a function", 2) end
   if type(method) ~= "string" then error("method must be a string", 2) end
@@ -21,22 +24,20 @@ local function watch_common(path, method, flags, cb)
   if flags then
     for _, flag in ipairs(flags) do
       if gio.FileMonitorFlags[flag] then
-        flag_int = flag_int & gio.FileMonitorFlags[flag] -- bitwise AND
+        flag_int = flag_int | gio.FileMonitorFlags[flag] -- bitwise AND
       end
     end
   end
 
-  file[method](file, flag_int).on_changed:connect(function(_, file1, file2, type)
+  local monitor = file[method](file, flag_int)
+  garbage_collection.save(monitor)
+  ---@param type GioFileMonitorEvent can't guarentee, but best I can do
+  monitor.on_changed:connect(handle_error(function(_, file1, file2, type)
     local path1 = file1:get_path() ---@type string
     ---Only if using the deprecated flag G_FILE_MONITOR_SEND_MOVED flag and event_type is G_FILE_MONITOR_EVENT_MOVED
     local path2 = file2 and file2:get_path() ---@type string?
-    for k, v in pairs(gio.FileMonitorEvent) do -- Reverse lookup -- given flag value, return key
-      ---@cast k GioFileMonitorEvent can't guarentee, but best I can do
-      if type == v then
-        return cb(k, path1, path2) ---path2 is likely nil
-      end
-    end
-  end)
+    return cb(type, path1, path2) ---path2 is likely nil
+  end))
 end
 
 return watch_common
