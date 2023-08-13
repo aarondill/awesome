510d1392351c3900871e16609d6252e3907e0c80
aarondill <aaronsacks2006@gmail.com>
2023-08-13 07:29:52 -0500

    make battery widget more generic

--- widget/battery/files.lua
+++ widget/battery/files.lua
@@ -6,20 +6,34 @@ local PATH_TO_ICONS = gfilesystem.get_configuration_dir() .. "widget/battery/ico
 ---@field status string?
 ---@field percentage number?
 
+---@alias battery_files  "capacity" | "status"
+---@type table<battery_files, {path:string, match:string}>
+local battery_files = {
+  capacity = { path = "/capacity", match = "(%d+)\n" },
+  status = { path = "/status", match = "(.+)\n" },
+}
+---Get the battery stat from battery_files
+---@param battery_path string path to the sysfs folder
+---@param stat string battery_files[stat]
+---@param ret table a table to store the result in. It will be stored in ret[stat]
+---@param cb any
+---@param cb_args any
+local function get_battery_stat(battery_path, stat, ret, cb, cb_args)
+  local battery_file = battery_files[stat]
+  read_async(battery_path .. battery_file.path, function(content)
+    ret.capacity = content:match(battery_file.match)
+    cb(table.unpack(cb_args))
+  end)
+end
 ---returns battery information
 ---@param battery_path string the path to the battery directory (sysfs)
 ---@param callback_fn fun(info: battery_info) the function to call when the data has been retrieved
 local function get_battery_info(battery_path, callback_fn)
   local ret = {}
-  local function get_capacity(stdout)
-    ret.capacity = stdout:match("(%d+)\n")
-    callback_fn(ret)
-  end
-  local function get_status(stdout)
-    ret.status = stdout:match("(.+)\n")
-    read_async(battery_path .. "/capacity", get_capacity)
-  end
-  read_async(battery_path .. "/status", get_status)
+  local callback_fn_args = { ret }
+  local get_status_args = { battery_path, "status", ret, callback_fn, callback_fn_args }
+  local get_capacity_args = { battery_path, "capacity", ret, get_battery_stat, get_status_args }
+  get_battery_stat(table.unpack(get_capacity_args))
 end
 
 ---Find the first battery in the sysfs
