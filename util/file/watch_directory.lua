a99a506cba05f90eb7d1577d2aed7229a1d212c0
aarondill <aaronsacks2006@gmail.com>
2023-08-13 05:08:11 -0500

    pass optional flags in watch_*

--- util/file/watch_directory.lua
+++ util/file/watch_directory.lua
@@ -1,7 +1,11 @@
 local watch = require("util.file.watch")
-local function watch_directory(path, cb)
+---@param path string the file path to watch
+---See https://docs.gtk.org/gio/flags.FileMonitorFlags.html
+---@param flags GioFileMonitorFlags[]? the flags to pass to the file watch method. nil corresponds to G_FILE_MONITOR_NONE
+---@param cb GioFileWatcherHandler the function to call when the directory changes
+local function watch_directory(path, flags, cb)
   if type(cb) ~= "function" then error("callback must be a function", 2) end
   if type(path) ~= "string" then error("path must be a string", 2) end
-  watch(path, "monitor_directory", nil, cb)
+  watch(path, "monitor_directory", flags, cb)
 end
 return watch_directory
--- util/file/watch_file.lua
+++ util/file/watch_file.lua
@@ -1,7 +1,11 @@
 local watch = require("util.file.watch")
-local function watch_file(path, cb)
+---@param path string the file path to watch
+---See https://docs.gtk.org/gio/flags.FileMonitorFlags.html
+---@param flags GioFileMonitorFlags[]? the flags to pass to the file watch method. nil corresponds to G_FILE_MONITOR_NONE
+---@param cb GioFileWatcherHandler the function to call when the file changes
+local function watch_file(path, flags, cb)
   if type(cb) ~= "function" then error("callback must be a function", 2) end
   if type(path) ~= "string" then error("path must be a string", 2) end
-  watch(path, "monitor_file", nil, cb)
+  watch(path, "monitor_file", flags, cb)
 end
 return watch_file
