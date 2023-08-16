196d316464bfd07572b33a600106d7357a355723
aarondill <aaronsacks2006@gmail.com>
2023-08-16 10:51:32 -0500

    handle error should handle nil returns

--- util/handle_error.lua
+++ util/handle_error.lua
@@ -1,7 +1,7 @@
 local notifs = require("util.notifs")
 ---@generic T :function
 ---Run func and notify on error
----@param func T the funcitno to call
+---@param func T the function to call
 ---@param on_error? any if type is function, it will be called with the error as the only parameter. Else, it will be returned in the case of an error.
 ---@return T safe_cb Note: the return types are now different, but lua_ls can't handle the generics neccisary to describe this.
 local function handle_error(func, on_error)
@@ -10,14 +10,18 @@ local function handle_error(func, on_error)
   local on_err_cb = type(on_error) == "function" and handle_error(on_error) or on_error
 
   return function(...)
-    local ok, val_or_err = pcall(func, ...)
-    if ok then return val_or_err end
+    local res = table.pack(pcall(func, ...))
+    local ok = res[1]
+    if ok then
+      return select(2, table.unpack(res)) -- Remove the ok boolean
+    end
 
-    notifs.critical(tostring(val_or_err), {
+    local err = res[2]
+    notifs.critical(tostring(err), {
       title = "Something went terribly wrong",
     })
     if type(on_err_cb) == "function" then -- Allow returning nil
-      return on_err_cb(val_or_err)
+      return on_err_cb(err)
     else
       return on_err_cb
     end
