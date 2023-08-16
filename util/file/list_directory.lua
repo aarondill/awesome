c8f467408b6b0fa4539af00302f1fb69330c9bd3
aarondill <aaronsacks2006@gmail.com>
2023-08-16 07:39:56 -0500

    improve typing of list_directory
    the args are now optional

--- util/file/list_directory.lua
+++ util/file/list_directory.lua
@@ -4,13 +4,20 @@ local scan_directory = require("util.file.scan_directory")
 
 --- Return a file list (name only)
 ---@param path string
----@param args list_directory_args
+---@param args list_directory_args?
 ---@param cb fun(names: string[])
+---@overload fun(path: string, cb: fun(names: string[]))
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function list_directory(path, args, cb)
   if not path then return end
-  if not cb then error("callback is required") end
+  if type(args) == "function" and cb == nil then
+    cb, args = args, nil
+  end
   args = args or {}
+  assert(type(args) == "table", "args must be a table")
+  if args.match then assert(type(args.match) == "string", "args.match must be a string") end
+  assert(type(path) == "string", "path must be a string")
+  assert(type(cb) == "function", "callback must be a function")
   scan_directory(path, { attributes = { "FILE_ATTRIBUTE_STANDARD_NAME" } }, function(contents)
     local ret = {}
     for _, v in ipairs(contents) do
