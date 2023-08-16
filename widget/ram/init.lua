2d29617c212a77bf9bfc2795449c610e1130d268
aarondill <aaronsacks2006@gmail.com>
2023-08-16 13:05:59 -0500

    fixup parts of ram

--- widget/ram/init.lua
+++ widget/ram/init.lua
@@ -1,12 +1,11 @@
 local icons = require("theme.icons")
 local mat_icon = require("widget.material.icon")
-local mat_list_item = require("widget.material.list-item")
 local mat_slider = require("widget.material.slider")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
 local awful = require("awful")
-local bind = require("util.bind")
 local gears = require("gears")
+local handle_error = require("util.handle_error")
 
 local slider = wibox.widget({
   read_only = true,
@@ -14,30 +13,32 @@ local slider = wibox.widget({
 })
 
 local function free_handler(stdout)
-  local mem_line = stdout:match("^Mem: [^\n]*")
+  local mem_line = stdout:match("Mem: [^\n]*")
   -- local total, used, free, shared, buff_cache, available =
-  local total, used = mem_line:match("^Mem:%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)")
-  -- local swap_line = stdout:match("^Swap: [^\n]*")
+  local total, _, _, _, _, available = mem_line:match("^Mem:%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)")
+  -- local swap_line = stdout:match("Swap: [^\n]*")
   -- local total_swap, used_swap, free_swap = swap_line:match("^Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
 
-  slider:set_value(used / total * 100)
+  slider:set_value(math.floor((total - available) / total * 100))
 end
 
 local timer = gears.timer.new({
   timeout = 1,
   call_now = true,
-  callback = bind(awful.spawn.easy_async, "free", free_handler),
+  callback = function()
+    awful.spawn.easy_async("free", handle_error(free_handler))
+  end,
 })
 timer:start()
 
 local ram_meter = wibox.widget({
-  wibox.widget({
+  {
     icon = icons.memory,
     size = dpi(24),
     widget = mat_icon,
-  }),
+  },
   slider,
-  widget = mat_list_item,
+  widget = wibox.layout.fixed.horizontal,
+  forced_width = dpi(20),
 })
-
 return ram_meter
