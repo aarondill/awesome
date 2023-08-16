207490d323f77f6b344c8687fd431c95a250c6b8
aarondill <aaronsacks2006@gmail.com>
2023-08-16 10:04:11 -0500

    read_async can pass an error

--- util/file/read_async.lua
+++ util/file/read_async.lua
@@ -2,7 +2,7 @@ local gio = require("lgi").Gio
 
 --- Replace a file content or create a new one - Async :)
 ---@param path string file path to write to
----@param cb fun(content?: string) function to call when done. content will be nil if the file does not exist
+---@param cb fun(content?: string, error?: userdata) function to call when done. content will be nil if the file does not exist
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_read(path, cb)
   if type(path) ~= "string" then error("path must be a string", 2) end
@@ -10,8 +10,12 @@ local function file_read(path, cb)
 
   ---params(load_contents_async) GFile* file, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
   gio.File.new_for_path(path):load_contents_async(nil, function(file, task)
-    local content = file:load_contents_finish(task)
-    cb(content)
+    local content, error = file:load_contents_finish(task)
+    if not content then
+      cb(nil, error)
+    else
+      cb(content, nil)
+    end
   end)
 end
 
