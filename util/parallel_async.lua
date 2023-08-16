b28324240e3be276f43df848a29f81e08bcb0f72
aarondill <aaronsacks2006@gmail.com>
2023-08-16 08:54:10 -0500

    create a parallel_async util to run a function over each value in a table and call a function with all of the results

--- util/parallel_async.lua
+++ util/parallel_async.lua
@@ -1,27 +1,22 @@
 local gears = require("gears")
----@generic T, R
----@alias each T
----@alias for_each T[]
----@alias each_cb fun(info: T, ...: any): R
----@alias done_cb fun(info: R[], ...: any)
-
 ---Runs each_cb on each of for_each in parellel and calls done_cb when all are done
----@param val each
+---@generic T, R
+---@param val T
 ---@param ret table a table to store the result in. It will be stored in ret[stat]
 ---@param done_tbl boolean[] A table to store true in when done
 ---@param index integer
----@param each_cb each_cb
----@param done_cb done_cb
+---@param each_cb fun(val: T, cb: fun(res: R), ...: any) the function to call with each piece of information. The return value is kept
+---@param done_cb fun(res: R[], ...: any) the function to call when the data has been retrieved
 ---@param ... any passed to each_cb and done_cb functions after all other arguments
 local function run_callbacks_in_parellel(done_tbl, index, ret, each_cb, done_cb, val, ...)
-  local function not_val(v)
-    return not v
+  local function is_false(v)
+    return v == false
   end
-  each_cb(function(...)
-    ret[val] = { ... }
+  each_cb(val, function(res)
+    ret[val] = res
     done_tbl[index] = true
-    local first_false = gears.table.find_first_key(done_tbl, not_val, false)
-    local is_done = first_false ~= nil
+    local first_false = gears.table.find_first_key(done_tbl, is_false, false)
+    local is_done = first_false == nil -- All are true
     if is_done then
       done_tbl[index] = false -- Reduce the chance of race conditions, subsequent calculations will return false
       return done_cb(ret)
@@ -30,9 +25,12 @@ local function run_callbacks_in_parellel(done_tbl, index, ret, each_cb, done_cb,
 end
 
 ---Runs each_cb on each of for_each in parellel and calls done_cb when all are done
----@param for_each for_each
----@param each_cb each_cb the function to call with each piece of information. The return value is kept
----@param done_cb done_cb the function to call when the data has been retrieved
+---@generic T, R
+---@param for_each T[]
+---@param each_cb fun(val: T, cb: fun(res: R), ...: any) the function to call with each piece of information. The return value is kept
+---@param done_cb fun(res: R[], ...: any) the function to call when the data has been retrieved
+---The done_cb will be called with a table containing the results of all the callbacks
+---with each ret[key] being the result of each_cb(for_each[key], function(res) end)
 ---@param ... any passed to each_cb and done_cb functions after all other arguments
 ---@return nil
 local function parallel_async(for_each, each_cb, done_cb, ...)
@@ -48,5 +46,4 @@ local function parallel_async(for_each, each_cb, done_cb, ...)
   end
 end
 
---- The done_cb will be called with a table containing the results of all the callbacks, with each ret[key] being the result of each_cb(for_each[key])
 return parallel_async
