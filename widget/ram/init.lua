bf123ab70b84a966d40aa5230234bae1163a3b7c
aarondill <aaronsacks2006@gmail.com>
2023-08-16 12:48:58 -0500

    rewrite widget/ram
    This still uses `free` though

--- widget/ram/init.lua
+++ widget/ram/init.lua
@@ -2,21 +2,33 @@ local icons = require("theme.icons")
 local mat_icon = require("widget.material.icon")
 local mat_list_item = require("widget.material.list-item")
 local mat_slider = require("widget.material.slider")
-local watch = require("awful.widget.watch")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
+local awful = require("awful")
+local bind = require("util.bind")
+local gears = require("gears")
 
 local slider = wibox.widget({
   read_only = true,
   widget = mat_slider,
 })
 
-watch('bash -c "free | grep -z Mem.*Swap.*"', 1, function(_, stdout)
-  local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap =
-    stdout:match("(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
+local function free_handler(stdout)
+  local mem_line = stdout:match("^Mem: [^\n]*")
+  -- local total, used, free, shared, buff_cache, available =
+  local total, used = mem_line:match("^Mem:%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)")
+  -- local swap_line = stdout:match("^Swap: [^\n]*")
+  -- local total_swap, used_swap, free_swap = swap_line:match("^Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
+
   slider:set_value(used / total * 100)
-  collectgarbage("collect")
-end)
+end
+
+local timer = gears.timer.new({
+  timeout = 1,
+  call_now = true,
+  callback = bind(awful.spawn.easy_async, "free", free_handler),
+})
+timer:start()
 
 local ram_meter = wibox.widget({
   wibox.widget({
