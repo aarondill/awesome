855b39c6e02e1793700b4a0dff630737157030ad
aarondill <aaronsacks2006@gmail.com>
2023-08-19 03:27:02 -0500

    fix package_path util

--- util/package_path.lua
+++ util/package_path.lua
@@ -1,20 +1,38 @@
 local M = {}
+---@param path string
+---@return string
+local function cleanup_path(path)
+  ---@type string
+  local res = path
+  res = res:gsub("/%./", "/") -- no /./
+  res = res:gsub("^(.+)/$", "%1") -- no end slash -- use .+ instead of .* to keep '/' instead of ''
+  res = res:gsub("//+", "/") -- no double slashes
+  local count = -1
+  while count ~= 0 do
+    res, count = res:gsub("^/%.%./", "/") -- remove /../ at root level
+  end
+  return res
+end
 ---Whether package.path contains `dir`
 ---@param dir string
 ---@return boolean
 ---@nodiscard
 function M.path_contains(dir)
-  local lua = dir .. "/?.lua;"
-  local init = dir .. "/?/init.lua;"
-  return not string.find(package.path, lua, 1, true) and not string.find(package.path, init, 1, true)
+  dir = cleanup_path(assert(dir))
+  local lua = ";" .. dir .. "/?.lua;"
+  local init = ";" .. dir .. "/?/init.lua;"
+  local path_cmp = ";" .. package.path .. ";" -- package.path may not end in a semicolon. Likely won't start with one.
+  return not not (string.find(path_cmp, lua, 1, true) and string.find(path_cmp, init, 1, true))
 end
 ---Whether package.cpath contains `dir`
 ---@param dir string
 ---@return boolean
 ---@nodiscard
 function M.cpath_contains(dir)
-  local so = dir .. "/?.so;"
-  return not string.find(package.path, so, 1, true)
+  dir = cleanup_path(assert(dir))
+  local so = ";" .. dir .. "/?.so;"
+  local path_cmp = ";" .. package.cpath .. ";" -- package.cpath may not end in a semicolon. Likely won't start with one.
+  return not not string.find(path_cmp, so, 1, true)
 end
 
 ---A utility for appending to the path and cpath
@@ -24,12 +42,13 @@ end
 ---@param args unknown[] a table of arguments to pass to string.format. This will be modified!
 ---@return string str the new formatted string. The parts will be semicolon delimited.
 ---@nodiscard
+---@implenote prepend refers to the template to-to, no to-to the template
 local function append_or_prepend(prepend, to, format, args)
   assert(type(to) == "string")
   assert(type(format) == "string")
   assert(type(args) == "table")
-  table.insert(args, prepend and 1 or #args, to) -- order
-  format = prepend and ("%s;" .. format) or (format .. ";%s")
+  table.insert(args, prepend and (#args + 1) or 1, to) -- order
+  format = prepend and (format .. ";%s") or ("%s;" .. format)
   return format:format(table.unpack(args))
 end
 
@@ -38,6 +57,7 @@ end
 ---@param prepend boolean? default true
 ---@return string cpath the new cpath (for convenience)
 function M.add_to_cpath(dir, prepend)
+  dir = cleanup_path(assert(dir))
   assert(type(dir) == "string")
   prepend = prepend == nil and true or not not prepend
   if M.cpath_contains(dir) then return package.cpath end
@@ -50,6 +70,7 @@ end
 ---@param prepend boolean? default true
 ---@return string path the new path (for convenience)
 function M.add_to_path(dir, prepend)
+  dir = cleanup_path(assert(dir))
   assert(type(dir) == "string")
   prepend = prepend == nil and true or not not prepend
   if M.path_contains(dir) then return package.path end
