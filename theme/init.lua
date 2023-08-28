a7fe8c429cc9ab2f1b5ff5ca4ff0379fd915c8d2
aarondill <aaronsacks2006@gmail.com>
2023-08-28 08:16:55 -0500

    clean up themes better

--- theme/init.lua
+++ theme/init.lua
@@ -1,11 +1,9 @@
 local default_theme = require("theme.default-theme")
 local gfile = require("gears.filesystem")
-local gtable = require("gears.table")
-local theme = require("theme.titus-theme")
 local system_default_ok, system_default = pcall(dofile, gfile.get_themes_dir() .. "default/theme.lua")
+local theme_dir = gfile.get_configuration_dir() .. "/theme"
 
-local final_theme = {}
-gtable.crush(final_theme, (system_default_ok and system_default) or {})
-gtable.crush(final_theme, default_theme)
-gtable.crush(final_theme, theme)
+local final_theme = system_default_ok and system_default or {}
+if type(final_theme) ~= "table" then final_theme = {} end
+default_theme(final_theme, theme_dir)
 return final_theme
--- theme/titus-theme.lua
+++ theme/titus-theme.lua
@@ -1,23 +1,22 @@
-local filesystem = require("gears.filesystem")
 local mat_colors = require("theme.mat-colors")
-local theme_dir = filesystem.get_configuration_dir() .. "/theme"
 
-local theme = {}
-theme.icons = theme_dir .. "/icons/"
-theme.font = "Roboto medium 10"
+local function do_theme(theme, theme_dir)
+  theme.icons = theme_dir .. "/icons/"
+  theme.font = "Roboto medium 10"
 
--- Colors Pallets
+  -- Colors Pallets
 
--- Primary
-theme.primary = mat_colors.indigo
-theme.primary.hue_500 = "#003f6b"
--- Accent
-theme.accent = mat_colors.pink
+  -- Primary
+  theme.primary = mat_colors.indigo
+  theme.primary.hue_500 = "#003f6b"
+  -- Accent
+  theme.accent = mat_colors.pink
 
--- Background
-theme.background = mat_colors.blue_grey
+  -- Background
+  theme.background = mat_colors.blue_grey
 
-theme.background.hue_800 = "#192933"
-theme.background.hue_900 = "#121e25"
+  theme.background.hue_800 = "#192933"
+  theme.background.hue_900 = "#121e25"
+end
 
-return theme
+return do_theme
