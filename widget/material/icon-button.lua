146966e840dae3c0551d0eafa44af21af1cb34bc
aarondill <aaronsacks2006@gmail.com>
2023-09-08 16:32:22 -0500

    icon-button should be usable in a declarative layout

--- widget/material/icon-button.lua
+++ widget/material/icon-button.lua
@@ -1,19 +1,54 @@
 local require = require("util.rel_require")
 
 local clickable_container = require(..., "clickable-container") ---@module "widget.material.clickable-container"
+local gtable = require("gears.table")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
 
-local function build(imagebox)
-  -- return wibox.container.margin(container, 6, 6, 6, 6)
-  return wibox.widget({
+local IconButton = {}
+
+---@param img string the path to the image
+---@return boolean success
+function IconButton:set_image(img)
+  return self:get_children_by_id("iconbox")[1]:set_image(img)
+end
+for _, v in pairs({ "margins", "left", "right", "top", "bottom" }) do
+  for _, t in ipairs({ "set", "get" }) do
+    local method = string.format("%s_%s", t, v)
+    IconButton[method] = function(self, m)
+      local margin = self:get_children_by_id("margin")[1]
+      return margin[method](margin, m)
+    end
+  end
+end
+
+---@param children table[]
+---Note: ensure that the iconbox has an id="iconbox"
+function IconButton:set_children(children) -- allow set own iconbox
+  if #children > 0 then
+    return self:get_children_by_id("margin")[1]:set_children(children)
+  else
+    return false
+  end
+end
+
+local function new(img, margins)
+  local ret = wibox.widget({
     {
-      imagebox,
-      margins = dpi(5),
+      {
+        image = img,
+        id = "iconbox",
+        widget = wibox.widget.imagebox,
+      },
+      id = "margin",
+      margins = margins or dpi(5),
       widget = wibox.container.margin,
     },
     widget = clickable_container,
   })
+  gtable.crush(ret, IconButton, true)
+
+  return ret
 end
 
-return build
+return new
