bb6166484d5c62fe5b4fc2a40428169caecb5795
aarondill <aaronsacks2006@gmail.com>
2023-09-08 03:33:46 -0500

    make the quake term button open a new terminal when right or middle clicked

--- widget/quake-button.lua
+++ widget/quake-button.lua
@@ -3,14 +3,18 @@ local awful = require("awful")
 local bind = require("util.bind")
 local gtable = require("gears.table")
 local icons = require("theme.icons")
+local open = require("configuration.apps.open")
 local wibox = require("wibox")
 
--- Create an imagebox widget which will contain an icon indicating which layout we're using.
--- We need one layoutbox per screen.
 local QuakeButton = function()
   local imgbox = wibox.widget.imagebox(icons.term or icons.power)
   local iconbutton = IconButton(imgbox)
-  iconbutton:buttons(gtable.join(awful.button({}, 1, bind.with_args(awesome.emit_signal, "quake::toggle"))))
+  local open_terminal = bind.with_args(open.terminal)
+  iconbutton:buttons(gtable.join(
+    awful.button({}, 1, bind.with_args(awesome.emit_signal, "quake::toggle")),
+    awful.button({}, 2, open_terminal), -- open a new terminal on middle click
+    awful.button({}, 3, open_terminal) -- open a new terminal on right click
+  ))
   -- Not supported yet:
   -- local imgbox = layoutBox:get_children_by_id("imagebox")[1]
   -- imgbox:set_stylesheet([[ svg{ color: white; } ]])
