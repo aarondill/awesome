2538eb5ff3fb549e16ef20ab3c0ea032cf35b2d3
aarondill <aaronsacks2006@gmail.com>
2023-09-10 06:04:54 -0500

    cleanup layoutbox

--- widget/layout-box.lua
+++ widget/layout-box.lua
@@ -3,25 +3,17 @@ local clickable_container = require("widget.material.clickable-container")
 local gtable = require("gears.table")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
+local bind = require("util.bind")
 
 -- Create an imagebox widget which will contain an icon indicating which layout we're using.
 -- We need one layoutbox per screen.
 local LayoutBox = function(s)
   local layoutBox = awful.widget.layoutbox(awesome.version <= "v4.3" and s or { screen = s })
-  layoutBox:buttons(gtable.join(
-    awful.button({}, 1, function()
-      awful.layout.inc(1)
-    end),
-    awful.button({}, 3, function()
-      awful.layout.inc(-1)
-    end),
-    awful.button({}, 4, function()
-      awful.layout.inc(1)
-    end),
-    awful.button({}, 5, function()
-      awful.layout.inc(-1)
-    end)
-  ))
+  local up = bind.with_args(awful.layout.inc, 1)
+  local down = bind.with_args(awful.layout.inc, -1)
+  layoutBox:buttons(
+    gtable.join(awful.button({}, 1, up), awful.button({}, 3, down), awful.button({}, 4, up), awful.button({}, 5, down))
+  )
   -- Not supported yet:
   -- local imgbox = layoutBox:get_children_by_id("imagebox")[1]
   -- imgbox:set_stylesheet([[ svg{ color: white; } ]])
