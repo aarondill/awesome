a67ccd9c6fed35d0ff6dec966e9bb58004eba53f
aarondill <aaronsacks2006@gmail.com>
2023-09-10 06:04:35 -0500

    make handle_error more efficient

--- util/handle_error.lua
+++ util/handle_error.lua
@@ -1,6 +1,21 @@
 local notifs = require("util.notifs")
+
+---Handle pcall by packing the return values if ok
+---@param ok boolean?
+---@param ... unknown
+---@return boolean
+---@return table|unknown
+local function pcall_handler(ok, ...)
+  ok = not not ok ---@cast ok boolean
+  if ok then -- only return a table if ok
+    return ok, table.pack(...)
+  end
+  return ok, ...
+end
+
 ---@generic T :function
 ---Run func and notify on error
+---If a custom error handler is provided, it will be called instead of notifing the user.
 ---@param func T the function to call
 ---@param on_error? any if type is function, it will be called with the error as the only parameter. Else, it will be returned in the case of an error.
 ---@return T safe_cb Note: the return types are now different, but lua_ls can't handle the generics neccisary to describe this.
@@ -10,21 +25,17 @@ local function handle_error(func, on_error)
   local on_err_cb = type(on_error) == "function" and handle_error(on_error) or on_error
 
   return function(...)
-    local res = table.pack(pcall(func, ...))
-    local ok = res[1]
+    local ok, ret = pcall_handler(pcall(func, ...))
     if ok then
-      return table.unpack(res, 2, res.n) -- Remove the ok boolean
+      return table.unpack(ret, 2, ret.n) -- Remove the ok boolean
     end
 
-    local err = res[2]
-    notifs.critical(tostring(err), {
-      title = "Something went terribly wrong",
-    })
-    if type(on_err_cb) == "function" then -- Allow returning nil
-      return on_err_cb(err)
-    else
-      return on_err_cb
-    end
+    local err = ret
+    if type(on_err_cb) == "function" then return on_err_cb(err) end
+
+    -- only notify if no function is provided
+    notifs.critical(tostring(err), { title = "Something went terribly wrong" })
+    return on_err_cb
   end
 end
 
