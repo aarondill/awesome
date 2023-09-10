ab1b26e3a298dec8c448e0ac2d9c9e3c63dfd377
aarondill <aaronsacks2006@gmail.com>
2023-09-10 14:34:40 -0500

    rewrite task-list.lua to use declarative layout and simplify certain parts

--- widget/task-list.lua
+++ widget/task-list.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable-next-line :undefined-global
+local capi = { button = button, client = client }
 local awful = require("awful")
 local bind = require("util.bind")
 local clickable_container = require("widget.material.clickable-container")
@@ -7,7 +9,7 @@ local handle_error = require("util.handle_error")
 local icons = require("theme.icons")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
-local capi = { button = button }
+local compat = require("util.compat")
 ---Common method to create buttons.
 ---@param buttons table?
 ---@param object table
@@ -29,34 +31,46 @@ local function create_buttons(buttons, object)
   return btns
 end
 
+---Return container if if_bool is true, else return container[index]
+---@param container table pass to wibox.widget
+---@param if_bool unknown? boolean to check
+---@param index unknown? The index to get object from. Defaults to 1.
+local function optional_container(if_bool, container, index)
+  index = index or 1
+  assert(type(container or {}) == "table", "container must be a table")
+
+  if not container or if_bool then
+    return container
+  else
+    return container[index]
+  end
+end
 ---Creates tasklist widgets and returns them
 ---@param buttons table a set of `button`s
 ---@param c table a client
 ---@param max_width integer the maximum width of each textbox
 ---@return table widgets the set of tasklist widgets
 local function create_tasklist_widgets(buttons, c, max_width)
-  local ib = wibox.widget.imagebox() --- imagebox
-  local tb = wibox.widget.textbox() --- textbox
-  local tbm = wibox.widget({ --- textbox margin
-    { --- textbox constraint
-      tb,
-      strategy = "max",
-      width = max_width,
-      widget = wibox.container.constraint,
-    },
-    left = dpi(4),
-    widget = wibox.container.margin,
-  })
-  local ibm = wibox.widget({ --- imagebox margin
-    ib,
-    widget = wibox.container.margin,
-    margins = dpi(4),
-  })
   local bgb = wibox.widget({ --- background
     { -- clickable_container
       { --- layout
-        ibm,
-        tbm,
+        { --- imagebox margin
+          { --- image box
+            id = "ib",
+            widget = wibox.widget.imagebox,
+          },
+          widget = wibox.container.margin,
+          margins = dpi(4),
+        },
+        { --- textbox margin
+          optional_container(max_width, { --- textbox constraint
+            { id = "tb", widget = wibox.widget.textbox },
+            strategy = "max",
+            width = max_width,
+            widget = wibox.container.constraint,
+          }),
+          widget = wibox.container.margin,
+        },
         { --- close button margin (non-clickable)
           { --- clickable container for close button
             { -- margin for close button (clickable)
@@ -68,7 +82,7 @@ local function create_tasklist_widgets(buttons, c, max_width)
                   widget = wibox.widget.imagebox,
                 },
                 valign = "center",
-                [awesome.version <= "v4.3" and "align" or "halign"] = "center",
+                [compat.widget.halign] = "center",
                 widget = wibox.container.place,
               },
               margins = dpi(2),
@@ -88,6 +102,8 @@ local function create_tasklist_widgets(buttons, c, max_width)
     },
     widget = wibox.container.background,
   })
+  local tb = bgb:get_children_by_id("tb")[1]
+  local ib = bgb:get_children_by_id("ib")[1]
 
   -- Tooltip to display whole title, if it was truncated
   local tt = awful.tooltip({ --- tooltip
@@ -100,13 +116,12 @@ local function create_tasklist_widgets(buttons, c, max_width)
     ib = ib,
     tb = tb,
     bgb = bgb,
-    tbm = tbm,
-    ibm = ibm,
     tt = tt,
   }
 end
 
 ---setup and update widgets on the titlebar
+---update the widgets, creating them if needed
 ---@param config TaskListArgs
 ---@param self table widget
 ---@param buttons table of buttons
@@ -114,56 +129,31 @@ end
 ---@param data table a weekly referenced (keys) table for use in caching
 ---@param clients table a table of the clients to display
 local function list_update(config, self, buttons, label, data, clients)
-  -- update the widgets, creating them if needed
   self:reset()
   for _, c in ipairs(clients) do
-    local cache = data[c]
+    local widgets = data[c] or create_tasklist_widgets(buttons, c, config.max_width)
+    if not data[c] then data[c] = widgets end
 
-    local widgets = cache or create_tasklist_widgets(buttons, c, config.max_width)
-    local ib, tb, bgb, tbm, ibm, tt = widgets.ib, widgets.tb, widgets.bgb, widgets.tbm, widgets.ibm, widgets.tt
-    if not cache then
-      data[c] = {
-        ib = ib,
-        tb = tb,
-        bgb = bgb,
-        tbm = tbm,
-        ibm = ibm,
-        tt = tt,
-      }
-    end
-
+    local ib, tb, bgb, tt = widgets.ib, widgets.tb, widgets.bgb, widgets.tt
     local text, bg, bg_image, icon, args = label(c, tb)
     args = args or {}
 
-    if text == nil or text == "" then
-      tbm:set_margins(0)
-    else
-      -- Needed to update geometry of the other widgets
-      if tt.textbox:set_markup_silently(text) then
-        tt:set_markup(text)
-      else
-        tt:set_markup("<i>&lt;Invalid text&gt;</i>")
-      end
-      tt:add_to_object(tb)
-      if not tb:set_markup_silently(text) then tb:set_markup("<i>&lt;Invalid text&gt;</i>") end
+    local error_string = "<i>&lt;Invalid text&gt;</i>"
+    if not pcall(tt.set_markup, tt, text) then -- set_markup_silently is not available on awful.tooltip
+      tt:set_markup(error_string)
     end
+    tt:add_to_object(tb) -- just in case
+
+    if not tb:set_markup_silently(text) then tb:set_markup(error_string) end
+
     bgb:set_bg(bg)
     bgb:set_bgimage(bg_image)
-    if icon then
-      ib.image = icon
-    else
-      ibm:set_margins(0)
-    end
+    ib.image = icon
 
     bgb.shape = args.shape
 
-    if awesome.version <= "v4.3" then
-      bgb.shape_border_width = args.shape_border_width
-      bgb.shape_border_color = args.shape_border_color
-    else
-      bgb.border_width = args.border_width
-      bgb.border_color = args.border_color
-    end
+    compat.widget.set_border_width(bgb, compat.widget.get_border_width(args))
+    compat.widget.set_border_color(bgb, compat.widget.get_border_color(args))
 
     self:add(bgb)
   end
@@ -172,7 +162,7 @@ end
 -- we can use a global set of buttons because they work with their parameters
 local tasklist_buttons = gtable.join(
   awful.button({}, 1, function(c)
-    if c == client.focus then
+    if c == capi.client.focus then
       c.minimized = true
     else
       -- Without this, the following
@@ -181,7 +171,7 @@ local tasklist_buttons = gtable.join(
       if not c:isvisible() and c.first_tag then c.first_tag:view_only() end
       -- This will also un-minimize
       -- the client, if needed
-      client.focus = c
+      capi.client.focus = c
       c:raise()
     end
   end),
