69f76848a4f9b2116d477289e176a39269346758
aarondill <aaronsacks2006@gmail.com>
2023-09-14 08:48:52 -0500

    make titlebars appear on floating

--- configuration/client/titlebar.lua
+++ configuration/client/titlebar.lua
@@ -1,10 +1,12 @@
+---@diagnostic disable-next-line :undefined-global
+local capi = { client = client, tag = tag }
 local awful = require("awful")
 local compat = require("util.compat")
 local gtable = require("gears.table")
 local wibox = require("wibox")
 
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
+capi.client.connect_signal("request::titlebars", function(c)
   -- buttons for the titlebar
   local buttons = gtable.join(
     awful.button({}, 1, function()
@@ -44,18 +46,30 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Show or hide the titlebar according to requests_no_titlebar and titlebars_enabled
-local function renderTitlebars(client)
-  if client.requests_no_titlebar then
-    awful.titlebar.hide(client, "top")
+local function renderTitlebars(c)
+  if c.requests_no_titlebar then
+    awful.titlebar.hide(c, "top")
   -- NOTE: This must be set manually (in a callback),
   -- the value of `properties: {titlebars_enabled = true}`
   -- is lost after request::titlebars event
-  elseif client.titlebars_enabled then
-    awful.titlebar.show(client, "top")
+  elseif c.floating or (c.first_tag and c.first_tag.layout == awful.layout.suit.floating) then
+    awful.titlebar.show(c, "top")
+  elseif c.titlebars_enabled then -- floating or titlebars enabled
+    awful.titlebar.show(c, "top")
+  else -- default to hide
+    awful.titlebar.hide(c, "top")
   end
 end
 
 -- If client requests not to show the titlebar
-client.connect_signal("property::requests_no_titlebar", renderTitlebars)
+capi.client.connect_signal("property::requests_no_titlebar", renderTitlebars)
+-- Handle floating changes
+capi.client.connect_signal("property::floating", renderTitlebars)
 -- The user has indicated that titlebars should be shown
-client.connect_signal("property::titlebars_enabled", renderTitlebars)
+capi.client.connect_signal("property::titlebars_enabled", renderTitlebars)
+-- Show titlebars on tags with the floating layout
+capi.tag.connect_signal("property::layout", function(t)
+  for _, c in pairs(t:clients()) do
+    renderTitlebars(c)
+  end
+end)
