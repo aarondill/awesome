e0859e12ce6e5c684e68ed0ce7d953478a9ee539
aarondill <aaronsacks2006@gmail.com>
2023-09-15 01:31:03 -0500

    rework icon-button into it's own widget

--- widget/material/icon-button.lua
+++ widget/material/icon-button.lua
@@ -10,14 +10,10 @@ local IconButton = {}
 ---@param img string the path to the image
 ---@return boolean success
 function IconButton:set_image(img)
-  local i = self:get_children_by_id("iconbox")[1]
-  if not i then return false end
-  return i:set_image(img)
+  return self._private.iconbox:set_image(img)
 end
 function IconButton:get_image()
-  local i = self:get_children_by_id("iconbox")[1]
-  if not i then return nil end
-  return i:get_image()
+  return self._private.iconbox:get_image()
 end
 -- alias icon to image
 IconButton.set_icon = IconButton.set_image
@@ -27,26 +23,10 @@ for _, v in pairs({ "margins", "left", "right", "top", "bottom" }) do
   for _, t in ipairs({ "set", "get" }) do
     local method = string.format("%s_%s", t, v)
     IconButton[method] = function(self, m)
-      local margin = self:get_children_by_id("margin")[1]
-      if not m then return {} end
+      local margin = self._private.margin
       return margin[method](margin, m)
     end
   end
-end
-
----@param children table[]
----@return nil
----Note: ensure that the iconbox has an id="iconbox"
-function IconButton:set_children(children)
-  if #children <= 0 then return {} end
-  local m = self:get_children_by_id("margin")[1]
-  if not m then return end
-  m:set_children(children)
-end
-function IconButton:get_children() ---@return table[] children
-  local m = self:get_children_by_id("margin")[1]
-  if not m then return {} end
-  return m:get_children()
 end
 
 --- Creates a button with the path specified
@@ -56,21 +36,17 @@ end
 ---@param buttons? unknown[]
 ---@return clickable_container
 local function new(img, margins, buttons)
-  local ret = wibox.widget({
-    {
-      {
-        image = img,
-        id = "iconbox",
-        widget = wibox.widget.imagebox,
-      },
-      id = "margin",
-      margins = margins or dpi(5),
-      widget = wibox.container.margin,
-    },
-    buttons = buttons,
-    widget = clickable_container,
-  })
+  local iconbox = wibox.widget.imagebox(img)
+  local margin = wibox.container.margin(iconbox)
+  local container = clickable_container(margin, buttons)
+
+  local ret = wibox.widget.base.make_widget(container, nil, { enable_properties = true })
   gtable.crush(ret, IconButton, true)
+
+  ret._private.iconbox = iconbox
+  ret._private.margin = margin
+  ret._private.container = container
+  margin:set_margins(margins or dpi(5))
 
   return ret
 end
