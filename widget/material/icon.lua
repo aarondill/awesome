dcb6caf5776c9abe9a952c751600babf28eb7381
aarondill <aaronsacks2006@gmail.com>
2023-09-15 00:18:14 -0500

    handle render_empty in material.icon

--- widget/material/icon.lua
+++ widget/material/icon.lua
@@ -11,19 +11,20 @@ local Icon = {}
 
 function Icon:layout(_, width, height)
   if not self._private.icon then return {} end
-  if not self._private.size then return end
+  if not self._private.size then return { base.place_widget_at(self._private.imagebox, 0, 0, width, height) } end
   return {
     base.place_widget_at(
       self._private.imagebox,
       width / 2 - self._private.size / 2,
       height / 2 - self._private.size / 2,
-      self._private.size,
-      self._private.size
+      width,
+      height
     ),
   }
 end
 
 function Icon:fit(_, width, height)
+  if not self._private.icon and not self._private.render_empty then return 0, 0 end
   local min = math.min(width, height)
   if self._private.size then min = math.min(min, self._private.size) end
   return min, min
@@ -45,12 +46,28 @@ function Icon:get_size()
   return self._private.size
 end
 
-local function new(icon, size)
+function Icon:set_render_empty(render_empty)
+  self._private.render_empty = render_empty
+  self:emit_signal("widget::layout_changed")
+end
+function Icon:get_render_empty()
+  return self._private.render_empty
+end
+
+---Creates a widget to hold an icon with a given size
+---The icon will be centered in a square of size
+---@param icon string? The icon to create (passed to wibox.widget.imagebox)
+---@param size integer? The size of the icon. If nil, all space will be used
+---@param render_empty boolean? Whether to show empty icons(default: true)
+---@return unknown
+local function new(icon, size, render_empty)
+  render_empty = render_empty == nil and true or render_empty
   local ret = base.make_widget(nil, nil, { enable_properties = true })
   gtable.crush(ret, Icon, true)
   ret._private.icon = icon
   ret._private.imagebox = wibox.widget.imagebox(icon)
   ret._private.size = size
+  ret._private.render_empty = render_empty
   return ret
 end
 return new
