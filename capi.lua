d35fc343d6a104bf2310e1b9f2a9fb74c827ac30
aarondill <aaronsacks2006@gmail.com>
2023-09-16 03:29:59 -0500

    WIP - capi types

--- capi.lua
+++ capi.lua
@@ -1,81 +1,7 @@
----@alias gears.surface unknown
+---@diagnostic disable :unused-local The variables in this file are placeholders for the types. They are not used, nor are they intended to be used
+---@diagnostic disable :missing-return The variables in this file are placeholders for the types. They are not used, nor are they intended to be used
 
----@alias exit_callback fun(type: "signal"|"exit", code: integer)
----@alias xkb_group 0|1|2|3
----@alias xprop_type string|number|boolean
----@alias xprop_type_str "string"|"number"|"boolean"
-
----@class Awesome
----@field quit fun(code: integer?)
----@field exec fun(cmd: string)
----@field spawn fun(cmd: string|string[], use_sn?: boolean, stdin?: boolean, stdout?: boolean, stderr?: boolean, exit_callback?: exit_callback, env?: table<string, string>): pid: integer|string, snid: string?, stdin: integer?, stdout: integer?, stderr: integer?
----@field restart fun()
----Note: these aren't possible to properly type, as the callback is different for each signal
----@field connect_signal fun(signal: string, callback: fun())
----@field disconnect_signal fun(signal: string, callback: fun())
----@field emit_signal fun(signal: string, ...?: unknown)
----@field systray fun(drawin: userdata, x: number, y: number, base_size: number, horiz: boolean?, bg: string, revers: boolean?, spacing: number, rows: number): entries: number, parent: table
----@field load_image fun(name: string): gears.surface, error: string
----@field pixbuf_to_surface fun(pixbuf: userdata): gears.surface
----@field set_preferred_icon_size fun(size: integer)
----@field register_xproperty fun(name: string, type:  xprop_type_str)
----@field set_xproperty fun(name: string, value: xprop_type)
----@field get_xproperty fun(name: string): xprop_type? Errors if not found
----@field xkb_set_layout_group fun(num: xkb_group|integer) --- Note: the signature allows integer, but this is just for convenience.
----@field xkb_get_layout_group fun(): num: xkb_group
----@field xkb_get_group_names fun(): string
----@field xrdb_get_value fun(class: string, name: string): string?
----@field kill fun(pid: integer, sig: string|integer): boolean
----@field sync fun()
----@field unix_signal { [string]: integer?, [integer]: string? }
----@field _get_key_name fun(input: string?): keysym: string?, printsymbol: string?
---@field __index fun(): nil
---@field __newindex fun(): nil
-
----@class AwesomeRoot
----@field cursor fun(): nil
----@field fake_input fun(): nil
----@field drawins fun(): nil
----@field content fun(): nil
----@field size fun(): nil
----@field size_mm fun(): nil
----@field tags fun(): nil
----@field set_index_miss_handler fun(): nil
----@field set_call_handler fun(): nil
----@field set_newindex_miss_handler fun(): nil
----@field _buttons fun(): nil
----@field _keys fun(): nil
----@field _wallpaper fun(): nil
----@field __index fun(): nil
----@field __newindex fun(): nil
-
----@class AwesomeDbus
----@field request_name fun(): nil
----@field release_name fun(): nil
----@field add_match fun(): nil
----@field remove_match fun(): nil
----@field connect_signal fun(): nil
----@field disconnect_signal fun(): nil
----@field emit_signal fun(): nil
----@field __index fun(): nil
----@field __newindex fun(): nil
-
----@class AwesomeKeygrabber
----@class AwesomeMousegrabber
----@class AwesomeMouse
----@class AwesomeScreen
----@class AwesomeButton
----@class AwesomeTag
----@class AwesomeWindow
----@class AwesomeDrawable
----@class AwesomeDrawin
----@class AwesomeClient
-
----@alias selectionFunc fun(): string
----moved under selection.selection in v5
----@alias AwesomeSelection {selection: selectionFunc} | selectionFunc
-
-return { ---@diagnostic disable :undefined-global These are injected by AwesomeWM
+local capi = { ---@diagnostic disable :undefined-global These are injected by AwesomeWM
   awesome = awesome, ---@type Awesome
   root = root, ---@type AwesomeRoot
   dbus = dbus, ---@type AwesomeDbus?
@@ -90,4 +16,178 @@ return { ---@diagnostic disable :undefined-global These are injected by AwesomeW
   drawin = drawin, ---@type AwesomeDrawin
   client = client, ---@type AwesomeClient
   selection = selection, ---@type AwesomeSelection
+  key = key, ---@type AwesomeKey
 } ---@diagnostic enable :undefined-global
+
+local types = {}
+
+---@alias gears.surface unknown
+---@alias exit_callback fun(type: "signal"|"exit", code: integer)
+---@alias xkb_group 0|1|2|3
+---@alias xprop_type string|number|boolean
+---@alias xprop_type_str "string"|"number"|"boolean"
+
+---@class AwesomeSignalClass
+types.AwesomeSignalClass = {
+  ---@param signal string
+  add_signal = function(signal) end, ---@deprecated Use connect_signal without calling add_signal
+  ---@param signal string
+  ---@param callback function
+  connect_signal = function(signal, callback) end,
+  ---@param signal string
+  ---@param callback function
+  disconnect_signal = function(signal, callback) end,
+  ---@param signal string
+  ---@param ... unknown
+  emit_signal = function(signal, ...) end,
+  instances = function() end, ---@return integer
+  ---@param handler fun(self: AwesomeSignalClass, k: any)
+  set_index_miss_handler = function(handler) end, --- Typically this shouldn't be used
+  ---@param handler fun(self: AwesomeSignalClass, k: any, v: any)
+  set_newindex_miss_handler = function(handler) end, --- Typically this shouldn't be used
+}
+---@class Awesome
+---@field unix_signal { [string]: integer?, [integer]: string? }
+types.Awesome = {
+  quit = function(code) end, ---@param code integer?
+  exec = function(cmd) end, ---@param cmd string
+  ---@param cmd string|string[]
+  ---@param use_sn? boolean
+  ---@param stdin? boolean
+  ---@param stdout? boolean
+  ---@param stderr? boolean
+  ---@param exit_callback? exit_callback
+  ---@param env? table<string, string>
+  ---@return integer|string pid
+  ---@return string? snid
+  ---@return integer? stdin
+  ---@return integer? stdout
+  ---@return integer? stderr
+  spawn = function(cmd, use_sn, stdin, stdout, stderr, exit_callback, env) end,
+  restart = function() end,
+  ---Note: these aren't possible to properly type, as the callback is different for each signal
+  ---@param signal string
+  ---@param callback function
+  connect_signal = function(signal, callback) end,
+  ---Note: these aren't possible to properly type, as the callback is different for each signal
+  ---@param signal string
+  ---@param callback function
+  disconnect_signal = function(signal, callback) end,
+  ---Note: these aren't possible to properly type, as the callback is different for each signal
+  ---@param signal string
+  ---@param ... unknown?
+  emit_signal = function(signal, ...) end,
+  ---@param drawin userdata
+  ---@param x number
+  ---@param y number
+  ---@param base_size number
+  ---@param horiz boolean
+  ---@param bg string
+  ---@param revers boolean
+  ---@param spacing number
+  ---@param rows number
+  ---@return number entries
+  ---@return table parent
+  systray = function(drawin, x, y, base_size, horiz, bg, revers, spacing, rows) end,
+  ---@param name string
+  ---@return gears.surface
+  ---@return string error
+  load_image = function(name) end,
+  ---@param pixbuf userdata
+  ---@return gears.surface
+  pixbuf_to_surface = function(pixbuf) end,
+  ---@param size integer
+  set_preferred_icon_size = function(size) end,
+  ---@param name string
+  ---@param type xprop_type_str
+  register_xproperty = function(name, type) end,
+  ---@param name string
+  ---@param value xprop_type
+  set_xproperty = function(name, value) end,
+  ---@param name string
+  ---@return xprop_type?
+  get_xproperty = function(name) end, ---Errors if not found
+  ---@param num xkb_group|integer
+  --- Note: the signature allows integer, but this is just for convenience.
+  xkb_set_layout_group = function(num) end,
+  xkb_get_layout_group = function() end, ---@return xkb_group num
+  xkb_get_group_names = function() end, ---@return string
+  ---@param class string
+  ---@param name string
+  ---@return string?
+  xrdb_get_value = function(class, name) end,
+  ---@param pid integer
+  ---@param sig string|integer
+  ---@return boolean
+  kill = function(pid, sig) end,
+  sync = function() end,
+  ---@param input string?
+  ---@return string? keysym
+  ---@return string? printsymbol
+  _get_key_name = function(input) end,
+}
+function types.Awesome:__index(k) end
+function types.Awesome:__newindex(k, v) end
+
+---@class AwesomeRoot
+types.AwesomeRoot = {
+  cursor = function() end,
+  fake_input = function() end,
+  drawins = function() end,
+  content = function() end,
+  size = function() end,
+  size_mm = function() end,
+  tags = function() end,
+  set_index_miss_handler = function() end,
+  set_call_handler = function() end,
+  set_newindex_miss_handler = function() end,
+  _buttons = function() end,
+  _keys = function() end,
+  _wallpaper = function() end,
+}
+function types.AwesomeRoot:__index(k) end
+function types.AwesomeRoot:__newindex(k, v) end
+
+---@class AwesomeDbus
+types.AwesomeDbus = {
+  request_name = function() end,
+  release_name = function() end,
+  add_match = function() end,
+  remove_match = function() end,
+  connect_signal = function() end,
+  disconnect_signal = function() end,
+  emit_signal = function() end,
+  __index = function() end,
+  __newindex = function() end,
+}
+
+---@class AwesomeKeygrabber
+types.AwesomeKeygrabber = {}
+---@class AwesomeMousegrabber
+types.AwesomeMousegrabber = {}
+---@class AwesomeMouse
+types.AwesomeMouse = {}
+---@class AwesomeScreen :AwesomeSignalClass
+types.AwesomeScreen = {}
+---@class AwesomeButton :AwesomeSignalClass
+types.AwesomeButton = {}
+---@class AwesomeTag :AwesomeSignalClass
+types.AwesomeTag = {}
+---@class AwesomeWindow
+types.AwesomeWindow = {}
+---@class AwesomeDrawable :AwesomeSignalClass
+types.AwesomeDrawable = {}
+---@class AwesomeDrawin :AwesomeSignalClass
+types.AwesomeDrawin = {}
+---@class AwesomeClient :AwesomeSignalClass
+types.AwesomeClient = {}
+---@class AwesomeKey :AwesomeSignalClass
+types.AwesomeKey = {}
+
+---@alias selectionFunc fun(): string
+---@alias AwesomeSelection { selection: selectionFunc } | selectionFunc
+---moved under selection.selection in v5
+
+types = nil -- remove references to above tables
+collectgarbage("collect") -- Encourage lua to remove the above tables
+return capi
