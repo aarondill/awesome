bc86f80d5f6f23a38a86cc1e014a019fca099904
aarondill <aaronsacks2006@gmail.com>
2023-09-19 09:52:47 -0500

    make util.init work better with all paths

--- util/init.lua
+++ util/init.lua
@@ -1,6 +1,8 @@
-local mt = {
-  __index = function(_, key)
-    return require("util." .. key)
-  end,
-}
+local mt = {}
+local m_path = ...
+function mt:__index(key)
+  local require = require("util.rel_require")
+  local mod = require(m_path, key, false)
+  return mod
+end
 return setmetatable({}, mt)
