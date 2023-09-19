10adf813faed266204aad6665d4181ba59e52561
aarondill <aaronsacks2006@gmail.com>
2023-09-19 17:47:27 -0500

    make xss-lock and udiskie quiet for memory savings

--- configuration/apps/run_on_startup.lua
+++ configuration/apps/run_on_startup.lua
@@ -25,13 +25,9 @@ local run_on_startup = {
   -- "exec xfce4-power-manager", -- Power manager
   "xset s 0 0", -- disable screen saver
   "xset -dpms", -- Disable dpms because doesn't work with keys?
-  "xss-lock -- lock", -- Lock on suspend or dpms
+  { "xss-lock", "-q", "--", "lock" }, -- Lock on suspend or dpms
   "numlockx on",
-  {
-    "udiskie",
-    "-c",
-    config_file_dir .. "/udiskie.yml",
-  }, -- Automount disks.
+  { "udiskie", "-q", "-c", config_file_dir .. "/udiskie.yml" }, -- Automount disks.
   "ibus-daemon --xim -d", -- Run ibus-daemon for language and emoji keyboard support
   { notification_daemon },
   { "libinput-gestures", "--conffile", config_file_dir .. "/libinput-gestures.conf" }, -- Enable touch gesture support
