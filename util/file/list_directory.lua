370ab77277d1ffd46a9cb6049c00e1e9faea2bfb
aarondill <aaronsacks2006@gmail.com>
2023-10-15 03:52:15 -0500

    use the filter for list_directory.
    Max should now work as expected! :)

--- util/file/list_directory.lua
+++ util/file/list_directory.lua
@@ -1,5 +1,6 @@
 local gtable = require("gears.table")
 local require = require("util.rel_require")
+local FILE_NAME_PROP = "FILE_ATTRIBUTE_STANDARD_NAME"
 
 local scan_directory = require(..., "scan_directory") ---@module "util.file.scan_directory"
 ---@class list_directory_args :scan_directory_args
@@ -17,23 +18,26 @@ local function list_directory(path, args, cb)
   if type(args) == "function" and cb == nil then
     cb, args = args, nil
   end
-  args = args and gtable.clone(args) or {}
+  args = args and gtable.clone(args) or {} ---@type list_directory_args
   assert(type(args) == "table", "args must be a table")
   assert(type(args.match or "") == "string", "args.match must be a string")
-  args.attributes = { "FILE_ATTRIBUTE_STANDARD_NAME" } -- override user provided
+
   local match = args.match
-  args.match = nil -- don't pass to scan_directory
-  scan_directory(path, args, function(contents, error)
-    if not contents then return cb(contents, error) end
-    local ret = {}
-    for _, v in ipairs(contents) do
-      ---@type string
-      local name = v["FILE_ATTRIBUTE_STANDARD_NAME"]
-      if (not match) or name:match(match) ~= nil then
-        ret[#ret + 1] = name -- handle match pattern
-      end
+  args.filter = match -- if a match is specified, filter based on it.
+    and function(file)
+      local name = file[FILE_NAME_PROP] ---@type string
+      return name:match(match) ~= nil -- include only if filename matches pattern
     end
-    cb(ret, error)
+  args.attributes = { FILE_NAME_PROP } -- override user provided
+
+  args.match = nil -- don't pass to scan_directory
+  return scan_directory(path, args, function(files, err)
+    if files == nil then return cb(files, err) end
+    local names = {}
+    for k, v in ipairs(files) do
+      names[k] = v[FILE_NAME_PROP]
+    end
+    return cb(names, nil)
   end)
 end
 
