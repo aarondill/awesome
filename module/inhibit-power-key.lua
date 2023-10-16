7bee44758740e1d70106e8835da95c5a6b6e2784
aarondill <aaronsacks2006@gmail.com>
2023-10-16 06:07:57 -0500

    move create_inhibitor to it's own file
    Former-commit-id: c0ddb8cc8c791cb3153c1b686ea5910236d6619d

--- module/inhibit-power-key.lua
+++ module/inhibit-power-key.lua
@@ -1,68 +1,9 @@
-local capi = require("capi")
 local get_dbus_property = require("util.dbus.get_prop")
 local lgi = require("lgi")
 local Gio = lgi.require("Gio")
-local GLib = lgi.require("GLib")
+local create_inhibitor = require("util.dbus.create_inhibitor")
 
--- Workaround for https://github.com/pavouk/lgi/issues/142
-local function bus_get_async(type)
-  Gio.bus_get(type, nil, coroutine.running())
-  local _, b = coroutine.yield()
-  return Gio.bus_get_finish(b)
-end
-
-local function inhibit(bus, what, who, why, mode)
-  local name = "org.freedesktop.login1"
-  local object = "/org/freedesktop/login1"
-  local interface = "org.freedesktop.login1.Manager"
-  local message = Gio.DBusMessage.new_method_call(name, object, interface, "Inhibit")
-  message:set_body(GLib.Variant("(ssss)", { what, who, why, mode }))
-
-  local timeout = -1 -- Just use the default
-  local result, err = bus:async_send_message_with_reply(message, Gio.DBusSendMessageFlags.NONE, timeout, nil)
-
-  if err then
-    print("error: " .. tostring(err))
-    return
-  end
-
-  if result:get_message_type() == "ERROR" then
-    local _, err = result:to_gerror()
-    print("error: " .. tostring(err))
-    return
-  end
-
-  local fd_list = result:get_unix_fd_list()
-  local fd, err = fd_list:get(0)
-  if err then
-    print("error: " .. tostring(err))
-    return
-  end
-
-  -- Now... somehow turn this fd into something we can close
-  return Gio.UnixInputStream.new(fd, true)
-end
-
----@param what string
----@param why string
----@param mode string?
-local function create_inhibitor(what, why, mode, cb)
-  local bus = bus_get_async(Gio.BusType.SYSTEM)
-  -- The block is lost when this is garbage collected! It is scope leaked into the awesome.connect_signal to avoid this.
-  local fd = inhibit(bus, what, "AwesomeWM", why, mode or "block")
-  if not fd then return end -- something went wrong
-
-  capi.awesome.connect_signal("exit", function(_)
-    -- Stops the block
-    fd:close()
-    -- Speed up deletion of the GDBusMessage that still references the FD
-    collectgarbage("collect")
-    collectgarbage("collect")
-  end)
-  if cb then return cb() end
-end
-
-Gio.Async.call(create_inhibitor)("handle-power-key", "To manually handle power key", "block")
+create_inhibitor("handle-power-key", "To manually handle power key", "block")
 
 --This is fixed in the next release of Xorg, but until then, we've got this to inhibit idle timeouts
 local XDG_SESSION_ID = os.getenv("XDG_SESSION_ID") -- If this is not available, we can't reliably determine this (easily) anyways.
@@ -75,6 +16,6 @@ if XDG_SESSION_ID then
     assert(res.value[1].type == "s")
     local Type = res.value[1].value
     if Type ~= "tty" then return end -- the bug is fixed.
-    Gio.Async.call(create_inhibitor)("idle", "Because idle timeout is broken with startx", "block")
+    create_inhibitor("idle", "Because idle timeout is broken with startx", "block")
   end)
 end
