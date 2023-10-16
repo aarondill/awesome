5447e716280737ffa20c6de5adfea421b9617584
aarondill <aaronsacks2006@gmail.com>
2023-10-16 09:36:50 -0500

    remove before/after callbacks
    Just check the is_before arg
    
    
    Former-commit-id: 02a7eeec23391146d7f2786f864efd5bdb86e15a

--- module/suspend-listener.lua
+++ module/suspend-listener.lua
@@ -1,64 +1,56 @@
-local lgi = require("lgi")
-local Gio = lgi.require("Gio")
-
 local M = {}
-M.callbacks_before = {
-  strong = {},
-  weak = setmetatable({}, { __mode = "k" }),
-}
-M.callbacks_after = {
-  strong = {},
-  weak = setmetatable({}, { __mode = "k" }),
-}
+---@alias suspendCallback fun(is_before: boolean)
+M.callbacks = {} ---@type table<suspendCallback, unknown>
 
 local function handler(_bus, _sender, _object, _interface, _signal, params)
   -- "signals are sent right before (with the argument True) and
   -- after (with the argument False) the system goes down for
   -- reboot/poweroff, resp. suspend/hibernate."
   local before_sleep = params[1]
-  local type = before_sleep and "before" or "after"
-  for cb in pairs(M["callbacks_" .. type].strong) do
-    cb(type)
-  end
-  for cb in pairs(M["callbacks_" .. type].weak) do
-    cb(type)
+  for cb in pairs(M.callbacks) do
+    cb(before_sleep)
   end
 end
 
-local bus = nil
-local function listen_to_signals()
-  if bus then return end
-  bus = Gio.bus_get_sync(Gio.BusType.SYSTEM) -- This *needs* to be scope leaked above! The subscription will be lost if the bus is GCed
-  local sender = "org.freedesktop.login1"
-  local interface = "org.freedesktop.login1.Manager"
-  local object = "/org/freedesktop/login1"
-  local member = "PrepareForSleep"
-  bus:signal_subscribe(sender, interface, member, object, nil, Gio.DBusSignalFlags.NONE, handler)
+local id = nil ---@type SubscribeID?
+---Creates a new subscription.
+---Safe to call after already created subscription
+local function create_subscription()
+  if id then return end -- Keep the single subscription
+  -- This *needs* to be scope leaked above! The subscription will be lost if the bus is GCed
+  id = require("util.dbus.subscribe_signal").subscribe({
+    sender = "org.freedesktop.login1",
+    interface = "org.freedesktop.login1.Manager",
+    object = "/org/freedesktop/login1",
+    member = "PrepareForSleep",
+    callback = handler,
+  })
+end
+---Removes the subscription to the signal.
+---Safe to call even when callbacks still exist.
+local function remove_subscription()
+  if not id then return end -- There's no subscription.
+  if next(M.callbacks) then return end -- there's one+ callbacks still
+  require("util.dbus.subscribe_signal").unsubscribe(id)
+  id = nil
 end
 
 ---Register a callback
----@param cb fun(when: 'before'|'after')
----@param before boolean? register for before sleep? false means after wake. default: false
----@param weak boolean? weakly register callback?
+---@param cb suspendCallback
 ---@return boolean success whether something changed.
-M.register_listener = function(cb, before, weak)
-  local type = before and "before" or "after"
-  local t = M["callbacks_" .. type][weak and "weak" or "strong"]
-  if t[cb] then return false end
-  t[cb] = true
-  listen_to_signals() -- Ensure we are connected to dbus!
+M.register_listener = function(cb)
+  if M.callbacks[cb] then return false end
+  M.callbacks[cb] = true
+  create_subscription() -- Ensure we are connected to dbus!
   return true
 end
 ---Remove a previously registered callback
----@param cb fun(when: 'before'|'after')
----@param before boolean? register for before sleep? false means after wake. default: false
----@param weak boolean? weakly register callback?
+---@param cb suspendCallback
 ---@return boolean success whether something changed.
-M.unregister_listener = function(cb, before, weak)
-  local type = before and "before" or "after"
-  local t = M["callbacks_" .. type][weak and "weak" or "strong"]
-  if not t[cb] then return false end
-  t[cb] = nil
+M.unregister_listener = function(cb)
+  if not M.callbacks[cb] then return false end
+  M.callbacks[cb] = nil
+  remove_subscription()
   return true
 end
 
