59d3fec365f771196cdd6a787a687c27ad1c22fa
aarondill <aaronsacks2006@gmail.com>
2023-10-16 09:38:19 -0500

    add types on dbus.init.lua
    Former-commit-id: 55b51059a5cfa5b34638346277e4d6f84b07e03b

--- util/dbus/init.lua
+++ util/dbus/init.lua
@@ -2,6 +2,8 @@ local require = require("util.rel_require")
 ---@diagnostic disable: assign-type-mismatch -- To allow nil values
 local M = {
   get_prop = nil, ---@module "util.dbus.get_prop"
+  create_inhibitor = nil, ---@module "util.dbus.create_inhibitor"
+  subscribe_signal = nil, ---@module "util.dbus.subscribe_signal"
 }
 ---@diagnostic enable: assign-type-mismatch
 assert(#M == 0, "Tag list module has keys set. This is a bug! Use the metatable!")
