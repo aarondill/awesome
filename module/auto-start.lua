9ff36deb2adebace49f9f34bdc245aa968d1bc47
aarondill <aaronsacks2006@gmail.com>
2023-10-18 07:12:21 -0500

    fix(auto-start): remove logs
    This avoids the issue of opening (and keeping) too many file descriptors.
    Notifs are still sent for failed commands.

--- module/auto-start.lua
+++ module/auto-start.lua
@@ -4,15 +4,9 @@ local DEBUG = require("configuration").DEBUG
 
 local apps = require("configuration.apps")
 local capi = require("capi")
-local file_write = require("util.file.write_async")
-local gfilesystem = require("gears.filesystem")
 local notifs = require("util.notifs")
-local serialize_table = require("util.serialize_table")
 local spawn = require("util.spawn")
 
---- Directory for logging failed(?) application's output
---- This *MUST* end in a slash
-local log_dir = gfilesystem.get_cache_dir() .. "auto-start" .. "/"
 --- A map of cmds to pids
 ---@type table<CommandProvider, integer>
 local processes = {}
@@ -24,67 +18,34 @@ local function err(cmd, e)
   })
 end
 
+local function get_warning(exitreason, exitcode)
+  if exitreason == "exit" then return string.format("exit code: %d", exitcode) end
+  -- Segfault:
+  if exitcode == capi.awesome.unix_signal.SIGSEGV then return "Segfaulted!" end
+  local signame = tostring(capi.awesome.unix_signal[exitcode])
+  return string.format("killed with signal: %d (%s)", tostring(exitcode), signame)
+end
 ---@param cmd CommandProvider the thing to run
 ---@return integer? pid of the process or nil if error
 local function run_once(cmd)
   if not cmd then return nil end
-  --- Used in log to ensure that the date matches the *start* date, not the *end* date
-  local CMD_DATE = os.date() ---@cast CMD_DATE string
-  local pid = spawn.async(cmd, function(stdout, stderr, exitreason, exitcode)
-    if exitreason == "exit" and exitcode == 0 then return end
-    -- Command not found. I don't want a warning.
-    -- Remove this to send notifications for missing commands.
-    if exitreason == "exit" and exitcode == 127 and not DEBUG then return end
-
-    --- The pid of the process. Don't assume this exists. Race Conditions.
-    local pid = processes[cmd]
-    --- Files to write to. These may be nil.
-    local log_file_stdout, log_file_stderr
-    -- Exclude sigterm, as it was likely user input anyways.
-    if pid and not (exitreason == "signal" and exitcode == capi.awesome.unix_signal.SIGTERM) then
-      log_file_stdout = log_dir .. pid .. "-stdout.log"
-      log_file_stderr = log_dir .. pid .. "-stderr.log"
-    end
-    if log_file_stdout and log_file_stderr then
-      --- Header for the log file
-      local header = table
-        .concat({ "Command: %s", "Date: %s", "----------------------------------------", "" }, "\n")
-        :format(type(cmd) == "table" and serialize_table(cmd) or cmd, CMD_DATE)
-      -- Ensure it exists!
-      gfilesystem.make_parent_directories(log_file_stdout)
-      gfilesystem.make_parent_directories(log_file_stderr)
-      -- *Async* write.
-      file_write(log_file_stdout, header .. stdout)
-      file_write(log_file_stderr, header .. stderr)
-    end
-
-    local text = ""
-    if exitreason == "signal" then
-      if exitcode == capi.awesome.unix_signal.SIGSEGV then -- Segfault
-        text = "Segfaulted!"
-      else
-        local signame = tostring(capi.awesome.unix_signal[exitcode])
-        text = string.format("killed with signal: %d (%s)", tostring(exitcode), signame)
-      end
-    else
-      text = string.format("exit code: %d", exitcode)
-    end
-    if log_file_stdout and log_file_stderr then
-      text = text .. ("\nLogs are available at:\n%s\n%s"):format(log_file_stdout, log_file_stderr)
-    end
-    err(cmd, text)
-  end, {
-    on_failure_callback = function(e)
+  local info = spawn.nosn(cmd, {
+    exit_callback_err = function(exitreason, exitcode)
+      -- Command not found. I don't want a warning.
+      -- Remove this to send notifications for missing commands.
+      if exitreason == "exit" and exitcode == 127 and not DEBUG then return end
+      return err(cmd, get_warning(exitreason, exitcode))
+    end,
+    on_failure_callback = DEBUG or nil and function(e)
       -- Something went wrong. Likely isn't installed. This would be where you notify if you want to when a command is not found.
-      if DEBUG then return err(cmd, e) end
+      return err(cmd, e)
     end,
   })
-  return pid
+  if info then return info.pid end
 end
 
 for _, app in ipairs(apps.run_on_startup) do
-  local pid = run_once(app)
-  if pid then processes[app] = pid end
+  processes[app] = run_once(app)
 end
 -- Kill them all on exit
 capi.awesome.connect_signal("exit", function(_)
