07eefaa3e83c605e9fc4f2fb14cb0b0416949a52
aarondill <aaronsacks2006@gmail.com>
2023-10-22 16:37:39 -0500

    refactor decorate-client.

--- module/decorate-client.lua
+++ module/decorate-client.lua
@@ -7,10 +7,16 @@ local gshape = require("gears.shape")
 local gtimer = require("gears.timer")
 local quake = require("module.quake")
 
-local function renderClient(client, mode)
+---@param client AwesomeClientInstance
+---@param rounded_corners boolean
+local function renderClient(client, rounded_corners)
   if quake:client_is_quake(client) then return end
-  if client.skip_decoration or (client.rendering_mode == mode) then return end
-  if client.is_new then
+  if client.skip_decoration then return end ---@diagnostic disable-line :undefined-field this is an injected field
+
+  if client.rendering_mode == rounded_corners then return end -- Check cached.
+  client.rendering_mode = rounded_corners ---@diagnostic disable-line :inject-field this is an injected field
+
+  if not capi.awesome.startup then
     client.floating = false
     client.maximized = false
     client.above = false
@@ -19,87 +25,61 @@ local function renderClient(client, mode)
     client.sticky = false
     client.maximized_horizontal = false
     client.maximized_vertical = false
-    client.is_new = false
   end
 
-  client.rendering_mode = mode
-  if client.rendering_mode == "maximized" then
-    client.border_width = 0
-    client.shape = gshape.rectangle
-  elseif client.rendering_mode == "tiled" then
-    client.border_width = beautiful.border_width
-    client.shape = function(cr, w, h)
-      gshape.rounded_rect(cr, w, h, 8)
-    end
+  if not rounded_corners then
+    client.border_width, client.shape = 0, gshape.rectangle
+    return
+  end
+
+  client.border_width = beautiful.border_width
+  client.shape = function(cr, w, h)
+    gshape.rounded_rect(cr, w, h, 8)
   end
 end
 
-local changesOnScreenCalled = false
+local changesOnScreenPending = false
 
-local function changesOnScreen(currentScreen)
-  local tagIsMax = currentScreen.selected_tag ~= nil
-    and (
-      currentScreen.selected_tag.layout == awful.layout.suit.max
-      or currentScreen.selected_tag.layout == awful.layout.suit.max.fullscreen
-    )
-  local clientsToManage = {}
-
-  for _, client in pairs(currentScreen.clients or {}) do
+local function is_tag_maximized(tag)
+  if not tag then return false end
+  if tag.layout == awful.layout.suit.max then return true end
+  if tag.layout == awful.layout.suit.max.fullscreen then return true end
+end
+local function changesOnScreen(currentScreen) ---@param currentScreen AwesomeScreenInstance
+  local tag_is_max = is_tag_maximized(currentScreen.selected_tag)
+  local managed_clients = {}
+  for _, client in pairs(currentScreen.clients) do
+    ---@diagnostic disable-next-line :undefined-field this is an injected field
     if not client.skip_decoration and not client.hidden and not quake:client_is_quake(client) then
-      table.insert(clientsToManage, client)
+      table.insert(managed_clients, client)
     end
   end
 
-  if tagIsMax or #clientsToManage == 1 then
-    currentScreen.client_mode = "maximized"
-  else
-    currentScreen.client_mode = "tiled"
+  local use_round_courners = (not tag_is_max and #managed_clients > 1)
+  for _, client in pairs(managed_clients) do
+    renderClient(client, client.fullscreen or use_round_courners)
   end
-
-  for _, client in pairs(clientsToManage) do
-    renderClient(client, currentScreen.client_mode)
-  end
-  changesOnScreenCalled = false
+  changesOnScreenPending = false
 end
 
-local function clientCallback(client)
-  if not changesOnScreenCalled then
-    if not client.skip_decoration and client.screen then
-      changesOnScreenCalled = true
-      local screen = client.screen
-      gtimer.delayed_call(bind.with_args(changesOnScreen, screen))
-    end
-  end
+local function clientCallback(client) ---@param client AwesomeClientInstance
+  if changesOnScreenPending then return end
+  if client.skip_decoration or not client.screen then return end ---@diagnostic disable-line :undefined-field this is an injected field
+  changesOnScreenPending = true
+  gtimer.delayed_call(bind.with_args(changesOnScreen, client.screen))
+end
+local function tagCallback(tag) ---@param tag AwesomeTagInstance
+  if changesOnScreenPending then return end
+  if not tag.screen then return end
+  changesOnScreenPending = true
+  gtimer.delayed_call(bind.with_args(changesOnScreen, tag.screen))
 end
 
-local function tagCallback(tag)
-  if not changesOnScreenCalled then
-    if tag.screen then
-      changesOnScreenCalled = true
-      local screen = tag.screen
-      gtimer.delayed_call(bind.with_args(changesOnScreen, screen))
-    end
-  end
-end
-
-capi.client.connect_signal(compat.signal.manage, function(c)
-  if not capi.awesome.startup then c.is_new = true end
-  clientCallback(c)
-end)
+capi.client.connect_signal(compat.signal.manage, clientCallback)
 capi.client.connect_signal(compat.signal.unmanage, clientCallback)
-
 capi.client.connect_signal("property::hidden", clientCallback)
-
 capi.client.connect_signal("property::minimized", clientCallback)
+capi.client.connect_signal("property::fullscreen", clientCallback)
 
-capi.client.connect_signal("property::fullscreen", function(c)
-  if c.fullscreen then
-    renderClient(c, "maximized")
-  else
-    clientCallback(c)
-  end
-end)
-
-awful.tag.attached_connect_signal(nil, "property::selected", tagCallback)
-
-awful.tag.attached_connect_signal(nil, "property::layout", tagCallback)
+capi.tag.connect_signal("property::selected", tagCallback)
+capi.tag.connect_signal("property::layout", tagCallback)
