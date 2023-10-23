797916b5ef55092227529a8492b8c1b6e1c74e5d
aarondill <aaronsacks2006@gmail.com>
2023-10-23 08:10:44 -0500

    check multiple reminder files until we find a matching one

--- module/reminder.lua
+++ module/reminder.lua
@@ -2,10 +2,28 @@ local find_home = require("util.find_home")
 local notifs = require("util.notifs")
 local read_async = require("util.file.read_async")
 
-return read_async(find_home("./reminder"), function(content, _, path)
-  if not content then return end
+local function extensions(path, exts)
+  local ret = { path }
+  for _, ext in ipairs(exts) do
+    table.insert(ret, ("%s.%s"):format(path, ext))
+  end
+  return ret
+end
+local home_reminder = find_home("./reminder")
+local paths = extensions(home_reminder, { "txt", "md" })
+local index = 1
+
+local function handler(content, _, path)
+  if not content then -- Repeat until we find one.
+    index = index + 1 -- Try the next one.
+    if not paths[index] then return end
+    return read_async(paths[index], handler)
+  end
+
   return notifs.info(
     content:gsub("^\n*", ""):gsub("\n*$", ""):gsub("\n\n\n", "\n\n"),
     { title = ("Reminder (%s)"):format(path) }
   )
-end)
+end
+
+return read_async(paths[index], handler)
