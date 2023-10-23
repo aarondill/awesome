6e96dbbeea0791cd7be3b7f77dd7af95b7db8216
aarondill <aaronsacks2006@gmail.com>
2023-10-23 07:56:41 -0500

    remove the last newline from the reminder content

--- module/reminder.lua
+++ module/reminder.lua
@@ -2,7 +2,7 @@ local find_home = require("util.find_home")
 local notifs = require("util.notifs")
 local read_async = require("util.file.read_async")
 
-return read_async(find_home() .. "/reminder", function(content)
+return read_async(find_home("./reminder"), function(content, _, path)
   if not content then return end
-  return notifs.info(content, { title = "Reminder" })
+  return notifs.info(content:gsub("\n$", ""), { title = ("Reminder (%s)"):format(path) })
 end)
