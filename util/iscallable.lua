b2296e5587ca7daa6b459f6c6814bc5022bda5a5
aarondill <aaronsacks2006@gmail.com>
2023-10-25 12:26:02 -0500

    iscallable should allow nil sometimes
    Former-commit-id: b75d54a42e01fcdac2dba16c73d845a865d19ff4

--- util/iscallable.lua
+++ util/iscallable.lua
@@ -1,12 +1,29 @@
+---@param bool boolean
+---@param x unknown?
+---@return boolean
+---@return string?
+local function failed(bool, x)
+  if bool then return true end
+  return false, "expected function got type " .. type(x) -- Return string for assertions
+end
+
 ---From https://stackoverflow.com/a/58795138
 ---@param x unknown?
-local function iscallable(x)
+---@param permit_nil boolean? default: false
+---@return boolean callable
+---@return string? err For convenience with assert()
+local function iscallable(x, permit_nil)
+  permit_nil = not not permit_nil
+  if x == nil then return failed(permit_nil, x) end
   if type(x) == "function" then return true end
-  if type(x) ~= "table" then return false end
-  if not debug then return false end -- Give up.
-
-  local mt = debug.getmetatable(x)
-  return type(mt) == "table" and type(mt.__call) == "function"
+  if type(x) ~= "table" then return failed(false, x) end
+  local ok, mt = pcall(getmetatable, x)
+  if not ok or (mt and type(mt) ~= "table") then -- Errored or returned a non-table
+    if not debug then return failed(false, x) end -- Give up.
+    mt = debug.getmetatable(x)
+  end
+  local callable = type(mt) == "table" and type(mt.__call) == "function"
+  return failed(callable, x)
 end
 
 return iscallable
