b24099a5c91fb6d8d9f500cf58ea634db233e954
aarondill <aaronsacks2006@gmail.com>
2023-10-25 12:26:23 -0500

    assertions in write_outputstream
    Former-commit-id: 0117f00b82c91b8483fe43d0db44a9a395312a0e

--- util/file/write_outputstream.lua
+++ util/file/write_outputstream.lua
@@ -1,3 +1,4 @@
+local iscallable = require("util.iscallable")
 ---@class GioOutputStream
 ---@field write_async fun(self: GioOutputStream, content: string, len: integer, cancellable?: userdata, callback?: fun(self: GioOutputStream, task: userdata))
 ---@field write_finish fun(self: GioOutputStream, task: userdata): new_etags: userdata?, err: userdata?
@@ -7,6 +8,8 @@
 ---@param content string
 ---@param cb? fun(err?: userdata): any?
 local function outputstream_write(stream, content, cb)
+  assert(type(content) == "string", "Content must be a string")
+  assert(iscallable(cb, true))
   --- only pass callback if results are needed.
   ---params(stream:write_async) GOutputStream* stream, void* buffer, gsize count, int io_priority, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
   return stream:write_async(content, content:len(), nil, cb and function(file, task)
