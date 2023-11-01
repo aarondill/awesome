07c956c6070ee14aa5ab02fdc9e53eb13d099baf
aarondill <aaronsacks2006@gmail.com>
2023-11-01 13:27:08 -0500

    refactor persistent-tag to produce less garbage
    Former-commit-id: 3a73bafe54cf62dbf2ad410a4e1ac2ca139d9c7d

--- module/persistent-tag.lua
+++ module/persistent-tag.lua
@@ -1,38 +1,38 @@
 local awful = require("awful")
 local capi = require("capi")
-local gtable = require("gears.table")
 local filepath = "/tmp/awesomewm-last-selected-tags"
 capi.awesome.connect_signal("exit", function(reason_restart)
   if not reason_restart then return end
-  local str = ""
-  for s in capi.screen do
-    local indicies = gtable.map(function(tag)
-      return tag and tag.index
-    end, s.selected_tags)
-    str = ("%s%s\n"):format(str, table.concat(indicies, " "))
-    -- Write synchronously so it's written before awesome closes!
-    local f = assert(io.open(filepath, "w+"))
-    f:write(str)
-    f:close()
+  local t = {}
+  for s in capi.screen do ---@cast s AwesomeScreenInstance
+    local indicies = {}
+    for _, tag in ipairs(s.selected_tags) do
+      table.insert(indicies, tag.index)
+    end
+    table.insert(t, table.concat(indicies, " "))
   end
+  local str = table.concat(t, "\n")
+  -- Write synchronously so it's written before awesome closes!
+  local f = io.open(filepath, "w+")
+  if not f then return error("Could not open file " .. filepath) end
+  f:write(str)
+  return f:close()
 end)
 
 capi.awesome.connect_signal("startup", function()
   -- This is intentionally synchronous to ensure it's done *before* user control.
   local f = io.open(filepath, "r")
   if not f then return end -- file doesn't exist
-  local content = f:read("a")
-  f:close()
-  if not content then return end
 
   local screen_tags = {}
-  for line in content:gmatch("(.*)\n") do
+  for line in f:lines("l") do
     local selected_tags = {}
     for match in line:gmatch("%d+") do
       table.insert(selected_tags, tonumber(match))
     end
     table.insert(screen_tags, selected_tags)
   end
+  f:close()
 
   for s in capi.screen do
     local sel_tags = screen_tags[s.index]
@@ -43,5 +43,5 @@ capi.awesome.connect_signal("startup", function()
     end
   end
 
-  os.remove(filepath)
+  return os.remove(filepath)
 end)
