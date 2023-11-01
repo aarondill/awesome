aa7bee189d8a4400adc3fc79e6e85e80ab89f07c
aarondill <aaronsacks2006@gmail.com>
2023-11-01 13:48:37 -0500

    fullscreen and max are now essentially the same
    Former-commit-id: 38622ab2904ba0561e0ff5adbfed60d7e2336c23

--- configuration/layouts.lua
+++ configuration/layouts.lua
@@ -2,7 +2,6 @@ local layout_suit = require("awful.layout.suit")
 local layouts = {
   layout_suit.tile,
   layout_suit.fair,
-  layout_suit.max,
   layout_suit.max.fullscreen,
   layout_suit.magnifier,
   layout_suit.floating,
