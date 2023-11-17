31522a31f2f70733e5fd2d2dff94e3244b969f83
aarondill <aaronsacks2006@gmail.com>
2023-11-17 14:33:55 -0600

    use table.concat in shell_escape
    Former-commit-id: 706ce37d6ae008ac5982de8814c3fc811aa2e314

--- util/shell_escape.lua
+++ util/shell_escape.lua
@@ -4,9 +4,13 @@ return function(args)
   if type(args) == "string" then args = { args } end
   local ret = {}
   for _, a in pairs(args) do
-    local s = tostring(a) or "nil"
+    local s = tostring(a)
     if s:match("[^A-Za-z0-9_/:-]") then -- If contains special chars
-      s = "'" .. s:gsub("'", "'\\''") .. "'"
+      s = table.concat({
+        "'",
+        s:gsub("'", "'\\''"),
+        "'",
+      })
     end
     table.insert(ret, s)
   end
