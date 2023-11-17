b74b6186414a86ca55ed5fceda65fe004472361f
aarondill <aaronsacks2006@gmail.com>
2023-11-17 00:52:55 -0600

    move normalize_path to a path util module
    Former-commit-id: e8da017684fc9b41de3f4bfb0263977badc3f1fe

--- util/normalize_path.lua
+++ util/normalize_path.lua
@@ -1,30 +1,3 @@
----Removes /../ from the string, repeatedly
----@param path string
-local function remove_leading_backtrack(path)
-  if path:sub(1, 1) ~= "/" then return path end -- If it isn't absolute, return it.
-  local s = 1 + ("/"):len() -- Start of window
-  local e = ("../"):len() + (s - 1) -- End of window
-  while s < path:len() and path:sub(s, e) == "../" do -- while the window is ../ (we already know it starts with /)
-    s = e + 1 -- Move forward the window (in front of previous window)
-    e = e + ("../"):len() -- Move forward the window (in front of previous window)
-  end
-  if s > path:len() then return "/" end
-  local res = "/" .. path:sub(s) -- Remove the leading (/../)+
-  if res == "/.." then return "/" end -- Special case for no trailing slash
-  return res
-end
----Normalizes the path without doing any file operations.
----@param path string
----@return string
-local function normalize_path(path)
-  if not path or path == "" then return path end -- empty path should be the same
-  ---@type string
-  local res = path
-    :gsub("/%./", "/") -- no /./
-    :gsub("^(.+)/$", "%1") -- no end slash -- use .+ instead of .* to keep '/' instead of ''
-    :gsub("//+", "/") -- no double slashes
-  if res:len() == 0 then return "." end -- We have removed everything. This shouldn't happen?
-  return remove_leading_backtrack(res)
-end
+require("gears.debug").deprecate("Use util.path.normalize instead")
 
-return normalize_path
+return require("util.path").normalize
