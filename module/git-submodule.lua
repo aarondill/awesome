79ab0e56de721ab140c548307d05043629f9e54f
aarondill <aaronsacks2006@gmail.com>
2023-11-17 04:11:37 -0600

    shell_escape the git submodule command

--- module/git-submodule.lua
+++ module/git-submodule.lua
@@ -1,12 +1,13 @@
 local gfile = require("gears.filesystem")
 local notifs = require("util.notifs")
+local shell_escape = require("util.shell_escape")
 
 -- NEVER use io.popen. But, I *need* this to be synchronous so the modules are available later in the code.
 -- So, this is a rare exception. This command should only take a long time the first time it is called.
 if not io.popen then return end
 
 -- -C=run in this directory, since lua doesn't support 'cd'ing
-local cmd = string.format("git -C '%s' submodule update --init", gfile.get_configuration_dir())
+local cmd = shell_escape({ "git", "-C", gfile.get_configuration_dir(), "submodule", "update", "--init" })
 
 local file = io.popen(cmd, "r")
 if not file then return end
