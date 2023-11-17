c15e7512f5673009a52e1dd705cfe95723579133
aarondill <aaronsacks2006@gmail.com>
2023-11-17 03:29:25 -0600

    use GLib instead of a hand-made which function
    Former-commit-id: e6dd75ecea3fa90ea37b291b235d882fda32829d

--- util/make_clickable_if_prog.lua
+++ util/make_clickable_if_prog.lua
@@ -2,7 +2,9 @@ local abutton = require("awful.button")
 local bind = require("util.bind")
 local clickable_container = require("widget.material.clickable-container")
 local replace_in_widget = require("util.replace_in_widget")
-local which = require("util.which")
+local GLib = require("lgi").GLib
+local which = GLib.find_program_in_path ---@type fun(prog: string): string?
+
 -- replace_if_present(cmd, replace_widget, replace_in, function(path, replace_widget, replace_in)
 -- Do something with path (guarenteed to be non-nil)
 -- end)
@@ -12,8 +14,7 @@ local which = require("util.which")
 ---@param cb fun(path:string, replace_widget:table, replace_in:table)
 local function make_clickable_if_prog(cmd, replace_widget, replace_in, cb)
   if not cmd or not replace_widget or not replace_in or not cb then error("Replace_if_present requires 4 arguments") end
-  if type(cmd) == "table" then cmd = cmd[1] end
-  if not cmd then return end
+  if type(cmd) == "table" then cmd = assert(cmd[1]) end
   local path_or_nil = which(cmd)
   if not path_or_nil then return end
   local buttons = abutton({}, 1, nil, bind.with_args(cb, path_or_nil, replace_widget, replace_in)) -- Call callback on click
