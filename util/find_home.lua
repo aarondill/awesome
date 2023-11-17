04189ccad33c544137d1269d56db195b042872a1
aarondill <aaronsacks2006@gmail.com>
2023-11-17 04:12:18 -0600

    use GLib.get_home_dir() for find_home.lua

--- util/find_home.lua
+++ util/find_home.lua
@@ -1,31 +1,14 @@
+local lgi = require("lgi")
 local path = require("util.path")
-local home_cached ---@type string? -- Don't directly access. Use get_and_cache_home
-
-local function get_and_cache_home()
-  if home_cached then return home_cached end
-
-  local home = os.getenv("HOME")
-  if not home then
-    local file = assert(io.popen([[ getent passwd "${USER:-$(id -nu)}" | cut -d: -f6 ]]))
-    home = file:read("l") ---@type string
-    file:close()
-  end
-  ---Remove trailing slashes
-  local ret = #home == 0 and "." or path.normalize(home)
-  home_cached = ret
-  return ret
-end
+local GLib = lgi.GLib
 
 --- Finds a valid HOME for the user
---- Tries to handle the case where HOME is unset
---- Use the gears.filesystem.get_xdg_* functions instead if possible
---- Calls io.popen if HOME is unset, but caches the result, so only one call will be made.
 --- Note: absolute paths will still be appended to home. IE: `/file`, `./file`, and `file` are all equivalent.
 ---@param file string? a file to find under $HOME
 ---@return string home
 local function find_home(file)
-  local home = get_and_cache_home()
+  local home = GLib.get_home_dir() ---@type string
   if not file then return home end
-  return path.normalize(("%s/%s"):format(home, file))
+  return path.resolve(home, file)
 end
 return find_home
