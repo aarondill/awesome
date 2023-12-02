62b9f31a6246ed7b4428f7c1398535289328b01c
aarondill <aaronsacks2006@gmail.com>
2023-12-02 14:32:53 -0600

    add has_key and get to table utils

--- util/table.lua
+++ util/table.lua
@@ -49,4 +49,39 @@ function M.foreach(t, f)
   end
 end
 
+---has_key({a={b=1}}, 'a', 'b') returns true
+---Checks if a key path exists in the table.
+---
+---@param tbl table The table to check for the key in
+---@param ... unknown A keypath to check for
+---@return boolean
+---@return unknown|nil value located at the keypath (to save double iterations)
+function M.has_key(tbl, ...)
+  local keys = table.pack(...)
+
+  assert(type(tbl) == "table", ("table expected, got %s"):format(type(tbl)))
+  assert(keys.n > 0, "No keys provided")
+  local count = 0
+  local tmp = tbl ---@type unknown
+  for _, k in ipairs(keys) do
+    if type(tmp) ~= "table" then return false, nil end -- not a table
+    local v = tmp[k]
+    if v == nil then return false, nil end
+    tmp = tmp[k]
+    count = count + 1
+  end
+  -- There's a nil key in the list. Nil keys are not allowed in lua
+  if count < keys.n then return false, nil end
+  return true, tmp
+end
+
+---Gets the value at the keypath. Returns nil if the keypath does not exist
+---@param tbl table
+---@param ... unknown a keypath to get
+---@return unknown|nil
+function M.get(tbl, ...)
+  local has, v = M.has_key(tbl, ...)
+  return has and v or nil
+end
+
 return M
