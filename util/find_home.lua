b9646dc316afbbff35df542bcf8427a2bbcb9275
aarondill <aaronsacks2006@gmail.com>
2023-12-11 00:49:40 -0600

    add get_home_dir to glib

--- util/find_home.lua
+++ util/find_home.lua
@@ -1,13 +1,12 @@
-local lgi = require("lgi")
+local GLib = require("util.lgi").GLib
 local path = require("util.path")
-local GLib = lgi.GLib
 
 --- Finds a valid HOME for the user
 --- Note: absolute paths will still be appended to home. IE: `/file`, `./file`, and `file` are all equivalent.
 ---@param file string? a file to find under $HOME
 ---@return string home
 local function find_home(file)
-  local home = GLib.get_home_dir() ---@type string
+  local home = GLib.get_home_dir()
   if not file then return home end
   return path.resolve(home, file)
 end
