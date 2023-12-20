ea9e2a9849d829514aada75dcad77b67de1e7b20
aarondill <aaronsacks2006@gmail.com>
2023-12-20 03:48:35 -0600

    add close field to GioOutputStream class def
    Former-commit-id: a8b2806e2620ef1b655b2a5ec639be7ad50efe20

--- util/file/write_outputstream.lua
+++ util/file/write_outputstream.lua
@@ -3,6 +3,7 @@ local assert_util = require("util.assert_util")
 ---@class GioOutputStream
 ---@field write_async fun(self: GioOutputStream, content: string, len: integer, cancellable?: userdata, callback?: fun(self: GioOutputStream, task: userdata))
 ---@field write_finish fun(self: GioOutputStream, task: userdata): new_etags: userdata?, err: userdata?
+---@field close fun(self: GioOutputStream)
 
 --- Write to a stream - async
 ---@param stream GioOutputStream
