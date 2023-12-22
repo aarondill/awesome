4600653c7508725d72f6fce89b26b7f6aa844271
aarondill <aaronsacks2006@gmail.com>
2023-12-22 04:20:30 -0600

    path.resolve should *always* return an absolute path
    Former-commit-id: 453760503e218e30226f4cbb29b9cdddc3d5a8b9

--- util/path/resolve.lua
+++ util/path/resolve.lua
@@ -1,13 +1,16 @@
 local require = require("util.rel_require")
 
+local init = require(..., "init") ---@module "util.path"
 local join = require(..., "join") ---@module 'util.path.join'
 local normalize = require(..., "normalize") ---@module 'util.path.normalize'
 
 ---exactly equivalent to path.normalize(path.join(...), true)
+---Except that if no path is passed, the result is the root, instead of the empty string.
 ---Returns a normalized absolute path
 ---@param ... string
 ---@return string
 local function resolve(...)
-  return normalize(join(...), true)
+  local p = normalize(join(...), true)
+  return p ~= "" and p or init.root
 end
 return resolve
