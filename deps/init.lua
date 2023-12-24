1eea131377575b985c78a3279e00aae68d25a9b1
aarondill <aaronsacks2006@gmail.com>
2023-12-24 02:29:00 -0600

    move /deps/build to /deps/.build
    for the sake of making it clear it's not a full dependency directory
    
    
    Former-commit-id: 08057a87082cfb8081e2dfbee3427039278c860b

--- deps/init.lua
+++ deps/init.lua
@@ -2,7 +2,12 @@ local path = require("util.path")
 local source_path = require("util.source_path")
 
 local version = _VERSION:match("%d+%.%d+")
-local build_dir = path.resolve(source_path.dirname(), "build") -- This file -> ./build
+if not version then
+  require("util.notifs").error("Could not determine lua version!")
+  return { path = {}, cpath = {} }
+end
+
+local build_dir = path.resolve(source_path.dirname(), ".build") -- This file -> ./.build
 -- These are directories that should be added to pacakge.[c]path to make dependencies requireable
 return {
   path = { ---@type string[]
