c6e20347988303239f3db2a14210f7c872ea6277
aarondill <aaronsacks2006@gmail.com>
2023-12-27 06:34:21 -0600

    make exit-screen module more dynamic
    You can now rearrange things from just one table

--- module/exit-screen.lua
+++ module/exit-screen.lua
@@ -11,18 +11,39 @@ local icons = require("theme.icons")
 local systemctl_cmd = require("util.systemctl_cmd")
 local wibox = require("wibox")
 local dpi = require("beautiful").xresources.apply_dpi
+local bind = require("util.bind")
 local compat = require("util.compat")
+local tables = require("util.tables")
 
 -- Appearance
 local icon_size = beautiful.exit_screen_icon_size or dpi(140)
 
+-- Create the widget
+local exit_screen = wibox({
+  visible = false,
+  ontop = true,
+  type = "splash",
+})
+local exit_screen_grabber
+local function exit_screen_hide()
+  akeygrabber.stop(exit_screen_grabber)
+  exit_screen.visible = false
+end
+---@param key ExitScreenKey
+---@param type 'key'|'click'
+local function run_cmd(key, type)
+  exit_screen_hide() -- Always hide the exit screen first!
+  if not key.cmd then return end
+  return key:cmd(type)
+end
+
 ---Build a clickable button for the exit screen
----@param icon string the path to the icon to show
----@param text string the text to display under the button
----@param on_release function? passed to connect_signal button::release.
----Passing this ensures only the icon/margin is clickable, rather than the caption below.
+---@param key ExitScreenKey
 ---@return table button the button widget to show on the exit_screen
-local function buildButton(icon, text, on_release)
+local function buildButton(key)
+  local title = key[1] or "<No Text Provided>"
+  local k = key[2]
+  local text = k and ("%s (%s)"):format(title, k) or title
   local clickable = wibox.widget({
     widget = clickable_container,
     shape = gshape.circle,
@@ -31,10 +52,11 @@ local function buildButton(icon, text, on_release)
     {
       widget = wibox.container.margin,
       margins = dpi(16),
-      wibox.widget.imagebox(icon),
+      wibox.widget.imagebox(key.icon),
     },
   })
-  local widget = wibox.widget({
+  clickable:connect_signal("button::release", bind.with_args(run_cmd, key, "click"))
+  return wibox.widget({
     {
       widget = wibox.container.margin,
       left = dpi(24),
@@ -49,18 +71,8 @@ local function buildButton(icon, text, on_release)
     },
     layout = wibox.layout.fixed.vertical,
   })
-
-  if on_release then clickable:connect_signal("button::release", on_release) end
-
-  return widget
 end
 
--- Create the widget
-local exit_screen = wibox({
-  visible = false,
-  ontop = true,
-  type = "splash",
-})
 local function update_wibox_screen(s) ---@param s AwesomeScreenInstance?
   if not s then return end
   if exit_screen.screen ~= s then
@@ -89,58 +101,46 @@ local bg = beautiful.exit_screen_bg or beautiful.wibar_bg or beautiful.bg_normal
 exit_screen.bg = bg:match("^#......") .. "DD" -- light transparency
 exit_screen.fg = beautiful.exit_screen_fg or beautiful.wibar_fg or beautiful.fg_normal or "#FEFEFE"
 
-local exit_screen_grabber
-
-local function exit_screen_hide()
-  akeygrabber.stop(exit_screen_grabber)
-  exit_screen.visible = false
-end
-
-local function exit_command()
-  exit_screen_hide()
-  capi.awesome.quit(0)
-end
-local function lock_command()
-  exit_screen_hide()
-  apps.open.lock()
-end
 local function suspend_command()
-  exit_screen_hide()
-  apps.open.lock() -- This doesn't block
-  systemctl_cmd("suspend-then-hibernate")
-end
-local function poweroff_command()
-  exit_screen_hide()
-  systemctl_cmd("poweroff")
-end
-local function reboot_command()
-  exit_screen_hide()
-  systemctl_cmd("reboot")
+  apps.open.lock()
+  return systemctl_cmd("suspend-then-hibernate")
 end
 
-local poweroff = buildButton(icons.power, "Poweroff (p)", handle_error(poweroff_command))
-local reboot = buildButton(icons.restart, "Restart (r)", handle_error(reboot_command))
-local suspend = buildButton(icons.sleep, "Suspend-Then-Hibernate (s)", handle_error(suspend_command))
-local exit = buildButton(icons.logout, "Exit AWM (e)", handle_error(exit_command))
-local lock = buildButton(icons.lock, "Lock (l)", handle_error(lock_command))
+---@class ExitScreenKey
+---@field [1] string the display text of the button (note: the key will be appended)
+---@field [2]? string the key to be pressed
+---@field cmd? fun(self: ExitScreenKey, type: 'click'|'key'): any
+---@field icon? string The path to the icon to be displayed
+
+---@type string[]|true
+---Note: if exit_keys is `true` then any unrecognized keys will exit
+local exit_keys = true --- { "Escape", "q", "x" }
+local keys = { ---@type ExitScreenKey[]
+  { "Poweroff", "p", cmd = bind.with_args(systemctl_cmd, "poweroff"), icon = icons.power },
+  { "Restart", "r", cmd = bind.with_args(systemctl_cmd, "reboot"), icon = icons.restart },
+  { "Suspend-Then-Hibernate", "s", cmd = suspend_command, icon = icons.sleep },
+  { "Exit AWM", "e", cmd = bind.with_args(capi.awesome.quit, 0), icon = icons.logout },
+  { "Lock", "l", cmd = apps.open.lock, icon = icons.lock },
+}
+local buttons = tables.map(keys, buildButton) -- Note: keeps order of keys array
 
 local function exit_screen_show()
   exit_screen_grabber = akeygrabber.run(handle_error(function(mods, key, event)
-    if event == "release" or not #mods == 0 then return false end
+    if event == "release" or not #mods == 0 then return false end -- this isn't my event!
 
-    if key == "s" then
-      return suspend_command()
-    elseif key == "e" then
-      return exit_command()
-    elseif key == "l" then
-      return lock_command()
-    elseif key == "p" then
-      return poweroff_command()
-    elseif key == "r" then
-      return reboot_command()
-    elseif key == "Escape" or key == "q" or key == "x" then
-      return exit_screen_hide()
+    for _, v in ipairs(keys) do
+      if key == v[2] then
+        run_cmd(v, "key") -- call the cmd
+        return true -- we handled this event
+      end
     end
+
+    if exit_keys == true or gtable.hasitem(exit_keys, key) then
+      exit_screen_hide()
+      return true -- we handled this event
+    end
+
+    return false -- we didn't handle this event
   end))
   exit_screen.visible = true
 end
@@ -157,12 +157,8 @@ exit_screen:setup({
   {
     nil, -- No left
     { -- This should be centered
-      poweroff,
-      reboot,
-      suspend,
-      exit,
-      lock,
       layout = wibox.layout.fixed.horizontal,
+      table.unpack(buttons), -- Note: this must be last!
     },
     nil, -- No right
     expand = "none",
