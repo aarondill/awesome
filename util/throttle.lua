accd37f97b1e4f5f74a7d76f0bed36edf92c7ac6
aarondill <aaronsacks2006@gmail.com>
2024-01-17 01:14:41 -0600

    fix: assert that delay > 0 in throttle

--- util/throttle.lua
+++ util/throttle.lua
@@ -1,9 +1,14 @@
+local assert_util = require("util.assert_util")
 local gtimer = require("gears.timer")
 ---Returns a throttled version of f
 ---@param f fun(...: unknown): any
 ---@param delay integer seconds
 ---@return fun(...: unknown)
 local function throttle(f, delay)
+  assert_util.iscallable(f, false, "f", 2)
+  assert_util.type(delay, "number", "delay", 2)
+  assert_util.assert(delay > 0, "delay must be positive", 2)
+
   -- We have to use a timer to allow milliseconds (os.time can only support seconds)
   local timer = gtimer.new({
     timeout = delay,
