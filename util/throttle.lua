39e8f844588a4696dbdb84ef8620c4cc7f30676a
aarondill <aaronsacks2006@gmail.com>
2024-01-17 01:09:45 -0600

    fix: allow fractional seconds in throttle

--- util/throttle.lua
+++ util/throttle.lua
@@ -1,14 +1,18 @@
+local gtimer = require("gears.timer")
 ---Returns a throttled version of f
 ---@param f fun(...: unknown): any
 ---@param delay integer seconds
 ---@return fun(...: unknown)
 local function throttle(f, delay)
-  ---@type integer
-  local last_called
+  -- We have to use a timer to allow milliseconds (os.time can only support seconds)
+  local timer = gtimer.new({
+    timeout = delay,
+    single_shot = true, -- only run once
+  })
   return function(...)
-    -- We've already called, and it was too soon
-    if last_called and os.difftime(last_called, os.time()) < delay then return end
-    last_called = os.time() -- Update on successful call
+    --- timer.started will be false if it's ended (because single_shot)
+    if timer.started then return end
+    timer:again()
     f(...)
   end
 end
