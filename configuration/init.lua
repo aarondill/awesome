a29f081d168a8a6a5ef791502f29eb6cb569d946
aarondill <aaronsacks2006@gmail.com>
2024-01-24 15:43:59 -0600

    fix: throttle tag-list when scrolling with mouse
    Former-commit-id: 26ab6f8814105ed36ec81532dd0f96d54cd8a52e

--- configuration/init.lua
+++ configuration/init.lua
@@ -1,7 +1,21 @@
 local require = require("util.rel_require")
-return {
-  apps = require(..., "apps"), ---@module "configuration.apps"
-  keys = require(..., "keys"), ---@module "configuration.keys"
-  layouts = require(..., "layouts"), ---@module "configuration.layouts"
-  debug_autostart_failures = false,
+---@diagnostic disable: assign-type-mismatch
+local M = {
+  apps = nil, ---@module "configuration.apps"
+  keys = nil, ---@module "configuration.keys"
+  layouts = nil, ---@module "configuration.layouts"
 }
+---@diagnostic enable: assign-type-mismatch
+
+--- Send a notification if autostart fails
+M.debug_autostart_failures = false ---@type boolean
+--- throttle delay on tag changes (seconds)
+M.tag_throttle_delay = 0.25 ---@type number
+
+local this_path = ...
+return setmetatable(M, {
+  __index = function(_, key)
+    local m = require(this_path, key, false) -- No TCO!
+    return m
+  end,
+})
