85be2d45946fcb7b3e402e44115648647325986d
aarondill <aaronsacks2006@gmail.com>
2024-02-12 04:40:15 -0600

    feat: add deep_equal util in tables

--- util/tables.lua
+++ util/tables.lua
@@ -120,4 +120,24 @@ function M.get(tbl, ...)
   return has and v or nil
 end
 
+--- Deep compare values for equality
+--- @source /usr/share/nvim/runtime/lua/vim/shared.lua
+--- Tables are compared recursively unless they both provide the `eq` metamethod.
+--- All other types are compared using the equality `==` operator.
+---@param a any First value
+---@param b any Second value
+---@return boolean `true` if values are equals, else `false`
+function M.deep_equal(a, b)
+  if a == b then return true end
+  if type(a) ~= type(b) then return false end
+  if type(a) ~= "table" then return false end
+  for k, v in pairs(a) do
+    if not M.deep_equal(v, b[k]) then return false end
+  end
+  for k, _ in pairs(b) do
+    if a[k] == nil then return false end
+  end
+  return true
+end
+
 return M
