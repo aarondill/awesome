72ac6cf475ef5a91716abafac254df28dbcec371
aarondill <aaronsacks2006@gmail.com>
2024-02-16 15:36:13 -0600

    refactor: names in quake.lua
    Former-commit-id: bd8901bc796e50dccbcf7c600981d6fec25d71f2

--- module/quake.lua
+++ module/quake.lua
@@ -1,18 +1,18 @@
 local apps = require("configuration.apps")
 local bind = require("util.bind")
 local capi = require("capi")
-local quake_widget = require("widget.quake")
+local quake = require("widget.quake")
 
 local quake_class = "QuakeDD"
 --- Create a quake terminal
-local quake_instance = quake_widget({ spawn = apps.open.quake_terminal, class = quake_class })
-capi.awesome.connect_signal("quake::toggle", bind(quake_instance.toggle, quake_instance))
-capi.awesome.connect_signal("quake::kill", bind(quake_instance.kill, quake_instance))
+local quakei = quake.new({ spawn = apps.open.quake_terminal, class = quake_class })
+capi.awesome.connect_signal("quake::toggle", bind(quakei.toggle, quakei))
+capi.awesome.connect_signal("quake::kill", bind(quakei.kill, quakei))
 
-local quake = {}
-function quake:client_is_quake(c) ---@param c AwesomeClientInstance
+local M = {}
+function M:client_is_quake(c) ---@param c AwesomeClientInstance
   return c.instance == quake_class
 end
 ---This is the class used for the quake terminal. Use it only in awful.rules.
-quake.instance = quake_class
-return quake
+M.instance = quake_class
+return M
