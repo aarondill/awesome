7e2d967abd1e5890c3d795fdec4679a4ca63dfee
aarondill <aaronsacks2006@gmail.com>
2024-02-16 15:35:37 -0600

    fix: names in tbl_concaty

--- util/tables/tbl_concat.lua
+++ util/tables/tbl_concat.lua
@@ -1,10 +1,10 @@
 local rel_require = require("util.rel_require")
-local join = rel_require(..., "join") ---@module "util.tables.tbl_join"
+local tbl_join = rel_require(..., "tbl_join") ---@module "util.tables.tbl_join"
 ---Concat all arguments into a copy of t1, returns a new table.
 ---@param t1 unknown[]
 ---@param ... unknown
 ---@return table joined a new table containing the concatenation
 return function(t1, ...)
   local t2 = table.pack(...)
-  return join(t1, t2)
+  return tbl_join(t1, t2)
 end
