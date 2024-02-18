a4205f2a835155a226d0c9c382094dd2ffaa059c
aarondill <aaronsacks2006@gmail.com>
2024-02-18 09:21:08 -0600

    fix: compile luaposix with each installed lua interpreter
    this avoid accidentally compiling with the wrong version. it also allows me to simultainously run awesome on two different versions.
    
    
    Former-commit-id: b64e0df1ba234ce77bfad52f4b129ed660d53f5b

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -euC
 log() { printf '%s\n' "$@" || true; }
 err() { printf '%s\n' "$@" >&2 || true; }
+abort() { err "$1" && exit "${2:-1}"; }
 has() { [ -x "$(command -v "$1" 2>/dev/null)" ]; }
 
 # Ensure we're in the correct directory
@@ -49,18 +50,29 @@ if [ "$install" -eq 1 ]; then
 fi
 
 # Use the AWESOME and LUA variables to configure this if needed
+[ -n "${LUA:-}" ] || LUA=()
 AWESOME=${AWESOME:-awesome}
-if [ -z "${LUA:-}" ]; then
-  LUA_VERSION=$("$AWESOME" --version | perl -ne 'm/running with Lua (\d+.\d+)/ && print "$1"')
-  LUA="lua$LUA_VERSION"
+if [ "${#LUA[@]}" -eq 0 ]; then
+  declare -A lua_hash=() # Remove duplicate entries automatically
+  while read -r -d: path; do
+    path=${path:-"$PWD"} # empty path means pwd
+    [ -d "$path" ] || continue
+    for f in "$path"/lua*; do
+      [ -x "$f" ] || continue
+      case "$f" in */luac* | */luajit*) continue ;; esac # I'm not looking for luajit or luac
+      f=$(realpath -- "$f") || continue
+      lua_hash["$f"]=1
+    done
+  done < <(printf '%s' "$PATH:")
+  LUA=("${!lua_hash[@]}") lua_hash=()
+
+  lua_version=$("$AWESOME" --version | perl -ne 'm/running with Lua (\d+.\d+)/ && print "$1"')
+  has "lua$lua_version" || abort "Could not find lua $lua_version required for awesome $AWESOME!" 1
 fi
 
 # Check that lua is installed.
 # Do this to allow the user to choose their preferred lua version (since 'lua' is a virtual package in debian/ubuntu)
-if ! has "$LUA"; then
-  err "Could not find '$LUA'! Please make sure your lua installation matches awesome's compiled version (see awesome --help)"
-  exit 1
-fi
+[ "${#LUA[@]}" -gt 0 ] || abort "Could not find any lua installations!" 1
 
 git submodule update --init --recursive
 
@@ -68,13 +80,14 @@ AR_LAUNCHER_DIR="$dir/deps/autorandr/contrib/autorandr_launcher"
 log "Compiling autorandr_launcher (${AR_LAUNCHER_DIR#"$dir/"})"
 (cd "$AR_LAUNCHER_DIR" && make -s)
 
-LUAPOSIX_DIR="$dir/deps/luaposix"
-LUAPOSIX_DEST="$dir/deps/.build"
-log "Compiling luaposix (${LUAPOSIX_DIR#"$dir/"})"
-(
-  cd "$LUAPOSIX_DIR"
+LUAPOSIX_DIR="$dir/deps/luaposix" LUAPOSIX_DEST="$dir/deps/.build"
+
+pushd "$LUAPOSIX_DIR" >/dev/null
+for lua in "${LUA[@]}"; do
+  log "Compiling luaposix (${LUAPOSIX_DIR#"$dir/"}) using lua $lua"
   # Note: if using lua 5.1, we may be missing the 'bit32' module!
-  "$LUA" "$LUAPOSIX_DIR/build-aux/luke" --quiet LDOC='true' # build luaposix, set LDOC to 'true' to avoid compiling docs
+  "$lua" "$LUAPOSIX_DIR/build-aux/luke" --quiet LDOC='true' # build luaposix, set LDOC to 'true' to avoid compiling docs
   log "Installing luaposix to ${LUAPOSIX_DEST#"$dir/"}"
-  "$LUA" "$LUAPOSIX_DIR/build-aux/luke" --quiet PREFIX="$LUAPOSIX_DEST" install # 'install' to ./deps/.build so these can be required
-)
+  "$lua" "$LUAPOSIX_DIR/build-aux/luke" --quiet PREFIX="$LUAPOSIX_DEST" install # 'install' to ./deps/.build so these can be required
+done
+popd >/dev/null
