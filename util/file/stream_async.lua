e2b6447922d4268281c37a76d0c307682bdb5808
aarondill <aaronsacks2006@gmail.com>
2024-02-18 15:17:05 -0600

    feat: use GSubprocess in git-submodule
    Former-commit-id: 487081a32c556934760f21c229f52d36ad6641f4

--- util/file/stream_async.lua
+++ util/file/stream_async.lua
@@ -2,19 +2,6 @@ local lgi = require("lgi")
 local glib = lgi.GLib
 local gio = lgi.Gio
 
----@class GInputStream
----@field read_bytes_async fun(self: GInputStream, count: integer, io_p: integer, cancel, cb?: fun(source: GInputStream, task: unknown))
----@field skip_async fun(self: GInputStream, count: integer, io_p: integer, cancel, cb?: fun(source: GInputStream, task: unknown))
----@field seek fun(self: GInputStream, offset, type): suc: boolean, error: userdata
----@field tell fun(self: GInputStream): integer -- Zero if not seekable
----@field close_async fun(self: GInputStream, io_p: integer, cancel, cb?: fun(source: GInputStream, task: unknown))
----@field read_bytes_finish fun(source: GInputStream, task: unknown): gbytes: table?, error: userdata?
----@field skip_finish fun(source: GInputStream, task: unknown): skipped: integer, error: userdata?
-
----@class GDataInputStream :GInputStream
----@field read_line_async fun(self: GDataInputStream,io_p:integer, cancel, cb: fun(source: GDataInputStream, task:unknown))
----@field read_line_finish fun(source: GDataInputStream, task:unknown): line: string, len_or_err: number|userdata
-
 ---@class GInputStreamAsyncHelper
 ---@field data_stream GDataInputStream?
 ---@field stream GInputStream
