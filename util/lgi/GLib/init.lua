0110499397ee9f5cb76869c35aa2d524d2710e38
aarondill <aaronsacks2006@gmail.com>
2024-02-18 15:02:23 -0600

    feat: add GBytes types
    Former-commit-id: bf3f97d78e954eded55f4872e6956bd721d36213

--- util/lgi/GLib/init.lua
+++ util/lgi/GLib/init.lua
@@ -18,10 +18,19 @@
 ---@field Error GErrorStatic
 ---@field Bytes GBytesStatic
 
----@class GBytesStatic TODO:
----@field new fun(string): GBytes
+---@class GBytesStatic
+---@field new fun(content: string): GBytes
 
----@class GBytes TODO:
+---@class GBytes
+---@field compare fun(self: GBytes, other: GBytes): integer
+---@field equal fun(self: GBytes, other: GBytes): boolean
+---@field get_data fun(self: GBytes): string? Note: if length == 0, then nil is returned
+---@field get_region fun(self: GBytes, element_size: integer, offset: integer, n_elements: integer): userdata this is currently unusable in lua
+---@field get_size fun(self: GBytes): integer
+---@field hash fun(self: GBytes): integer
+---@field new_from_bytes fun(self: GBytes, offset: integer, length: integer): GBytes
+---This is added by lgi -- it also allows using #bytes to get the size
+---@field data string? Note: if length == 0, then nil is returned
 
 ---@type lgi
 local lgi = require("lgi")
