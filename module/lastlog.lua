fd684ca831cde777118ad5166bb749ed927fe098
aarondill <aaronsacks2006@gmail.com>
2024-02-28 11:16:34 -0600

    fix: use last instead of lastlog to notify the last login
    lastlog always returns the current session

--- module/lastlog.lua
+++ module/lastlog.lua
@@ -1,15 +1,20 @@
 local GLib = require("util.lgi.GLib")
+local gstring = require("gears.string")
 local notifs = require("util.notifs")
 local spawn = require("util.spawn")
 local strings = require("util.strings")
 
 local user = GLib.get_user_name()
-spawn.async({ "lastlog", "-u", user }, function(stdout, _, reason, code)
+spawn.async({ "last", "-n2", "--", user }, function(stdout, _, reason, code)
   if not spawn.is_normal_exit(reason, code) then return end
-  local lastlog = strings.split(stdout, "\n")[2]
+  local lines = strings.split(stdout, "\n")
+  local lastlog = lines[2]
+  if not user or not gstring.startswith(lastlog, user) then
+    lastlog = lines[1] -- in case there's only one line
+  end
   if not lastlog then return end
   local timedate = lastlog:match("^%S+%s+%S+%s+(.*)")
-  timedate = timedate:match("(.*)%s%-%d+ %d*") or timedate -- Attempt to remove time zone and year
-  if not timedate then return notifs.error("Could not parse lastlog output") end
+  timedate = timedate:match("(.*)%s%-.+") or timedate -- Attempt to remove log out
+  if not timedate then return notifs.error("Could not parse last output") end
   return notifs.info("Last login: " .. tostring(timedate))
 end)
