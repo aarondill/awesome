b4b9fceb8e76479d32a4ceb11930685215b1d269
aarondill <aaronsacks2006@gmail.com>
2024-04-30 22:24:11 -0500

    chore: format key functions better
    Fixes weird formatting of one-line functions by adding an empty comment

--- configuration/keys/client.lua
+++ configuration/keys/client.lua
@@ -24,41 +24,29 @@ local clientkeys = gtable.join(
 
   ckey({ modkey }, "q", function(c) c:kill() end, { description = "close", group = "client" }),
 
-  ckey(
-    { modkey, "Control" },
-    "s",
-    function(c) c.sticky = not c.sticky end,
-    { description = "toggle sticky", group = "client" }
-  ),
+  ckey({ modkey, "Control" }, "s", function(c) --
+    c.sticky = not c.sticky
+  end, { description = "toggle sticky", group = "client" }),
   ckey({ modkey, "Control" }, "space", aclient.floating.toggle, { description = "toggle floating", group = "client" }),
   ckey({ modkey, "Shift" }, "Return", function(c)
     local master = aclient.getmaster() ---@type AwesomeClientInstance?
     if master then return c:swap(master) end
   end, { description = "move to master", group = "client" }),
 
-  ckey(
-    { modkey, "Shift", "Control" },
-    "k",
-    function(c) c:move_to_screen() end,
-    { description = "move window to next screen", group = "client" }
-  ),
+  ckey({ modkey, "Shift", "Control" }, "k", function(c) --
+    c:move_to_screen()
+  end, { description = "move window to next screen", group = "client" }),
   ckey({ modkey, "Shift", "Control" }, "j", function(c)
     local s = c.screen and c.screen.index - 1 -- Might be nil, thus +1, but there's a bigger problem here
     c:move_to_screen(s)
   end, { description = "move window to prev screen", group = "client" }),
 
-  ckey(
-    { modkey },
-    "o",
-    function(c) c.opacity = math.min(c.opacity + 0.1, 1) end,
-    { description = "Increase opacity", group = "client" }
-  ),
-  ckey(
-    { modkey, "Shift" },
-    "o",
-    function(c) c.opacity = math.max(c.opacity - 0.1, 0.2) end,
-    { description = "Decrease opacity", group = "client" }
-  ),
+  ckey({ modkey }, "o", function(c) --
+    c.opacity = math.min(c.opacity + 0.1, 1)
+  end, { description = "Increase opacity", group = "client" }),
+  ckey({ modkey, "Shift" }, "o", function(c) --
+    c.opacity = math.max(c.opacity - 0.1, 0.2)
+  end, { description = "Decrease opacity", group = "client" }),
 
   ckey({ modkey }, "t", function(c) c.ontop = not c.ontop end, { description = "toggle keep on top", group = "client" }),
   ckey({ modkey }, "n", function(c)
@@ -79,11 +67,8 @@ local clientkeys = gtable.join(
     c:raise()
   end, { description = "(un)maximize horizontally", group = "client" }),
 
-  ckey(
-    { modkey },
-    "`",
-    function(c) atitlebar.toggle(c, "top") end,
-    { description = "toggle top titlebar", group = "client" }
-  )
+  ckey({ modkey }, "`", function(c) --
+    atitlebar.toggle(c, "top")
+  end, { description = "toggle top titlebar", group = "client" })
 )
 return clientkeys
