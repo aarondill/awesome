d7f18bd48499cc33edf2cf7d092745ecfd7c6ec7
aarondill <aaronsacks2006@gmail.com>
2024-05-05 03:37:32 -0500

    refactor: change environment config to be a table

--- configuration/environment.lua
+++ configuration/environment.lua
@@ -4,17 +4,35 @@ local handle_error = require("util.handle_error")
 local path = require("util.path")
 
 ---@param env string
----@param val string?
+---@param val (string|false)? Note: if false or nil is passed, unsets
 ---@param overwrite boolean? default to true
 ---@return boolean changed
 local function setenv(env, val, overwrite)
   if overwrite == nil then overwrite = true end
-  if not val then
-    if not os.getenv(env) then return false end
-    glib.unsetenv(env)
-    return true
+  if val then return glib.setenv(env, val, overwrite) end
+  if not os.getenv(env) then return false end
+  glib.unsetenv(env)
+  return true
+end
+
+---@param env table<string, (string|false)?> remember that `nil` keys aren't accessable!
+---@param overwrite boolean? default to true
+---@return boolean changed
+local function setenv_tbl(env, overwrite)
+  local changed = false
+  for k, v in pairs(env) do
+    if setenv(k, v, overwrite) then changed = true end
   end
-  return glib.setenv(env, val, overwrite)
+  return changed
+end
+
+---@param progs string[]
+---@return string?, string?
+local function first_in_path(progs)
+  for _, p in ipairs(progs) do
+    local ppath = glib.find_program_in_path(p)
+    if ppath then return p, ppath end
+  end
 end
 
 local function setup_environment()
@@ -22,19 +40,23 @@ local function setup_environment()
   -- A default PATH. If $PATH is unset, there's bigger problems.
   local pathvar = os.getenv("PATH") or table.concat({ "/usr/bin", "/bin", "/usr/sbin", "/sbin" }, path.delimiter)
   pathvar = table.concat({ deps_bindir, pathvar }, path.delimiter)
-  setenv("PATH", pathvar) -- Set the PATH environment variable to include /deps/.bin/
 
-  setenv("GTK_THEME", "Yaru:dark") -- Prefer dark theme
-  setenv("GTK_IM_MODULE", "ibus") -- Fix for browsers
-  setenv("QT_IM_MODULE", "ibus") -- Not sure if this works or not, but whatever
-  setenv("XMODIFIERS", "@im=ibus")
-  setenv("XDG_CURRENT_DESKTOP", "GNOME:AWESOME")
-  setenv("QT_QPA_PLATFORMTHEME", "gtk2")
-  setenv("QT_STYLE_OVERRIDE", "gtk2")
-  setenv("SHLVL", nil) -- Fix terminals opened in AwesomeWM
-  setenv("NO_AT_BRIDGE", "1") -- Don't try to connect to an accessibility bus
-  setenv("_JAVA_AWT_WM_NONREPARENTING", "1") -- expose awesome as a non-reparenting window to java
-  setenv("ZEITGEIST_LOG_DIRECT_READ", "1") -- Fix a bug in diodon
+  local ibus = first_in_path({ "ibus", "fcitx" }) -- IBUS --
+
+  return setenv_tbl({
+    GTK_IM_MODULE = ibus, -- Fix for browsers
+    QT_IM_MODULE = ibus, -- Not sure if this works or not, but whatever
+    XMODIFIERS = ibus and ("@im=" .. ibus),
+    PATH = pathvar, -- Set the PATH environment variable to include /deps/.bin/
+    GTK_THEME = "Yaru:dark", -- Prefer dark theme
+    XDG_CURRENT_DESKTOP = "GNOME:AWESOME",
+    QT_QPA_PLATFORMTHEME = "gtk2",
+    QT_STYLE_OVERRIDE = "gtk2",
+    SHLVL = false, -- Fix terminals opened in AwesomeWM
+    NO_AT_BRIDGE = "1", -- Don't try to connect to an accessibility bus
+    _JAVA_AWT_WM_NONREPARENTING = "1", -- expose awesome as a non-reparenting window to java
+    ZEITGEIST_LOG_DIRECT_READ = "1", -- Fix a bug in diodon
+  })
 end
 
 return handle_error(setup_environment)
