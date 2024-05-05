3d53b6c10e489c6a8b3de44361f02dad24141496
aarondill <aaronsacks2006@gmail.com>
2024-05-05 03:07:28 -0500

    fix: ibus variables to 'ibus' not 'xim'

--- configuration/environment.lua
+++ configuration/environment.lua
@@ -25,13 +25,13 @@ local function setup_environment()
   setenv("PATH", pathvar) -- Set the PATH environment variable to include /deps/.bin/
 
   setenv("GTK_THEME", "Yaru:dark") -- Prefer dark theme
-  setenv("GTK_IM_MODULE", "xim") -- Fix for browsers
-  setenv("QT_IM_MODULE", "xim") -- Not sure if this works or not, but whatever
+  setenv("GTK_IM_MODULE", "ibus") -- Fix for browsers
+  setenv("QT_IM_MODULE", "ibus") -- Not sure if this works or not, but whatever
   setenv("XMODIFIERS", "@im=ibus")
   setenv("XDG_CURRENT_DESKTOP", "GNOME:AWESOME")
   setenv("QT_QPA_PLATFORMTHEME", "gtk2")
   setenv("QT_STYLE_OVERRIDE", "gtk2")
-  setenv("SHLVL", "0") -- Fix terminals opened in AwesomeWM
+  setenv("SHLVL", nil) -- Fix terminals opened in AwesomeWM
   setenv("NO_AT_BRIDGE", "1") -- Don't try to connect to an accessibility bus
   setenv("_JAVA_AWT_WM_NONREPARENTING", "1") -- expose awesome as a non-reparenting window to java
   setenv("ZEITGEIST_LOG_DIRECT_READ", "1") -- Fix a bug in diodon
