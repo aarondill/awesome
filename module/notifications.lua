6586d2365cf20c7359c4046942e6eb92a963ce65
aarondill <aaronsacks2006@gmail.com>
2024-06-29 20:31:46 -0500

    fix: use full paths in icon search
    Former-commit-id: c4ec03de396e29bb19e914fbadf85c974657a4dc

--- module/notifications.lua
+++ module/notifications.lua
@@ -8,7 +8,7 @@ local naughty = require("naughty")
 naughty.config.icon_formats = { "ico", "icon", "jpg", "png", "svg" }
 naughty.config.icon_dirs = { "/usr/share/pixmaps/", "/usr/share/icons/Yaru/", "/usr/share/icons/hicolor/" }
 -- Async. Could miss first few notifications, but hopefully is done before too many notifications.
-list_directory("/usr/share/icons", function(names, _)
+list_directory("/usr/share/icons", { full_path = true }, function(names)
   if not names then return end
   return gtable.merge(naughty.config.icon_dirs, names)
 end)
--- util/file/list_directory.lua
+++ util/file/list_directory.lua
@@ -1,11 +1,13 @@
 local assertions = require("util.types.assertions")
 local gtable = require("gears.table")
+local join = require("util.path.join")
 local require = require("util.rel_require")
 local FILE_NAME_PROP = "FILE_ATTRIBUTE_STANDARD_NAME"
 
 local scan_directory = require(..., "scan_directory") ---@module "util.file.scan_directory"
 ---@class list_directory_args :scan_directory_args
 ---@field match string? a lua pattern to match against the file names
+---@field full_path boolean? Whether to return the full path or just the file name [default: false]
 
 ---@alias list_directory_cb fun(names?: string[], error?: userdata): any?
 --- Return a file list (name only)
@@ -23,7 +25,9 @@ local function list_directory(path, args, cb)
   assertions.type(args, "table", "args")
   assertions.type(args.match, { "string", "nil" }, "args.match")
 
-  local match = args.match
+  local match, full_path = args.match, args.full_path
+  args.match, args.full_path = nil, nil -- don't pass to scan_directory
+
   args.filter = match -- if a match is specified, filter based on it.
     and function(file)
       local name = file[FILE_NAME_PROP] ---@type string
@@ -31,14 +35,14 @@ local function list_directory(path, args, cb)
     end
   args.attributes = { FILE_NAME_PROP } -- override user provided
 
-  args.match = nil -- don't pass to scan_directory
   return scan_directory(path, args, function(files, err)
     if files == nil then return cb(nil, err) end
     local names = {} ---@type string[]
     for k, v in ipairs(files) do
       local name = v[FILE_NAME_PROP]
       assert(type(name) == "string", "File name is not a string")
-      names[k] = name
+      -- Return full path if specified
+      names[k] = full_path and join(path, name) or name
     end
     return cb(names, nil)
   end)
