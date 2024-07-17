64d32d7c634b0944971c3cf7f986829b8f349733
Aaron Dill <aaronsacks2006@gmail.com>
2024-07-17 04:36:10 -0500

    feat: actual debug notifs

--- util/notifs.lua
+++ util/notifs.lua
@@ -107,6 +107,15 @@ function M.notify(loglevel, text, opts, extra_opts)
 end
 function M.notify_once(loglevel, text, opts) return M.notify(loglevel, text, opts, { once = true }) end
 
+local function get_debug_msg(text, ...)
+  local inspect, stream = require("inspect"), require("stream")
+  if type(text) ~= "string" then return inspect.inspect(text) end
+  return text:format(stream.of(...):map(inspect.inspect):unpack())
+end
+---Usage: `debug({ table = true })` or `debug("table: %s", table)`
+function M.debug(text, ...) return M.notify("warn", get_debug_msg(text, ...)) end
+function M.debug_once(text, ...) return M.notify_once("warn", get_debug_msg(text, ...)) end
+
 M.low = bind(M.notify, "low") ---@type logFunc
 M.normal = bind(M.notify, "normal") ---@type logFunc
 M.critical = bind(M.notify, "critical") ---@type logFunc
@@ -114,7 +123,6 @@ M.ok = bind(M.notify, "ok") ---@type logFunc
 M.info = bind(M.notify, "info") ---@type logFunc
 M.warn = bind(M.notify, "warn") ---@type logFunc
 M.error = bind(M.notify, "critical") ---@type logFunc -- error as synonym for critical
-M.debug = bind(M.notify, "warn") ---@type logFunc -- debug as synonym for warn
 -- Once
 M.low_once = bind(M.notify_once, "low") ---@type logFunc
 M.normal_once = bind(M.notify_once, "normal") ---@type logFunc
@@ -123,6 +131,5 @@ M.ok_once = bind(M.notify_once, "ok") ---@type logFunc
 M.info_once = bind(M.notify_once, "info") ---@type logFunc
 M.warn_once = bind(M.notify_once, "warn") ---@type logFunc
 M.error_once = bind(M.notify_once, "critical") ---@type logFunc -- error as synonym for critical
-M.debug = bind(M.notify, "warn") ---@type logFunc -- debug as synonym for warn
 
 return M
