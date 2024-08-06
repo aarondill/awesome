4a0e2ac3fd4579394bedd05a8ecc731bee03c45a
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-05 19:09:41 -0500

    fix(file): use _bytes versions to avoid writing garbage
    Former-commit-id: 7720b14ddcfe642a2970e1e90f2ab27044842e96

--- util/file/write_outputstream.lua
+++ util/file/write_outputstream.lua
@@ -1,20 +1,19 @@
+local GLib = require("util.lgi.GLib")
 local assertions = require("util.types.assertions")
-
----@class GOutputStream
----@field write_async fun(self: GOutputStream, content: string, len: integer, cancellable?: userdata, callback?: fun(self: GOutputStream, task: userdata))
----@field write_finish fun(self: GOutputStream, task: userdata): new_etags: userdata?, err: userdata?
----@field close fun(self: GOutputStream)
 
 --- Write to a stream - async
 ---@param stream GOutputStream
----@param content string
+---@param content string|GBytes
 ---@param cb? fun(err?: userdata): any?
 local function outputstream_write(stream, content, cb)
   assertions.type(content, "string", "content")
   assertions.iscallable(cb, true, "cb")
+  if type(content) == "string" then -- convert to GBytes
+    content = GLib.Bytes.new(content)
+  end
   --- only pass callback if results are needed.
   ---params(stream:write_async) GOutputStream* stream, void* buffer, gsize count, int io_priority, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
-  return stream:write_async(content, content:len(), nil, cb and function(file, task)
+  return stream:write_async_bytes(content, GLib.PRIORITY_DEFAULT, nil, cb and function(file, task)
     local new_etags, err = file:write_finish(task)
     if not new_etags then return cb(err) end
     return cb(nil)
