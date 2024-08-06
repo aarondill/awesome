7720b14ddcfe642a2970e1e90f2ab27044842e96
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-05 19:09:41 -0500

    fix(file): use _bytes versions to avoid writing garbage

--- util/file/write_async.lua
+++ util/file/write_async.lua
@@ -1,25 +1,23 @@
-local garbage_collection = require("util.garbage_collection")
+local GLib = require("util.lgi.GLib")
 local new_file_for_path = require("util.file.new_file_for_path")
 
 --- Replace a file content or create a new one - Async :)
 ---@param path string file path to write to
----@param content string content to write to the file
+---@param content string|GBytes content to write to the file
 ---@param cb fun(error?: userdata)? function to call when done.
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_write(path, content, cb)
   if type(path) ~= "string" or type(content) ~= "string" then error("path and content must be strings", 2) end
   cb = cb or function() end
-  local index = garbage_collection.save(content)
 
+  if type(content) == "string" then -- convert to GBytes
+    content = GLib.Bytes.new(content)
+  end
   ---params(replace_contents_async): string contents, string etag, boolean make_backup, GFileCreateFlags flags, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
-  --- NOTE: This function does not copy `content`, so we must protect it from being garbage-collected (resulting in garbage being written to disk)
-  new_file_for_path(path):replace_contents_async(content, nil, false, 0, nil, function(file, task)
+  new_file_for_path(path):replace_contents_bytes_async(content, nil, false, 0, nil, function(file, task)
     --- Finish the write operation and close the file(?)
     local new_etags, error = file:replace_contents_finish(task)
 
-    --- Clear the content to allow garbage collection - Avoid a memory leak
-    garbage_collection.release(index)
-    index = nil
     collectgarbage("collect")
     if not new_etags then
       assert(type(error) == "userdata", "Non-userdata error returned from replace_contents_finish!")
