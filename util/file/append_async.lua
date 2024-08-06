81e790bc48a57224a3224dc0946c20ae8964c513
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-05 21:52:25 -0500

    fix: append_async *don't* save the string
    write_outputstream doesn't need this anymore
    
    
    Former-commit-id: 186c96e1d08525a66b869a8014d01bb1cceae055

--- util/file/append_async.lua
+++ util/file/append_async.lua
@@ -1,6 +1,6 @@
-local garbage_collection = require("util.garbage_collection")
 local new_file_for_path = require("util.file.new_file_for_path")
 local outputstream_write = require("util.file.write_outputstream")
+local GLib = require("util.lgi").GLib
 
 --- Append to a file's content - Async :)
 ---@param path string file path to append to
@@ -10,23 +10,14 @@ local outputstream_write = require("util.file.write_outputstream")
 local function file_append(path, content, cb)
   if type(path) ~= "string" or type(content) ~= "string" then error("path and content must be strings", 2) end
   cb = cb or function() end
-  --- Store the content in the global array
-  local index = garbage_collection.save(content)
-  local io_priority = 0
 
   ---params(file:g_file_append_to_async): GFile* file, GFileCreateFlags flags, int io_priority, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
   -- Append to file
-  return new_file_for_path(path):append_to_async({}, io_priority, nil, function(file, task)
+  return new_file_for_path(path):append_to_async({}, GLib.PRIORITY_DEFAULT, nil, function(file, task)
     local stream, append_err = file:append_to_finish(task)
-    if not stream then
-      garbage_collection.release(index)
-      index = nil
-      return cb(append_err)
-    end
+    if not stream then return cb(append_err) end
     return outputstream_write(stream, content, function(write_err)
       stream:close_async()
-      garbage_collection.release(index)
-      index = nil
       return cb(write_err)
     end)
   end, 0)
