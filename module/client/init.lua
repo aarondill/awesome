6d5fd09ab94e450e272ea5ca6fbd4deac853036e
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-09 03:00:31 -0500

    fix: make sticky windows not unexpectedly steal the focus

--- module/client/init.lua
+++ module/client/init.lua
@@ -1,4 +1,13 @@
+local ewmh = require("awful.ewmh")
 local require = require("util.rel_require")
+
 require(..., "decoration") ---@module "module.client.decoration"
 require(..., "rules") ---@module "module.client.rules"
 require(..., "titlebar") ---@module "module.client.titlebar"
+
+local function focus_handler(c) ---@param c AwesomeClientInstance
+  if c.sticky then return false end
+end
+ewmh.add_activate_filter(focus_handler, "autofocus.check_focus")
+ewmh.add_activate_filter(focus_handler, "autofocus.check_focus_tag")
+ewmh.add_activate_filter(focus_handler, "mouse_enter")
