be1cd3e9a01d22aade822605d5624ad021d981b8
Aaron Dill <aaronsacks2006@gmail.com>
2024-08-11 01:41:05 -0500

    chore: cleanup unused code in setup.sh
    Former-commit-id: 5560f3f1bd3a460c799ef8d16b26e47047ed71ef

--- setup.sh
+++ setup.sh
@@ -49,45 +49,8 @@ if [ "$install" -eq 1 ]; then
   esac
 fi
 
-# Use the AWESOME and LUA variables to configure this if needed
-[ -n "${LUA:-}" ] || LUA=()
-AWESOME=${AWESOME:-awesome}
-if [ "${#LUA[@]}" -eq 0 ]; then
-  declare -A lua_hash=() # Remove duplicate entries automatically
-  while read -r -d: path; do
-    path=${path:-"$PWD"} # empty path means pwd
-    [ -d "$path" ] || continue
-    for f in "$path"/lua*; do
-      [ -x "$f" ] || continue
-      case "$f" in */luac* | */luajit*) continue ;; esac # I'm not looking for luajit or luac
-      f=$(realpath -- "$f") || continue
-      lua_hash["$f"]=1
-    done
-  done < <(printf '%s' "$PATH:")
-  LUA=("${!lua_hash[@]}") lua_hash=()
-
-  lua_version=$("$AWESOME" --version | perl -ne 'm/running with Lua (\d+.\d+)/ && print "$1"')
-  has "lua$lua_version" || abort "Could not find lua $lua_version required for awesome $AWESOME!" 1
-fi
-
-# Check that lua is installed.
-# Do this to allow the user to choose their preferred lua version (since 'lua' is a virtual package in debian/ubuntu)
-[ "${#LUA[@]}" -gt 0 ] || abort "Could not find any lua installations!" 1
-
 git submodule update --init --recursive
 
 AR_LAUNCHER_DIR="$dir/deps/autorandr/contrib/autorandr_launcher"
 log "Compiling autorandr_launcher (${AR_LAUNCHER_DIR#"$dir/"})"
 (cd "$AR_LAUNCHER_DIR" && make -s)
-
-# LUAPOSIX_DIR="$dir/deps/luaposix" LUAPOSIX_DEST="$dir/deps/.build"
-#
-# pushd "$LUAPOSIX_DIR" >/dev/null
-# for lua in "${LUA[@]}"; do
-#   log "Compiling luaposix (${LUAPOSIX_DIR#"$dir/"}) using lua $lua"
-#   # Note: if using lua 5.1, we may be missing the 'bit32' module!
-#   "$lua" "$LUAPOSIX_DIR/build-aux/luke" --quiet LDOC='true' # build luaposix, set LDOC to 'true' to avoid compiling docs
-#   log "Installing luaposix to ${LUAPOSIX_DEST#"$dir/"}"
-#   "$lua" "$LUAPOSIX_DIR/build-aux/luke" --quiet PREFIX="$LUAPOSIX_DEST" install # 'install' to ./deps/.build so these can be required
-# done
-# popd >/dev/null
