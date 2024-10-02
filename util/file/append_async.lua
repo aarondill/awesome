8444da272a40af90a58e41fa85cb6eac3b18e4dd
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-02 10:00:28 -0500

    fix: append_async shouldn't throw on GFile being passed in
    Former-commit-id: 926aed6b62fa4e469284597679a740b66d266563

--- util/file/append_async.lua
+++ util/file/append_async.lua
@@ -3,16 +3,12 @@ local outputstream_write = require("util.file.write_outputstream")
 local GLib = require("lgi").GLib
 
 --- Append to a file's content - Async :)
----@param path string file path to append to
+---@param path string|GFile file path to append to
 ---@param content string content to append to the file
 ---@param cb fun(err?: userdata)? function to call when done.
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_append(path, content, cb)
-  if type(path) ~= "string" or type(content) ~= "string" then error("path and content must be strings", 2) end
   cb = cb or function() end
-
-  ---params(file:g_file_append_to_async): GFile* file, GFileCreateFlags flags, int io_priority, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
-  -- Append to file
   return new_file_for_path(path):append_to_async("NONE", GLib.PRIORITY_DEFAULT, nil, function(file, task)
     local stream, append_err = file:append_to_finish(task)
     if not stream then return cb(append_err) end
