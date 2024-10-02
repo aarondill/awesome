926aed6b62fa4e469284597679a740b66d266563
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-02 10:00:28 -0500

    fix: append_async shouldn't throw on GFile being passed in

--- util/file/new_file_for_path.lua
+++ util/file/new_file_for_path.lua
@@ -2,8 +2,8 @@ local Gio = require("lgi").Gio
 ---If a File is passed, it is returned.
 ---@param path string|GFile
 local function new_file_for_path(path)
-  if type(path) ~= "string" then return path end
-  assert(type(path) == "string")
+  if type(path) == "userdata" then return path end
+  assert(type(path) == "string", "path must be a string or GFile")
   return Gio.File.new_for_path(path)
 end
 return new_file_for_path
