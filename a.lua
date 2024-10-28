07e71190c5b605d91c68267d747469cca0531cae
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-28 12:20:09 -0500

    feat: a.sync and a.wait

---
+++ a.lua
@@ -0,0 +1,37 @@
+---@class AsyncModule
+local a = {}
+---Use like javascript's new Promise constructor (await(function(resolve) resolve(1) end))
+---@generic A
+---@param f fun(resolve: fun(a: A, ...: any))
+---@return A, any
+function a.wait(f)
+  local co = coroutine.running()
+  local ret
+  f(function(...)
+    if coroutine.status(co) == "running" then
+      ret = ret or table.pack(...)
+    else
+      coroutine.resume(co, ...)
+    end
+  end)
+  if ret then return table.unpack(ret, 1, ret.n) end
+  return coroutine.yield()
+end
+
+---Use to define an async function. Use with a.wait(function(resolve) resolve(1) end)
+---The (optional) callback is the first argument, other arguments are passed to the function
+---The callback will be called with the return value(s)
+---Using this avoids the pyramid of doom, but screws with type checking
+---@generic A, R
+---@param f fun(...: A): R,...: unknown
+---@return fun(cb: fun(r: R): any?,...: A )
+function a.sync(f)
+  return coroutine.wrap(function(...)
+    local args = table.pack(...)
+    local callback = args[1]
+    -- NOTE: args[1] is the callback, so unpack from 2 to n
+    local val = table.pack(f(table.unpack(args, 2, args.n)))
+    if callback then return callback(table.unpack(val, 1, val.n)) end
+  end)
+end
+return a
--- widget/battery/dbus.lua
+++ widget/battery/dbus.lua
@@ -1,5 +1,5 @@
 local Gio = require("lgi").Gio
-local await = require("await")
+local a = require("a")
 local parallel_async = require("util.parallel_async")
 local properties = require("util.dbus.properties")
 local properties_changed = require("util.dbus.properties_changed")
@@ -9,36 +9,34 @@ local M = {}
 ---@type table<table, SubscribeID>
 local upower_listeners = setmetatable({}, { __mode = "k" })
 
----@param callback fun(bat_path?: string, err?: GError): unknown?
-local function find_battery(callback)
-  return coroutine.wrap(function()
-    ---@type GDBusConnection, GAsyncResult
-    local bus, gtask = await(function(resolve)
-      local name, path, iname = "org.freedesktop.UPower", "/org/freedesktop/UPower", "org.freedesktop.UPower"
-      return Gio.bus_get_sync(Gio.BusType.SYSTEM)
-        :call(name, path, iname, "EnumerateDevices", nil, nil, 0, -1, nil, resolve)
-    end)
-    local result, err = bus:call_finish(gtask)
-    if not result or err then return callback(nil, err) end
-    local devices = result[1] --[[ @as string[] ]]
-    ---@type table<string?, boolean>
-    local res = await(function(resolve)
-      return parallel_async(devices, function(dev, done)
-        return properties.get_all("org.freedesktop.UPower", dev, "org.freedesktop.UPower.Device", function(v_props)
-          local props = v_props[1] --[[ @as table<string, unknown> ]]
-          local Type, PowerSupply = props.Type, props.PowerSupply
-          assert(type(Type) == "number", "type is not a number") ---@cast Type number
-          assert(type(PowerSupply) == "boolean", "power_supply is not a boolean") ---@cast PowerSupply boolean
-          local is_battery = Type == 2
-          local is_laptop_battery = is_battery and PowerSupply
-          return done(is_laptop_battery)
-        end)
-      end, resolve)
-    end)
-    local _, bat = tables.find(res, function(is_battery) return is_battery end)
-    return callback(bat, nil)
-  end)()
-end
+---@type fun(callback: fun(bat_path?: string, err?: GError): unknown?)
+local find_battery = a.sync(function()
+  ---@type GDBusConnection, GAsyncResult
+  local bus, gtask = a.wait(function(resolve)
+    local name, path, iname = "org.freedesktop.UPower", "/org/freedesktop/UPower", "org.freedesktop.UPower"
+    return Gio.bus_get_sync(Gio.BusType.SYSTEM)
+      :call(name, path, iname, "EnumerateDevices", nil, nil, 0, -1, nil, resolve)
+  end)
+  local result, err = bus:call_finish(gtask)
+  if not result or err then return nil, err end
+  local devices = result[1] --[[ @as string[] ]]
+  ---@type table<string?, boolean>
+  local res = a.wait(function(resolve)
+    return parallel_async(devices, function(dev, done)
+      return properties.get_all("org.freedesktop.UPower", dev, "org.freedesktop.UPower.Device", function(v_props)
+        local props = v_props[1] --[[ @as table<string, unknown> ]]
+        local Type, PowerSupply = props.Type, props.PowerSupply
+        assert(type(Type) == "number", "type is not a number") ---@cast Type number
+        assert(type(PowerSupply) == "boolean", "power_supply is not a boolean") ---@cast PowerSupply boolean
+        local is_battery = Type == 2
+        local is_laptop_battery = is_battery and PowerSupply
+        return done(is_laptop_battery)
+      end)
+    end, resolve)
+  end)
+  local _, bat = tables.find(res, function(is_battery) return is_battery end)
+  return bat, nil
+end)
 
 ---@param widget any
 ---@param callback fun(changed: {State: string}): any?
