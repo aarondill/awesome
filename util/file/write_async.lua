aaa576cd05f210d3367e50c82b411fd9a9da5936
Aaron Dill <aaronsacks2006@gmail.com>
2024-12-31 16:00:36 -0600

    feat: add file.sync.write_sync
    Former-commit-id: e4a2222b9ce687b9507d6fdc8936a5d51e041b12

--- util/file/write_async.lua
+++ util/file/write_async.lua
@@ -2,17 +2,15 @@ local GLib = require("lgi").GLib
 local new_file_for_path = require("util.file.new_file_for_path")
 
 --- Replace a file content or create a new one - Async :)
----@param path string file path to write to
+---@param path string|GFile file path to write to
 ---@param content string|GBytes content to write to the file
 ---@param cb fun(error?: userdata)? function to call when done.
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_write(path, content, cb)
-  if type(path) ~= "string" or type(content) ~= "string" then error("path and content must be strings", 2) end
+  -- convert to GBytes
+  if type(content) == "string" then content = GLib.Bytes.new(content) end
   cb = cb or function() end
 
-  if type(content) == "string" then -- convert to GBytes
-    content = GLib.Bytes.new(content)
-  end
   ---params(replace_contents_async): string contents, string etag, boolean make_backup, GFileCreateFlags flags, GCancellable* cancellable, GAsyncReadyCallback callback, gpointer user_data
   new_file_for_path(path):replace_contents_bytes_async(content, nil, false, 0, nil, function(file, task)
     --- Finish the write operation and close the file(?)
