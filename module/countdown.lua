01e3e39917b53b02bfe813ba0f152b5a557fb820
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-29 08:18:15 -0600

    feat: custom units in countdown
    Former-commit-id: bf6add8e0f687a467f91e3e080b74a7e867f824e

--- module/countdown.lua
+++ module/countdown.lua
@@ -3,6 +3,7 @@ local ascreen = require("awful.screen")
 local desktop = require("widget.desktop")
 local gtable = require("gears.table")
 local gtimer = require("gears.timer")
+local tables = require("util.tables")
 local wibox = require("wibox")
 local widgets = require("util.awesome.widgets")
 
@@ -21,6 +22,7 @@ local CountdownWidget = {
 ---@field screen AwesomeScreenInstance
 ---@field end_time number
 ---@field event string
+---@field units? table<string, integer>
 ---@field fg? string
 ---@field bg? string
 
@@ -48,42 +50,56 @@ function CountdownWidget.new(opts)
   return self
 end
 
+---@param num number
+---@param dec_places? integer
+---@return number
+local function round(num, dec_places) --
+  local mult = 10 ^ (dec_places or 0)
+  num = num * mult
+  local res = num >= 0 and math.floor(num + 0.5) or math.ceil(num - 0.5)
+  return res / mult
+end
+
 ---@param u string
 ---@param n integer
-local function unit(u, n) return ("%d %s%s"):format(n, u, (n == 1 and "" or "s")) end
-function CountdownWidget:get_time_remaining(t) ---@param t integer
+local function unit(u, n) return ("%s %s%s"):format(n, u, (n == 1 and "" or "s")) end
+---@param units? table<string, integer>
+function CountdownWidget:get_time_remaining(t, units) ---@param t integer
   local diff = os.difftime(t, os.time())
   if diff < 0 then return "Time's up!" end
 
   --- Calculate the time remaining
-  local years = math.floor(diff / 60 / 60 / 24 / 365)
-  diff = diff % (60 * 60 * 24 * 365)
-  local days = math.floor(diff / 60 / 60 / 24)
-  diff = diff % (60 * 60 * 24)
-  local hours = math.floor(diff / 60 / 60)
-  diff = diff % (60 * 60)
-  local minutes = math.floor(diff / 60)
-  diff = diff % 60
-  local seconds = math.floor(diff)
+  units = units or { year = 365 * 24 * 60 * 60, day = 24 * 60 * 60, hour = 60 * 60, minute = 60, second = 1 }
+  local unit_names_sorted = gtable.keys(units)
+  table.sort(unit_names_sorted, function(a, b) return units[a] > units[b] end)
 
-  local units = { "year", "day", "hour", "minute", "second" }
-  local fmt = { years, days, hours, minutes, seconds }
-  local fmt_len = #fmt
-
-  --- Remove leading zeros
-  for i, val in ipairs(fmt) do
-    if val ~= 0 then break end
-    fmt[i], units[i] = nil, nil
+  ---@type table<string, number>
+  local unit_counts = {}
+  for _, unit_name in ipairs(unit_names_sorted) do
+    local unit_sec = units[unit_name]
+    local unit_count = math.floor(diff / unit_sec)
+    unit_counts[unit_name] = unit_count
+    diff = diff - (unit_count * unit_sec)
   end
 
+  ---@type number[]
+  local fmt = tables.map_val(unit_names_sorted, function(unit_name) return unit_counts[unit_name] end)
   --- Add units
   local formatted = {}
-  for i = 1, fmt_len do
-    local val = fmt[i]
-    if val then
-      local formatted_unit = unit(units[i], val)
+  --- Remove leading zeros
+  local has_found_non_zero = false
+  for i, val in ipairs(fmt) do
+    if has_found_non_zero or val ~= 0 then
+      has_found_non_zero = true
+      local formatted_unit = unit(unit_names_sorted[i], val)
       table.insert(formatted, formatted_unit)
     end
+  end
+  -- If there are no non-zero values, show the fraction of the last unit (rounded to 2 decimal places)
+  if #formatted == 0 then
+    local u = unit_names_sorted[#unit_names_sorted]
+    local v = round(diff / units[u], 2)
+    return unit(u, v)
   end
 
   return table.concat(formatted, "\n")
@@ -93,7 +109,7 @@ end
 function CountdownWidget:update()
   if not self.visible then return end
   local end_time, event = self.end_time, self.event
-  local diff_str = self:get_time_remaining(end_time)
+  local diff_str = self:get_time_remaining(end_time, self.units)
 
   local w = self.widget ---@type widget
   local textbox = widgets.get_by_id(w, "textbox")
