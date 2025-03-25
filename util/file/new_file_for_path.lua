cc4d46732e78e69c2c5763a782eab1715073dea3
Aaron Dill <aaronsacks2006@gmail.com>
2025-03-25 02:13:07 -0500

    fix: new_file_for_path should only return the gfile if it is a gfile
    Former-commit-id: 9abd94273c99ade3a2270de3df839ba6e1c06b3d

--- util/file/new_file_for_path.lua
+++ util/file/new_file_for_path.lua
@@ -1,8 +1,13 @@
-local Gio = require("lgi").Gio
+local lgi = require("lgi")
+local Gio, GObject = lgi.Gio, lgi.GObject
 ---If a File is passed, it is returned.
 ---@param path string|GFile
+---@return GFile
 local function new_file_for_path(path)
-  if type(path) == "userdata" then return path end
+  if GObject.Object:is_type_of(path) then --- There's no better way to check for GFile
+    assert(type(path) == "userdata", "a GFile was somehow not a userdata")
+    return path
+  end
   assert(type(path) == "string", "path must be a string or GFile")
   return Gio.File.new_for_path(path)
 end
