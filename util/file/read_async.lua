e1bf39d04a13fed61197cac3a5d51265ac8b1960
Aaron Dill <aaronsacks2006@gmail.com>
2025-03-25 02:13:21 -0500

    fix: types in read_async
    Former-commit-id: 757965a17824d03ccbd3d824a9aae23f0d61628e

--- util/file/read_async.lua
+++ util/file/read_async.lua
@@ -2,22 +2,23 @@ local assertions = require("util.types.assertions")
 local new_file_for_path = require("util.file.new_file_for_path")
 
 ---Get the contents of a file - Async :)
----@generic Path :string
+---@generic Path :string|GFile
 ---@param path Path file path to read
 ---function to call when done. content will be nil if the file does not exist
 ---Path is the same as was passed into the function. It is *not* expanded.
----@param cb fun(content?: string, error?: userdata, path: Path): any
+---@param cb fun(content?: string, error?: GError, path: Path): any
 ---@source https://github.com/Elv13/awesome-configs/blob/master/utils/fd_async.lua
 local function file_read(path, cb)
-  assertions.type(path, "string", "path")
   assertions.iscallable(cb, "cb")
 
   return new_file_for_path(path):load_contents_async(nil, function(file, task)
     -- onsuccess: content, etag_out
     -- onfail: success(false), error
     local content, error = file:load_contents_finish(task)
-    -- LuaLS's type narrowing doesn't realize that this can't be a number if content is nil.
-    if not content and type(error) ~= "number" then return cb(nil, error, path) end
+    if not content then
+      assert(type(error) == "userdata", "error is not a GError")
+      return cb(nil, error, path)
+    end
     return cb(content, nil, path)
   end)
 end
