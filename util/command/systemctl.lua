aa84d5dd6c0d7fc44f551fd9601f05a4ee42d548
Aaron Dill <aaronsacks2006@gmail.com>
2026-01-28 22:28:40 -0600

    feat: systemctl commands notify on fail
    this makes the poweroff button in the power menu notify when shutdown is
    inhibited
    
    
    Former-commit-id: f199ece8bd7624ccbd28f6f238f379e4c8d59583

--- util/command/systemctl.lua
+++ util/command/systemctl.lua
@@ -1,10 +1,21 @@
-local bind = require("util.bind")
+local notifs = require("util.notifs")
 local spawn = require("util.spawn")
+local strings = require("util.strings")
 ---Run `systemctl cmd`, handling sudo and polkit
 ---@param cmd string? the command to run.
-local function systemctl(cmd)
+---@param notify_on_fail? boolean? Whether to notify on failure. Default: true
+local function systemctl(cmd, notify_on_fail)
   cmd = cmd or "suspend-then-hibernate"
-  local spawner = bind.with_args(spawn.nosn, { "systemctl", cmd })
+  local spawner = function()
+    spawn.async({ "systemctl", cmd }, function(_stdout, stderr, reason, code)
+      if spawn.is_normal_exit(reason, code) then return end -- Success
+      if notify_on_fail == false then return end
+      notifs.warn(strings.trim(stderr), {
+        title = "systemctl",
+        timeout = 10,
+      })
+    end, { sn_rules = false })
+  end
   -- Try with sudo incase no password is needed (for hibernate)
   return spawn.nosn({ "sudo", "-n", "--", "systemctl", cmd }, {
     exit_callback_err = spawner, -- Spawn without sudo if original fails
