382f856dc74af3b7a6f7cb7468c63cb8a720cb32
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-17 16:07:20 -0600

    feat: move sources to embedded lua files
    Former-commit-id: e5e8f396606037e7b539cb505264bc7709aa99f5

---
+++ wallpapers/C/sources.lua
@@ -0,0 +1,12 @@
+--- Files are downloaded to ./images/<name>
+return { ---@type WallpaperSourceSet
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/wallhaven-9mjw78.png",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/staircase.jpg",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/river.png",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/mountains.png",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/minimal_landscape.jpg",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/lake.jpg",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/australia.jpg",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/Computerized_Art_3440x1440_7.jpg",
+  "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/beach_landscape.png",
+}
--- wallpapers/download.lua
+++ wallpapers/download.lua
@@ -1,34 +1,16 @@
 local basename = require("util.path.basename")
+local exists = require("util.file.sync.exists")
 local gfilesystem = require("gears.filesystem")
 local gtable = require("gears.table")
+local lgi = require("lgi")
 local new_file_for_path = require("util.file.new_file_for_path")
-local parallel_async = require("util.parallel_async")
-local Gio, GLib = require("lgi").Gio, require("lgi").GLib
-local GObject = require("lgi").GObject
-local a = require("a")
-local exists = require("util.file.sync.exists")
 local notifs = require("util.notifs")
+local parallel_async = require("util.parallel_async")
 local path = require("util.path")
-local read_async = require("util.file.read_async")
 local tables = require("util.tables")
-local write_async = require("util.file.write_async")
----@alias URL string
+local Gio, GLib, GObject = lgi.Gio, lgi.GLib, lgi.GObject
+---@class URL :string
 ---@alias WallpaperSourceSet URL[] | table<URL, string>
----@type table<string, WallpaperSourceSet>
-local sets = {
-  C = {
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/wallhaven-9mjw78.png",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/staircase.jpg",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/river.png",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/mountains.png",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/minimal_landscape.jpg",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/lake.jpg",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/australia.jpg",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/Computerized_Art_3440x1440_7.jpg",
-    "https://raw.githubusercontent.com/AlexandrosLiaskos/Awesome_Wallpapers/main/images/beach_landscape.png",
-  },
-  ---@type fun(done: fun(success: boolean), url: URL, dest: string)
-}
 
 ---@param callback fun(success: boolean)
 ---@param url string
@@ -54,22 +36,25 @@ local _tries = {
 ---@param done fun(success: boolean)
 ---@return boolean already_downloaded if true, no done will never be called
 local function get_set_async(set_name, done)
-  local set = sets[set_name]
-  if not set then return false end -- no urls needed in this set
+  ---@type boolean, WallpaperSourceSet
+  local ok, set = pcall(require, "wallpapers." .. set_name .. ".sources")
+  if not ok or not set then return false end -- no urls needed in this set
+  assert(type(set) == "table")
   -- make `dest` relative to /wallpapers/<set>
   local p = path.resolve(gfilesystem.get_configuration_dir(), "wallpapers", set_name, "images")
   ---@type { url: URL, dest: string }[]
   local info = {}
   for k, v in pairs(set) do
+    local r
     if type(v) == "string" then
       local name = basename(v)
-      table.insert(info, { url = v, dest = path.resolve(p, name) })
+      r = { url = v, dest = path.resolve(p, name) }
     else
       assert(type(k) == "string")
-      table.insert(info, { url = k, dest = path.resolve(p, v) })
+      r = { url = k, dest = path.resolve(p, v) }
     end
+    if not exists(r.dest) then table.insert(info, r) end -- Remove existing files
   end
-  info = tables.filter(info, function(val) return not exists(val.dest) end) -- Remove existing files
   if #info == 0 then return true end
   local tries = _tries[set_name] or 0
   _tries[set_name] = tries + 1
