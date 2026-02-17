c4e8e434465d9b09062980adfd00aae906f4749d
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-17 16:43:30 -0600

    fix: download set A dynamically
    Former-commit-id: 3195281fec9e27d1b37c39e31f50f8903261446a

--- wallpapers/A/sources.lua
+++ wallpapers/A/sources.lua
@@ -1,3 +1,11 @@
 return { ---@type WallpaperSourceSet
   ["https://unsplash.com/photos/_OQ8Jc7kBmA/download?force=true"] = "forest.jpg",
+  ["https://4kwallpapers.com/images/wallpapers/wrecked-boat-sailing-boat-night-time-starry-sky-milky-way-3840x2160-5011.jpg"] = "boat.jpg",
+  ["https://unsplash.com/photos/8Gl7Ew-q6D8/download?force=true"] = "water-under-moon.jpg",
+  ["https://4kwallpapers.com/images/wallpapers/lightning-strike-stormy-clouds-dark-sky-natural-phenomena-5k-3840x2160-5844.jpg"] = "lightning.jpg",
+  ["https://unsplash.com/photos/zZkMki0yH6I/download?force=true"] = "fire.jpg",
+  ["https://unsplash.com/photos/u27Rrbs9Dwc/download?force=true"] = "mountain.jpg",
+  ["https://unsplash.com/photos/6uEtb1fLX7E/download?force=true"] = "black-leafed-tree.jpg",
+  ["https://unsplash.com/photos/WsEbnsnKbUE/download?force=true"] = "lightbulb.jpg",
+  ["https://unsplash.com/photos/2QUvkQTBh5s/download?force=true"] = "clouds.jpg",
 }
