9e95d2b3722bea322541bc6d39f2e28e1b76446a
Aaron Dill <aaronsacks2006@gmail.com>
2026-09-03 01:30:00 -0500

    refactor: remove dead code

--- util/path/dirname.lua
+++
@@ -1,7 +0,0 @@
-local lgi = require("lgi")
-local GLib = lgi.GLib
----Gets the dirname
----@param path string
----@return string
-local function dirname(path) return GLib.path_get_dirname(path) end
-return dirname
